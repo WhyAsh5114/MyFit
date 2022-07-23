e4c6352a5ad63f50df4cd6e87b428b7db2abbc9a
WhyAsh5114 <whyash5114@gmail.com>
2022-07-23 15:57:40 +0530

    improved modal colors based on Title

--- src/lib/MyModal.svelte
+++ src/lib/MyModal.svelte
@@ -1,18 +1,32 @@
 <script lang="ts">
 	export let modalOpen = false;
+	export let modalTitle: string;
 	export let modalTexts: string[] = [];
+	export let onClose: () => void = () => {};
+
+	let modalTitleColor = 'text-white';
+
+	// The dollar means re-run whenever referenced value (modalTitle) is changed
+	// Whenever modalTitle is changed we want to change the color of it as well
+	$: if (modalTitle == 'Error') {
+		modalTitleColor = 'text-red-500';
+	} else if (modalTitle == 'Success') {
+		modalTitleColor = 'text-green-500';
+	} else if (modalTitle == 'Warning') {
+		modalTitleColor = 'text-yellow-500';
+	}
 </script>
 
 <input type="checkbox" id="my-modal" class="modal-toggle" bind:checked={modalOpen} />
-<label for="my-modal" class="modal cursor-pointer">
-	<label
-		class="modal-box relative bg-primary"
-		for=""
-		on:click|self={() => {
-			modalOpen = false;
-		}}
-	>
-		<h3 class="text-lg font-bold text-error">Error</h3>
+<label
+	for="my-modal"
+	class="modal modal-bottom md:modal-middle cursor-pointer"
+	on:click={() => {
+		onClose();
+	}}
+>
+	<label class="modal-box relative bg-primary" for="">
+		<h3 class="text-lg font-bold text-error {modalTitleColor}">{modalTitle}</h3>
 		<ul class="py-4" data-test-id="modal-messages-list">
 			{#each modalTexts as text}
 				<li>{text}</li>
