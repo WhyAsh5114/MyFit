3728524b7ad13ba442505479bfa49368edfcb0b7
WhyAsh5114 <whyash5114@gmail.com>
2022-07-23 16:01:14 +0530

    udpated homepage testing

--- tests/pages/index.spec.ts
+++ tests/pages/index.spec.ts
@@ -5,19 +5,17 @@ test.beforeEach(async ({ page }) => {
 	await page.goto('/');
 });
 
-test.describe('Testing home page', () => {
-	test('should only show login in dropdown when not signed in', async ({ page }) => {
+test('should show [login, create account] in dropdown when not signed in', async ({ page }) => {
+	const dropdown = page.locator('ul[data-test-id=profile-options-dropdown] li');
+	const options = await dropdown.allTextContents();
+	expect(options).toStrictEqual(['Login', 'Register']);
+});
+
+testLoggedIn(
+	'should show [profile, settings, logout] in dropdown when signed in',
+	async ({ page }) => {
 		const dropdown = page.locator('ul[data-test-id=profile-options-dropdown] li');
 		const options = await dropdown.allTextContents();
-		expect(options).toStrictEqual(['Login']);
-	});
-
-	testLoggedIn(
-		'should show [profile, settings, logout] in dropdown when signed in',
-		async ({ page }) => {
-			const dropdown = page.locator('ul[data-test-id=profile-options-dropdown] li');
-			const options = await dropdown.allTextContents();
-			expect(options).toStrictEqual(['Profile', 'Settings', 'Logout']);
-		}
-	);
-});
+		expect(options).toStrictEqual(['Profile', 'Settings', 'Logout']);
+	}
+);
