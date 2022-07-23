10fbfb7434204a9126f92ce1ca84035130b82b70
WhyAsh5114 <whyash5114@gmail.com>
2022-07-23 17:32:00 +0530

    rename appropriately

---
+++ tests/pages/__layout.spec.ts
@@ -0,0 +1,44 @@
+import { test, expect } from '@playwright/test';
+import { testLoggedIn } from '../fixtures.js';
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('/');
+});
+
+test('should show [login, create account] in dropdown when not signed in', async ({ page }) => {
+	const dropdown = page.locator('ul[data-test-id=profile-options-dropdown] li');
+	const options = await dropdown.allTextContents();
+	expect(options).toStrictEqual(['Login', 'Register']);
+
+	await page.locator('button[data-test-id=dropdown-button]').click();
+	await dropdown.first().click();
+	await expect(page).toHaveURL('/profile/login');
+	await page.goBack();
+
+	await page.locator('button[data-test-id=dropdown-button]').click();
+	await dropdown.nth(1).click();
+	await expect(page).toHaveURL('/profile/register');
+});
+
+testLoggedIn(
+	'should show [profile, settings, logout] in dropdown when signed in',
+	async ({ page }) => {
+		const dropdown = page.locator('ul[data-test-id=profile-options-dropdown] li');
+		const options = await dropdown.allTextContents();
+		expect(options).toStrictEqual(['Profile', 'Settings', 'Logout']);
+
+		await page.locator('button[data-test-id=dropdown-button]').click();
+		await dropdown.nth(0).click();
+		await expect(page).toHaveURL('/profile');
+		await page.goBack();
+
+		await page.locator('button[data-test-id=dropdown-button]').click();
+		await dropdown.nth(1).click();
+		await expect(page).toHaveURL('/profile/settings');
+		await page.goBack();
+
+		await page.locator('button[data-test-id=dropdown-button]').click();
+		await dropdown.nth(2).click();
+		await expect(page).toHaveURL('/profile/login');
+	}
+);
