67e43a20c131dad74403efc57d9933d3fd56d010
WhyAsh5114 <whyash5114@gmail.com>
2022-07-23 16:01:41 +0530

    simpler tests removed describe, too clunky and unnecessary

--- tests/pages/profile/login.spec.ts
+++ tests/pages/profile/login.spec.ts
@@ -4,30 +4,28 @@ test.beforeEach(async ({ page }) => {
 	await page.goto('/profile/login');
 });
 
-test.describe('Testing login page', () => {
-	test('should throw error (Username and Password cannot be empty)', async ({ page }) => {
-		await page.locator('button', { hasText: 'Submit' }).click();
+test('should throw error (Username and Password cannot be empty)', async ({ page }) => {
+	await page.locator('button', { hasText: 'Submit' }).click();
 
-		const messages = page.locator('ul[data-test-id=modal-messages-list] li');
-		expect(await messages.allTextContents()).toStrictEqual([
-			'Username cannot be empty',
-			'Password cannot be empty'
-		]);
-	}),
+	const messages = page.locator('ul[data-test-id=modal-messages-list] li');
+	expect(await messages.allTextContents()).toStrictEqual([
+		'Username cannot be empty',
+		'Password cannot be empty'
+	]);
+});
 
-    test('should throw error (Username cannot be empty)', async ({ page }) => {
-        await page.fill('input[placeholder=Password]', 'password');
-        await page.locator('button', { hasText: 'Submit' }).click();
+test('should throw error (Username cannot be empty)', async ({ page }) => {
+	await page.fill('input[placeholder=Password]', 'password');
+	await page.locator('button', { hasText: 'Submit' }).click();
 
-        const messages = page.locator('ul[data-test-id=modal-messages-list] li');
-        expect(await messages.allTextContents()).toStrictEqual(['Username cannot be empty']);
-    }),
+	const messages = page.locator('ul[data-test-id=modal-messages-list] li');
+	expect(await messages.allTextContents()).toStrictEqual(['Username cannot be empty']);
+});
 
-    test('should throw error (Password cannot be empty)', async ({ page }) => {
-        await page.fill('input[placeholder=Username]', 'username');
-        await page.locator('button', { hasText: 'Submit' }).click();
+test('should throw error (Password cannot be empty)', async ({ page }) => {
+	await page.fill('input[placeholder=Username]', 'username');
+	await page.locator('button', { hasText: 'Submit' }).click();
 
-        const messages = page.locator('ul[data-test-id=modal-messages-list] li');
-        expect(await messages.allTextContents()).toStrictEqual(['Password cannot be empty']);
-    })
+	const messages = page.locator('ul[data-test-id=modal-messages-list] li');
+	expect(await messages.allTextContents()).toStrictEqual(['Password cannot be empty']);
 });
