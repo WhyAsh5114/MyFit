b0b19ee30801b1c6e7df9ba50795d4423a3471e5
WhyAsh5114 <whyash5114@gmail.com>
2022-07-23 16:13:04 +0530

    added another test should throw error if user already exists in DB

--- tests/auth/register_auth_api.spec.ts
+++ tests/auth/register_auth_api.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { testWithCreatableUsername, testWithExistingUsername } from '../fixtures.js';
 
 test.beforeEach(async ({ page }) => {
-	await page.goto('/profile/login');
+	await page.goto('/profile/register');
 });
 
 testWithCreatableUsername(
@@ -19,6 +19,26 @@ testWithCreatableUsername(
 			page.locator('button', { hasText: 'Submit' }).click()
 		]);
 
-        
+        const messages = page.locator('ul[data-test-id=modal-messages-list] li');
+		expect(await messages.allTextContents()).toStrictEqual(['Account created successfully']);
+	}
+);
+
+testWithExistingUsername(
+	'should throw error (User already exists)',
+	async ({ page, username }) => {
+		await page.fill('input[placeholder=Username]', username);
+		await page.fill('input[placeholder=Password]', 'password');
+        await page.fill('input[placeholder="Confirm Password"]', 'password');
+
+        await Promise.all([
+			page.waitForResponse(
+				(response) => response.url().includes('/api/auth/register') && response.status() === 409
+			),
+			page.locator('button', { hasText: 'Submit' }).click()
+		]);
+
+        const messages = page.locator('ul[data-test-id=modal-messages-list] li');
+		expect(await messages.allTextContents()).toStrictEqual(['User already exists']);
 	}
 );
