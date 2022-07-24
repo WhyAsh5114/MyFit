055f45d5e34fbbf86295a4880a8271c01b23ce27
WhyAsh5114 <whyash5114@gmail.com>
2022-07-24 19:43:55 +0530

    simplified login and register links allows for better redirects

---
+++ src/lib/LoginButton.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    export let classes = '';
+</script>
+
+<a href="/profile/login?page={$page.url.pathname}" class={classes}>Login</a>
---
+++ src/lib/RegisterButton.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    export let classes = '';
+</script>
+
+<a href="/profile/register?page={$page.url.pathname}" class={classes}>Register</a>
