62398ff67887174df5735f6db5442d6bf0fb2f34
WhyAsh5114 <whyash5114@gmail.com>
2022-07-25 14:59:13 +0530

    changed menu button made it more simple for the homepage

--- src/lib/MenuButton.svelte
+++ src/lib/MenuButton.svelte
@@ -1,12 +1,11 @@
 <script lang="ts">
 	export let title: string;
-	export let description: string;
 	export let link: string;
 	export let imagePath: string;
 </script>
 
 <a
-	class="card cursor-pointer w-full bg-primary hover:bg-neutral shadow-xl transition-colors"
+	class="card btn normal-case cursor-pointer w-full bg-primary hover:bg-neutral shadow-xl transition-colors h-fit"
 	href={link}
 >
 	<figure class="pt-4 md:pt-7 lg:pt-10 w-full">
@@ -14,6 +13,5 @@
 	</figure>
 	<div class="card-body p-2.5 sm:p-3 md:p-3.5 items-center text-center">
 		<h2 class="card-title text-lg lg:text-xl">{title}</h2>
-		<p class="text-sm lg:text-lg">{description}</p>
 	</div>
 </a>
