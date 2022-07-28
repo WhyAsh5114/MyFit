0ec549693009588a866eb70473d088ed5a100f43
WhyAsh5114 <whyash5114@gmail.com>
2022-07-28 18:02:42 +0530

    added setUser function to modify userData

--- src/routes/api/_db.ts
+++ src/routes/api/_db.ts
@@ -9,43 +9,54 @@ export const getUser = async (username: string): Promise<UserData> => {
 	const existingUser = await db.get(username);
 	if (!existingUser) {
 		return Promise.reject(new Error('User does not exist'));
-	} else {
-		const user: User = JSON.parse(existingUser);
-		delete user.password;
-		return Promise.resolve(user);
 	}
+	const user: User = JSON.parse(existingUser);
+	delete user.password;
+	return Promise.resolve(user);
 };
+
+export const setUser = async (userData: UserData, session: string): Promise<string> => {
+	const existingUser = await db.get(userData.username);
+	// Make sure user exists
+	if (!existingUser) {
+		return Promise.reject(new Error('User does not exist'));
+	}
+	// Reject if session is unauthorized (expired/corrupted)
+	if (await getUsernameFromSession(session) !== userData.username) {
+		return Promise.reject(new Error('Unauthorized session'));
+	}
+	await db.set(userData.username, JSON.stringify(userData));
+	return Promise.resolve('User set successfully');
+}
 
 export const registerUser = async (credentials: AccountDetails): Promise<UserData> => {
 	const existingUser = await db.get(credentials.username);
 	if (existingUser) {
 		return Promise.reject(new Error('User already exists'));
-	} else {
-		const hashedPassword = await hash(credentials.password, 10);
-		const newUser: User = {
-			username: credentials.username,
-			password: hashedPassword,
-			splits: {}
-		};
-
-		await db.set(newUser.username, JSON.stringify(newUser));
-		delete newUser.password;
-		return Promise.resolve(newUser);
 	}
+	const hashedPassword = await hash(credentials.password, 10);
+	const newUser: User = {
+		username: credentials.username,
+		password: hashedPassword,
+		splits: {}
+	};
+
+	await db.set(newUser.username, JSON.stringify(newUser));
+	delete newUser.password;
+	return Promise.resolve(newUser);
 };
 
 export const loginUser = async ({ username, password }: AccountDetails): Promise<string> => {
 	const userData = await db.get(username);
 	if (!userData) {
 		return Promise.reject(new Error('User does not exist'));
+	}
+	const existingUser: User = JSON.parse(userData);
+	if (existingUser.password && (await compare(password, existingUser.password))) {
+		const sessionID = await createSession(username);
+		return Promise.resolve(sessionID);
 	} else {
-		const existingUser: User = JSON.parse(userData);
-		if (existingUser.password && (await compare(password, existingUser.password))) {
-			const sessionID = await createSession(username);
-			return Promise.resolve(sessionID);
-		} else {
-			return Promise.reject(new Error('Incorrect password'));
-		}
+		return Promise.reject(new Error('Incorrect password'));
 	}
 };
 
@@ -60,17 +71,15 @@ export const getUsernameFromSession = async (sessionID: string): Promise<string>
 	const username = await db.get(sessionID);
 	if (!username) {
 		return Promise.reject(new Error('User does not exist'));
-	} else {
-		return Promise.resolve(username);
 	}
+	return Promise.resolve(username);
 };
 
 export const removeSession = async (sessionID: string): Promise<string> => {
 	const username = await db.get(sessionID);
 	if (!username) {
 		return Promise.reject(new Error('Session not found'));
-	} else {
-		await db.del(sessionID);
-		return Promise.resolve(sessionID);
 	}
+	await db.del(sessionID);
+	return Promise.resolve(sessionID);
 };
