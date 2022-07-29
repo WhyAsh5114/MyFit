91dab731e44a08d794c1fc3f5419727e7a4cb4e6
WhyAsh5114 <whyash5114@gmail.com>
2022-07-29 18:25:34 +0530

    added logging and records page

---
+++ src/routes/logging/index.svelte
@@ -0,0 +1,40 @@
+<script>
+    import MenuButtonGrid from '$lib/MenuButtonGrid.svelte';
+    import ListButton from '$lib/ListButton.svelte';
+</script>
+
+<svelte:head>
+    <title>MyFit | Logging</title>
+</svelte:head>
+<MenuButtonGrid>
+    <ListButton
+        imagePath="/dumbbell.png"
+        title="Log workout"
+        description="Reps, sets, load, duration, calories burned"
+        link="/logging/workouts"
+    />
+    <ListButton
+        imagePath="/heart.svg"
+        title="Log cardio"
+        description="Duration, calories burned, average heart rate"
+        link="/logging/cardio"
+    />
+    <ListButton
+        imagePath="/moon.svg"
+        title="Sleep records"
+        description="Duration, quality, tiredness, wake up time"
+        link="/logging/sleep"
+    />
+    <ListButton
+        imagePath="/bowl.svg"
+        title="Nutritional records"
+        description="Calorie intake, macronutrients, foods"
+        link="/logging/nutrition"
+    />
+    <ListButton
+        imagePath="/scale.svg"
+        title="Nutritional records"
+        description="Steps, weight, height, fat percentage"
+        link="/logging/bodystats"
+    />
+</MenuButtonGrid>
---
+++ src/routes/records/index.svelte
@@ -0,0 +1,46 @@
+<script>
+    import MenuButtonGrid from "$lib/MenuButtonGrid.svelte";
+    import ListButton from "$lib/ListButton.svelte";
+</script>
+<svelte:head>
+    <title>MyFit | Records</title>
+</svelte:head>
+
+<MenuButtonGrid>
+    <ListButton
+        imagePath="/calendar.svg"
+        title="Splits records"
+        description="Check and modify created splits"
+        link="/records/splits"
+    />
+    <ListButton
+        imagePath="/dumbbell.png"
+        title="Workout records"
+        description="Check and modify performed workouts"
+        link="/records/workouts"
+    />
+    <ListButton
+        imagePath="/heart.svg"
+        title="Cardio records"
+        description="Check and modify performed cardio workouts"
+        link="/records/cardio"
+    />
+    <ListButton
+        imagePath="/moon.svg"
+        title="Sleep records"
+        description="Check and edit sleep logs"
+        link="/records/sleep"
+    />
+    <ListButton
+        imagePath="/bowl.svg"
+        title="Nutritional records"
+        description="Check and edit nutrition logs"
+        link="/records/nutrition"
+    />
+    <ListButton
+        imagePath="/scale.svg"
+        title="Nutritional records"
+        description="Check and edit body metrics"
+        link="/records/bodystats"
+    />
+</MenuButtonGrid>
