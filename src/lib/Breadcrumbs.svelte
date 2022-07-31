77a400170aa61a832e06663b71856e870ff8ab9c
WhyAsh5114 <whyash5114@gmail.com>
2022-07-31 15:55:10 +0530

    added border to breadcrumbs

--- src/lib/Breadcrumbs.svelte
+++ src/lib/Breadcrumbs.svelte
@@ -1,5 +1,5 @@
 <div
-    class="text-sm breadcrumbs flex flex-col shrink-0 place-items-center px-2 rounded-lg font-semibold w-full mb-2"
+    class="text-sm breadcrumbs flex flex-col shrink-0 place-items-center px-2 rounded-lg font-semibold w-full mb-2 border-2 border-secondary"
 >
     <slot />
 </div>
