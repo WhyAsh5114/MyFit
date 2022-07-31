9a5d6824d62e2a90c56e80aa71225e7ed735f65d
WhyAsh5114 <whyash5114@gmail.com>
2022-07-31 15:40:30 +0530

    UI change

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -66,6 +66,6 @@
         </ul>
     </div>
 </div>
-<div class="flex flex-col justify-center place-items-center px-2 pt-0.5 pb-3 flex-grow">
+<div class="flex flex-col place-items-center px-2 pt-0.5 pb-3 flex-grow">
     <slot />
 </div>
