82bdc5348baee0f327baa175194e8727ed358ca2
WhyAsh5114 <whyash5114@gmail.com>
2022-09-10 15:18:02 +0530

    new conventions for getUserRoute

---
+++ src/routes/api/auth/getUserData/+server.ts
@@ -0,0 +1,23 @@
+import { ErrorResponse, getUser, loginUser } from '../../_db';
+import type { RequestHandler } from '@sveltejs/kit';
+
+export const POST: RequestHandler = async ({ request }) => {
+    const body: { username: string; password: string } = await request.json();
+
+    // Try logging in the user
+    try {
+        await loginUser(body);
+        return new Response(JSON.stringify(await getUser(body.username)), {
+            status: 200
+        });
+    } catch (err) {
+        if (err instanceof ErrorResponse) {
+            return new Response(err.message, {
+                status: err.status
+            })
+        }
+        return new Response(JSON.stringify(err), {
+            status: 500
+        })
+    }
+};
