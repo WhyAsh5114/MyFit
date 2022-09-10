e01bb04631e7815c680ce964980f07587797e653
WhyAsh5114 <whyash5114@gmail.com>
2022-09-10 12:39:02 +0530

    added user fetching to base layout

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,14 +1,22 @@
 import type { ServerLoad } from '@sveltejs/kit';
+import { getUser, getUsernameFromSession } from './api/_db';
+import { parse } from 'cookie';
 
 // If user is logged in, don't show Logout option
-export const load: ServerLoad = ({ locals }) => {
-    if (locals?.user) {
-        return {
-            props: {
-                user: locals.user
+export const load: ServerLoad = async ({ request }) => {
+    const cookies = parse(request.headers.get('cookie') || '');
+    if (!cookies.sessionID) {
+        return {};
+    }
+
+    const username = await getUsernameFromSession(cookies.sessionID);
+    try {
+        if (username) {
+            return {
+                user: await getUser(username)
             }
-        };
-    } else {
+        }
+    } catch (err) {
         return {};
     }
 };
