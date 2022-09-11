0c2f774cfaec2e7625401de0f044166117d3b70c
WhyAsh5114 <whyash5114@gmail.com>
2022-09-11 13:17:39 +0530

    lint

--- src/routes/profile/+page.ts
+++ src/routes/profile/+page.ts
@@ -2,7 +2,7 @@ import { redirect, type Load } from '@sveltejs/kit';
 
 export const load: Load = async ({ parent }) => {
     const { user } = await parent();
-    
+
     if (user) {
         return { user };
     } else {
