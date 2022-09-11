cf4b4ecb36d28134d471dcc07750bd644b12029d
WhyAsh5114 <whyash5114@gmail.com>
2022-09-11 13:16:59 +0530

    always returns a user property fixed await parent() data types, also logs out if session has expired or is invalid

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -4,17 +4,22 @@ import { getUser, getUsernameFromSession } from './api/_db';
 export const load: LayoutServerLoad = async ({ cookies }) => {
     const sessionID = cookies.get('sessionID');
     if (!sessionID) {
-        return {};
+        return { user: undefined };
     }
 
-    const username = await getUsernameFromSession(sessionID);
     try {
+        const username = await getUsernameFromSession(sessionID);
         if (username) {
             const user = await getUser(username);
             delete user.password;
             return { user };
+        } else {
+            await fetch('/api/aut/logout', {
+                method: 'GET'
+            });
+            return { user: undefined }
         }
     } catch (err) {
-        return {};
+        return { user: undefined };
     }
 };
