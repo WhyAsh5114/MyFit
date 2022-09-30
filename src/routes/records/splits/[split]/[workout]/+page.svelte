c1c76f8cb602da9a889fcd77ed076e3558890b83
WhyAsh5114 <whyash5114@gmail.com>
2022-09-30 23:12:23 +0530

    created workout page (in split records)

---
+++ src/routes/records/splits/[split]/[workout]/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { page } from "$app/stores";
+    import ExerciseTable from "$lib/ExerciseTable.svelte";
+
+    const workout: Exercise[] = $page.data.user?.splits[$page.params.split].splitWorkouts[$page.params.workout] as Exercise[];
+    console.log(workout)
+</script>
+
+<ExerciseTable workoutName={$page.params.workout} exercises={workout} />
+<button class="footer-button">Modify split workout</button>
