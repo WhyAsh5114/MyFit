050e68bb486517feb38bd3fa6d9fddfe7c228099
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-02 18:30:21 +0530

    added Roboto font

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,13 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: ['./src/**/*.{html,js,svelte,ts}'],
     theme: {
-        extend: {}
+        extend: {},
+        fontFamily: {
+            'sans': ['Roboto', ...defaultTheme.fontFamily.sans]
+        }
     },
     plugins: [require('daisyui')],
     daisyui: {
