43b0441ec7a9beebb8d16a1c22aa7dc3462b49fd
WhyAsh5114 <whyash5114@gmail.com>
2022-10-02 13:15:29 +0530

    saves workout changes to store

--- src/routes/records/splits/[split]/[workout]/+page.svelte
+++ src/routes/records/splits/[split]/[workout]/+page.svelte
@@ -1,10 +1,36 @@
 <script lang="ts">
-    import { page } from "$app/stores";
-    import ExerciseTable from "$lib/ExerciseTable.svelte";
+    import { goto } from '$app/navigation';
+    import { page } from '$app/stores';
+    import ExerciseTable from '$lib/ExerciseTable.svelte';
+    import MyModal from '$lib/MyModal.svelte';
+    import { EditedWorkouts } from '../editSplitStore';
 
-    const workout: Exercise[] = $page.data.user?.splits[$page.params.split].splitWorkouts[$page.params.workout] as Exercise[];
-    console.log(workout)
+    let modalOpen = false;
+    let modalTexts = [''];
+
+    let workout: Exercise[] = JSON.parse(
+        JSON.stringify(
+            $page.data.user?.splits[$page.params.split].splitWorkouts[$page.params.workout]
+        )
+    ) as Exercise[];
+    if ($page.params.workout in $EditedWorkouts) {
+        workout = $EditedWorkouts[$page.params.workout];
+    }
+    const original_workout = JSON.parse(JSON.stringify(workout));
+
+    async function modifyWorkout() {
+        if (workout.length === 0) {
+            modalTexts = ["Should have at least one exercise"];
+            modalOpen = true;
+            return;
+        }
+        if (original_workout !== workout) {
+            $EditedWorkouts[$page.params.workout] = workout;
+            await goto(`/records/splits/${$page.params.split}`);
+        }
+    }
 </script>
 
-<ExerciseTable workoutName={$page.params.workout} exercises={workout} />
-<button class="footer-button">Modify split workout</button>
+<MyModal {modalTexts} modalTitle="Error" bind:modalOpen />
+<ExerciseTable workoutName={$page.params.workout} bind:exercises={workout} />
+<button class="footer-button" on:click={modifyWorkout}>Modify split workout</button>
