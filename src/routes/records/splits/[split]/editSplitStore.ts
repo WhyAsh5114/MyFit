03b395b9c041737fb54198cdf2864638e5c63145
WhyAsh5114 <whyash5114@gmail.com>
2022-10-02 13:15:07 +0530

    new store to save modified split schedule

--- src/routes/records/splits/[split]/editSplitStore.ts
+++ src/routes/records/splits/[split]/editSplitStore.ts
@@ -1,7 +1,7 @@
 import { type Writable, writable } from 'svelte/store';
 
 export const CurrentSplit: Writable<Split> = writable();
-
+export const SplitSchedule: Writable<string[]> = writable();
 export const EditingWorkoutName: Writable<string> = writable();
 export const EditingWorkout: Writable<Exercise[]> = writable();
 export const EditedWorkouts: Writable<Record<string, Exercise[]>> = writable({});
