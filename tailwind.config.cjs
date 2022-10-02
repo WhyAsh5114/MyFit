2fb958e80dcfcd36bd5ccc6ab0a5832dcdc2a021
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-02 17:14:50 +0530

    lint

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,4 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -6,7 +6,7 @@ module.exports = {
     theme: {
         extend: {},
         fontFamily: {
-            'sans': ['Roboto', ...defaultTheme.fontFamily.sans]
+            sans: ['Roboto', ...defaultTheme.fontFamily.sans]
         }
     },
     plugins: [require('daisyui')],
