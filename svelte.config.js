96723ca05f25b2f10ca1a70c47167d0bea4e0e83
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-07 20:19:34 +0530

    added preprocessor to dynamically import files in markup itself

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
 import preprocess from 'svelte-preprocess';
+import importAssets from 'svelte-preprocess-import-assets'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
     // Consult https://github.com/sveltejs/svelte-preprocess
     // for more information about preprocessors
     preprocess: [
+        importAssets({ sources: (defaultSources) => {
+            return [
+                ...defaultSources,
+                {
+                    tag: 'HomepageButton',
+                    srcAttributes: ['imagePath']
+                }
+            ]
+        }}),
         preprocess({
             postcss: true
         })
