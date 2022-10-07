3c221db6e0c2d742de21a1950ea4717922713c5e
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-07 20:40:03 +0530

    updated MenuButton to use slot

--- src/routes/splits/+page.svelte
+++ src/routes/splits/+page.svelte
@@ -13,27 +13,27 @@
 </div>
 <div class="menu-button-grid">
     <MenuButton
-        imagePath="/calendar_plus.svg"
         title="New split"
         description="Create a weekly routine from scratch"
         link="/splits/new"
-    />
+    >
+        <img src="$lib/assets/calendar_plus.svg" alt="" class="responsive-image-menu-button" />
+    </MenuButton>
+    <MenuButton title="Common splits" description="Adjust and use common splits" link="/">
+        <img src="$lib/assets/calendar_PPL.svg" alt="" class="responsive-image-menu-button" />
+    </MenuButton>
     <MenuButton
-        imagePath="/calendar_PPL.svg"
-        title="Common splits"
-        description="Adjust and use common splits"
-        link="/"
-    />
-    <MenuButton
-        imagePath="/calendar_gear.svg"
         title="Modify split"
         description="Modify current split's exercises, progression"
         link="/"
-    />
+    >
+        <img src="$lib/assets/calendar_gear.svg" alt="" class="responsive-image-menu-button" />
+    </MenuButton>
     <MenuButton
-        imagePath="/record.svg"
         title="Split records"
         description="View, copy and adjust previous splits"
         link="/records/splits"
-    />
+    >
+        <img src="$lib/assets/record.svg" alt="" class="responsive-image-menu-button" />
+    </MenuButton>
 </div>
