6bb7aca42f5e2318890956bf2ba04223f916fd8b
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-07 20:45:12 +0530

    updated menubutton to use slot

--- src/routes/logging/+page.svelte
+++ src/routes/logging/+page.svelte
@@ -7,33 +7,38 @@
 </svelte:head>
 <div class="menu-button-grid">
     <MenuButton
-        imagePath="/dumbbell.png"
         title="Log workout"
         description="Reps, sets, load, duration, calories burned"
         link="/logging/workouts"
-    />
+    >
+        <img src="$lib/assets/dumbbell.svg" alt="" class="responsive-image-menu-button" />
+    </MenuButton>
     <MenuButton
-        imagePath="/heart.svg"
         title="Log cardio"
         description="Duration, calories burned, average heart rate"
         link="/logging/cardio"
-    />
+    >
+        <img src="$lib/assets/heart.svg" alt="" class="responsive-image-menu-button" />
+    </MenuButton>
     <MenuButton
-        imagePath="/moon.svg"
         title="Sleep records"
         description="Duration, quality, tiredness, wake up time"
         link="/logging/sleep"
-    />
+    >
+        <img src="$lib/assets/moon.svg" alt="" class="responsive-image-menu-button" />
+    </MenuButton>
     <MenuButton
-        imagePath="/bowl.svg"
         title="Nutritional records"
         description="Calorie intake, macronutrients, foods"
         link="/logging/nutrition"
-    />
+    >
+        <img src="$lib/assets/bowl.svg" alt="" class="responsive-image-menu-button" />
+    </MenuButton>
     <MenuButton
-        imagePath="/scale.svg"
         title="Nutritional records"
         description="Steps, weight, height, fat percentage"
         link="/logging/bodystats"
-    />
+    >
+        <img src="$lib/assets/scale.svg" alt="" class="responsive-image-menu-button" />
+    </MenuButton>
 </div>
