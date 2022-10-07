b38d4984b8ddae369a14d8eed06c3ac5919def8a
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-07 20:38:54 +0530

    replaced img with slot

--- src/lib/MenuButton.svelte
+++ src/lib/MenuButton.svelte
@@ -2,7 +2,6 @@
     export let title: string;
     export let description: string;
     export let link: string;
-    export let imagePath: string;
 </script>
 
 <a
@@ -10,7 +9,7 @@
     href={link}
 >
     <div class="flex w-1/3 lg:w-2/5 justify-center items-center py-4">
-        <img src={imagePath} alt={imagePath} class="w-16 md:w-20 lg:w-24" />
+        <slot />
     </div>
     <div class="flex flex-col justify-center p-2.5 sm:p-3 md:p-3.5 w-2/3 lg:w-3/5">
         <h2 class="card-title text-lg md:text-xl lg:text-2xl">{title}</h2>
