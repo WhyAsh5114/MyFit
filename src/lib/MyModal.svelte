b88b41ecda22d860b42c792e3f5ea1f7d4c96c6b
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-08 21:24:29 +0530

    accessibility fix

--- src/lib/MyModal.svelte
+++ src/lib/MyModal.svelte
@@ -22,7 +22,7 @@
     }
 </script>
 
-<input type="checkbox" id={modalName} class="modal-toggle" bind:checked={modalOpen} />
+<input type="checkbox" id={modalName} class="modal-toggle" aria-label="modal-toggle" bind:checked={modalOpen} />
 <label
     for={modalName}
     class="modal modal-bottom md:modal-middle cursor-pointer"
