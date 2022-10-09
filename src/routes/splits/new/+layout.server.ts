cd0a9fd78026d8bc317a4c3b6ba0b6ade3786d34
Yash Kolekar <71895020+WhyAsh5114@users.noreply.github.com>
2022-10-10 00:30:11 +0530

    Update +layout.server.ts
    Added some TODOs from mobile

--- src/routes/splits/new/+layout.server.ts
+++ src/routes/splits/new/+layout.server.ts
@@ -1,6 +1,11 @@
 import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
 
+// TODO: all fetch requests from client (for example saving a split), should have a loader after sending a reauest
+// To prevent double requesting in real world scenario where database requests aren't instantaneous
+// Also might want to use redis cloud for this project?
+
+
 export const load: LayoutServerLoad = async ({ parent }) => {
     const { user } = await parent();
 
