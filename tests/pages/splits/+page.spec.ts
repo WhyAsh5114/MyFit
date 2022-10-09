848dcae33afd683f0c3a431c218ed61ca890f90a
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-09 21:25:15 +0530

    bugfix old implementation rarely causes a race condition, so fixed it

--- tests/pages/splits/+page.spec.ts
+++ tests/pages/splits/+page.spec.ts
@@ -5,27 +5,35 @@ test.beforeEach(async ({ page }) => {
 });
 
 test('should redirect to login page "New split"', async ({ page }) => {
-    await page.locator('h2', { hasText: 'New split' }).click();
-    await page.waitForNavigation();
+    await Promise.all([
+        page.locator('h2', { hasText: 'New split' }).click(),
+        page.waitForNavigation()
+    ])
     expect(page.url()).toContain('/profile/login');
 });
 
 test('should redirect to login page "Split records"', async ({ page }) => {
-    await page.locator('h2', { hasText: 'Split records' }).click();
-    await page.waitForNavigation();
+    await Promise.all([
+        page.locator('h2', { hasText: 'Split records' }).click(),
+        page.waitForNavigation()
+    ])
     expect(page.url()).toContain('/profile/login');
 });
 
 test('should redirect to "/splits/new" if logged in "New split"', async ({ loggedInPage }) => {
-    await loggedInPage.locator('h2', { hasText: 'New split' }).click();
-    await loggedInPage.waitForNavigation();
+    await Promise.all([
+        loggedInPage.locator('h2', { hasText: 'New split' }).click(),
+        loggedInPage.waitForNavigation()
+    ])
     expect(loggedInPage.url()).toContain('/splits/new');
 });
 
 test('should redirect to "/records/splits" if logged in "Split records"', async ({
     loggedInPage
 }) => {
-    await loggedInPage.locator('h2', { hasText: 'Split records' }).click();
-    await loggedInPage.waitForNavigation();
+    await Promise.all([
+        loggedInPage.locator('h2', { hasText: 'Split records' }).click(),
+        loggedInPage.waitForNavigation()
+    ])
     expect(loggedInPage.url()).toContain('/records/splits');
 });
