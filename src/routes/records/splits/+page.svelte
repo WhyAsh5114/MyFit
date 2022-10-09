8bec4a9ff99e2b401f567f21edb2085797830aa2
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-09 21:24:21 +0530

    UI changes centered create split

--- src/routes/records/splits/+page.svelte
+++ src/routes/records/splits/+page.svelte
@@ -25,25 +25,33 @@
         <li>Splits</li>
     </ul>
 </div>
-<div class="flex flex-col w-full max-w-md mt-2 px-3 gap-2 h-px flex-auto overflow-y-auto">
-    {#each splits as split}
-        <a
-            class="flex w-full bg-primary rounded-lg p-3 active:scale-95 hover:bg-opacity-50 transition-all border-2 {split.name ===
-            user?.activeSplit
-                ? 'border-accent'
-                : 'border-base-100'}"
-            in:scale
-            href="/records/splits/{split.name}"
-            on:click={() => clearStores(split.name)}
-        >
-            <h2 class="text-lg font-semibold overflow-hidden text-ellipsis">{split.name}</h2>
-            <h3 class="ml-auto basis-28 text-right shrink-0">
-                {getFormattedDate(split.timeCreated)}
-            </h3>
-        </a>
-    {/each}
+<div class="flex flex-col w-full max-w-md px-3 h-px flex-auto overflow-y-auto">
+    <ul data-test-id="splits-list" class="my-auto">
+        {#each splits as split}
+            <a
+                class="flex w-full bg-primary rounded-lg p-3 my-1.5 active:scale-95 hover:bg-opacity-50 transition-all border-2 {split.name ===
+                user?.activeSplit
+                    ? 'border-accent'
+                    : 'border-base-100'}"
+                in:scale
+                href="/records/splits/{split.name}"
+                on:click={() => clearStores(split.name)}
+            >
+                <h2 class="text-lg font-semibold overflow-hidden text-ellipsis">{split.name}</h2>
+                <h3 class="ml-auto basis-28 text-right shrink-0">
+                    {getFormattedDate(split.timeCreated)}
+                </h3>
+            </a>
+        {/each}
+    </ul>
     {#if splits.length === 0}
-        <h2 class="text-center">No split created</h2>
-        <a href="/splits/new" class="btn btn-primary w-44 normal-case">Create split</a>
+        <div class="flex flex-grow flex-col justify-center items-center gap-3">
+            <h2 class="text-center" data-test-id="no-split-label">No split created</h2>
+            <a
+                href="/splits/new"
+                class="btn btn-primary w-44"
+                data-test-id="create-split-button">Create split</a
+            >
+        </div>
     {/if}
 </div>
