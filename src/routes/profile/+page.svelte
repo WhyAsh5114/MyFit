76fd9cd4373122bf77304ac389530302318f984d
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-15 13:30:03 +0530

    added some test IDs for testing

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -6,11 +6,11 @@
     <title>MyFit | Profile</title>
 </svelte:head>
 <div class="flex flex-col w-full place-items-center h-full justify-center gap-3">
-    <b>Hi {$page.data.user.username}</b>
-    {#if $page.data.user.activeSplit}
-        <p>Active split: {$page.data.user.activeSplit}</p>
+    <b>Hi {$page.data.user?.username}</b>
+    {#if $page.data.user?.activeSplit}
+        <p>Active split: <b data-test-id="active-split">{$page.data.user?.activeSplit}</b></p>
     {:else}
-        <p>No active split</p>
+        <p data-test-id="no-active-split">No active split</p>
     {/if}
     <a
         class="btn btn-sm normal-case btn-error w-fit"
