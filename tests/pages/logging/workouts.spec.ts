05ed07bc8a8c7a6993c91db6f35f6ba23c174dad
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-27 19:26:59 +0530

    added a basic test i think i'll stop testing now, feels very boring

---
+++ tests/pages/logging/workouts.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from '../../fixtures.js';
+
+test("should display today's workout type", async ({ splitCreatedPage, split }) => {
+    const page = splitCreatedPage;
+    await page.goto('/logging/workouts');
+
+    const todaysWorkout = page.locator('[data-test-id=todays-workout]');
+    await expect(todaysWorkout).toHaveText(split.schedule.at(new Date().getDay() - 1) as string);
+});
+
+test('should disable split template button if Rest or no active split', async ({ page }) => {
+    await page.goto('/logging/workouts');
+
+    
+})
