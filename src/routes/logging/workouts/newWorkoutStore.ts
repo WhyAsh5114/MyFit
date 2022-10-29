3b35fb496449913fa721610dc68b96bb5b63b022
WhyAsh5114 <whyashplaysdota@gmail.com>
2022-10-29 10:38:33 +0530

    added some extra stores for more consistent behavior

--- src/routes/logging/workouts/newWorkoutStore.ts
+++ src/routes/logging/workouts/newWorkoutStore.ts
@@ -3,3 +3,6 @@ import { writable, type Writable } from 'svelte/store';
 export const WorkoutName: Writable<string> = writable();
 export const WorkoutExercises: Writable<Exercise[]> = writable();
 export const WorkoutCreatedDate: Writable<Date> = writable();
+export const OldWorkoutExercises: Writable<Exercise[]> = writable();
+export const SetSplit: Writable<string> = writable();
+export const SetWorkoutType: Writable<string> = writable();
