93f5ae5eb612fd6922de21b8416525b7203c9f6c
WhyAsh5114 <whyash5114@gmail.com>
2023-06-03 23:23:59 +0530

    updated sw spec according to vitePWA docs

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
 	plugins: [
 		sveltekit(),
 		SvelteKitPWA({
-			srcDir: './src',
+			srcDir: 'src',
 			registerType: 'prompt',
 			strategies: 'injectManifest',
 			scope: '/',
