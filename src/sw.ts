abe4cce2563c622ad39ad15fcd7deb6ec25c15cc
WhyAsh5114 <whyash5114@gmail.com>
2023-06-04 13:02:49 +0530

    updated SW according to SvelteKit standards

--- src/sw.ts
+++ src/sw.ts
@@ -1,14 +1,68 @@
-import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
+/// <reference types="@sveltejs/kit" />
+/// <reference types="@sveltejs/kit" />
+/// <reference no-default-lib="true"/>
+/// <reference lib="esnext" />
+/// <reference lib="webworker" />
 
-declare let self: ServiceWorkerGlobalScope;
+const sw = /** @type {ServiceWorkerGlobalScope} */ (/** @type {unknown} */ (self));
+import { build, files, version } from '$service-worker';
 
-cleanupOutdatedCaches();
+// Create a unique cache name for this deployment
+const CACHE = `cache-${version}`;
 
-self.addEventListener('message', (event) => {
-	if (event.data && event.data.type === 'SKIP_WAITING') self.skipWaiting();
+const ASSETS = [
+    ...build, // the app itself
+    ...files  // everything in `static`
+];
+
+sw.addEventListener('install', (event) => {
+    // Create a new cache and add all files to it
+    async function addFilesToCache() {
+        const cache = await caches.open(CACHE);
+        await cache.addAll(ASSETS);
+    }
+
+    event.waitUntil(addFilesToCache());
 });
 
-// self.__WB_MANIFEST is default injection point
-precacheAndRoute(self.__WB_MANIFEST);
+sw.addEventListener('activate', (event) => {
+    // Remove previous cached data from disk
+    async function deleteOldCaches() {
+        for (const key of await caches.keys()) {
+            if (key !== CACHE) await caches.delete(key);
+        }
+    }
 
-// TODO: actually create a meaningful service worker...
+    event.waitUntil(deleteOldCaches());
+});
+
+sw.addEventListener('fetch', (event) => {
+    // ignore POST requests etc
+    if (event.request.method !== 'GET') return;
+
+    async function respond() {
+        const url = new URL(event.request.url);
+        const cache = await caches.open(CACHE);
+
+        // `build`/`files` can always be served from the cache
+        if (ASSETS.includes(url.pathname)) {
+            return cache.match(url.pathname);
+        }
+
+        // for everything else, try the network first, but
+        // fall back to the cache if we're offline
+        try {
+            const response = await fetch(event.request);
+
+            if (response.status === 200) {
+                cache.put(event.request, response.clone());
+            }
+
+            return response;
+        } catch {
+            return cache.match(event.request);
+        }
+    }
+
+    event.respondWith(respond());
+});
