1212b939ea3a4524cd00d23d9f383d70c25e8f50
WhyAsh5114 <whyash5114@gmail.com>
2023-06-04 14:03:03 +0530

    removed fetch caching was causing some issues with OAuth

--- src/sw.ts
+++ src/sw.ts
@@ -35,34 +35,3 @@ sw.addEventListener('activate', (event) => {
 
     event.waitUntil(deleteOldCaches());
 });
-
-sw.addEventListener('fetch', (event) => {
-    // ignore POST requests etc
-    if (event.request.method !== 'GET') return;
-
-    async function respond() {
-        const url = new URL(event.request.url);
-        const cache = await caches.open(CACHE);
-
-        // `build`/`files` can always be served from the cache
-        if (ASSETS.includes(url.pathname)) {
-            return cache.match(url.pathname);
-        }
-
-        // for everything else, try the network first, but
-        // fall back to the cache if we're offline
-        try {
-            const response = await fetch(event.request);
-
-            if (response.status === 200) {
-                cache.put(event.request, response.clone());
-            }
-
-            return response;
-        } catch {
-            return cache.match(event.request);
-        }
-    }
-
-    event.respondWith(respond());
-});
