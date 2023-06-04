fd250ce0a17449a67b9b8c2513119bd3d2841109
WhyAsh5114 <whyash5114@gmail.com>
2023-06-04 12:38:35 +0530

    separated navbar component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
 	import '../app.postcss';
 	import { pwaInfo } from 'virtual:pwa-info';
-	import PWAButton from './PWAButton.svelte';
-	import { signIn, signOut } from '@auth/sveltekit/client';
-	import { page } from '$app/stores';
+	import Navbar from './Navbar.svelte';
 
 	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
 </script>
@@ -13,43 +11,7 @@
 	<title>MyFit</title>
 </svelte:head>
 
-<div class="navbar bg-primary">
-	<div class="flex-1">
-		<img src="/favicon.png" alt="" width="40" height="40" />
-		<a class="btn btn-ghost normal-case text-xl text-white" href="/">MyFit</a>
-	</div>
-	<div class="flex-none">
-		<div class="dropdown dropdown-end">
-			<button class="btn btn-ghost btn-circle avatar">
-				<div class="w-10 rounded-full">
-					{#if $page.data.session}
-						<img
-							src={$page.data.session.user?.image}
-							referrerpolicy="no-referrer"
-							alt="profile"
-							width="40"
-							height="40"
-						/>
-					{:else}
-						<img src="/profile.webp" alt="profile" />
-					{/if}
-				</div>
-			</button>
-			<ul
-				class="menu dropdown-content mt-3 p-2 shadow bg-secondary text-black font-semibold rounded-box"
-			>
-				<PWAButton />
-				<li>
-					<a> Profile </a>
-				</li>
-				<li><a>Settings</a></li>
-				{#if $page.data.session}
-					<li><button on:click={() => signOut()}>Logout</button></li>
-				{:else}
-					<li><button on:click={() => signIn()}>Login</button></li>
-				{/if}
-			</ul>
-		</div>
-	</div>
-</div>
-<slot />
+<Navbar />
+<main class="flex flex-col h-full justify-center items-center">
+	<slot />
+</main>
---
+++ src/routes/Navbar.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import '../app.postcss';
+	import PWAButton from './PWAButton.svelte';
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import { page } from '$app/stores';
+</script>
+
+<div class="navbar bg-primary">
+	<div class="flex-1">
+		<img src="/favicon.png" alt="" width="40" height="40" />
+		<a class="btn btn-ghost normal-case text-2xl text-white" href="/">MyFit</a>
+	</div>
+	<div class="flex-none">
+		<div class="dropdown dropdown-end">
+			<button class="btn btn-ghost btn-circle avatar">
+				<div class="w-10 rounded-full">
+					{#if $page.data.session}
+						<img
+							src={$page.data.session.user?.image}
+							referrerpolicy="no-referrer"
+							alt="profile"
+							width="40"
+							height="40"
+						/>
+					{:else}
+						<img src="/profile.webp" alt="profile" />
+					{/if}
+				</div>
+			</button>
+			<ul
+				class="menu dropdown-content mt-3 p-2 shadow bg-secondary text-black font-semibold rounded-box"
+			>
+				<PWAButton />
+				{#if $page.data.session}
+					<li><a href="/profile">Profile</a></li>
+					<li><a href="/settings">Settings</a></li>
+				{/if}
+				{#if $page.data.session}
+					<li><a href="/logout">Logout</a></li>
+				{:else}
+					<li><a href="/login">Login</a></li>
+				{/if}
+			</ul>
+		</div>
+	</div>
+</div>
