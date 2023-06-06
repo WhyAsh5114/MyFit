fe50d5e5e1b44d80bf375a13dfda7f045dc9e376
WhyAsh5114 <whyash5114@gmail.com>
2023-06-06 20:55:56 +0530

    added an onClose method for MyModal

--- src/lib/MyModal.svelte
+++ src/lib/MyModal.svelte
@@ -2,9 +2,17 @@
 	export let dialogElement: HTMLDialogElement;
 	export let title: string;
 	export let titleColor = 'text-white';
+	export let onClose: () => void = () => {
+		/* don't do anything on close by default */
+	};
 </script>
 
-<dialog id={title} class="modal modal-bottom sm:modal-middle" bind:this={dialogElement}>
+<dialog
+	id={title}
+	class="modal modal-bottom sm:modal-middle"
+	bind:this={dialogElement}
+	on:close={onClose}
+>
 	<form method="dialog" class="modal-box bg-primary text-white">
 		<button class="btn btn-sm btn-circle absolute right-2 top-2">✕</button>
 		<h3 class="font-bold text-lg {titleColor} mb-5">{title}</h3>
