7348ddfd5ebc6feda61dd352fa99f5d3e84384c4
WhyAsh5114 <whyash5114@gmail.com>
2023-06-06 16:08:15 +0530

    added SplitExercise type

---
+++ types/global.d.ts
@@ -0,0 +1,10 @@
+import type { commonMuscleGroups } from "$lib/commonDB";
+
+type SplitExercise = {
+	name: string | undefined;
+	sets: number | undefined;
+	setType: 'straight' | 'drop' | 'down' | 'top' | 'myorep' | 'myorep match' | 'giant' | undefined;
+	muscleTarget: (typeof commonMuscleGroups)[number] | undefined;
+	repRangeStart: number | undefined;
+	repRangeEnd: number | undefined;
+};
