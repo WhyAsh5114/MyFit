871617865d3c057f2b4ce7bda49296dc4e214dab
WhyAsh5114 <whyash5114@gmail.com>
2023-06-08 15:36:43 +0530

    throws error if meso doesn't exist

--- src/routes/mesocycles/view/[mesoIndex]/+page.server.ts
+++ src/routes/mesocycles/view/[mesoIndex]/+page.server.ts
@@ -4,16 +4,21 @@ import clientPromise from '$lib/mongodb';
 import type { Mesocycle } from '../../../../../types/global';
 
 export const load: PageServerLoad = async ({ locals, params }) => {
-    const session = await locals.getSession();
-    if (!session) {
-        throw error(403, 'Not logged in');
+	const session = await locals.getSession();
+	if (!session) {
+		throw error(403, 'Not logged in');
+	}
+
+	const client = await clientPromise;
+	const userData = await client.db().collection('users').findOne({ email: session.user?.email });
+	const meso = userData?.mesocycles[params.mesoIndex] as Mesocycle | null;
+
+    if (!meso) {
+		throw error(404, 'Mesocycle not found');
     }
 
-    const client = await clientPromise;
-    const userData = await client.db().collection('users').findOne({ email: session.user?.email });
-
-    return {
-        meso: userData?.mesocycles[params.mesoIndex] as Mesocycle | null,
-        ind: params.mesoIndex
-    }
-}
+	return {
+		meso,
+		ind: params.mesoIndex
+	};
+};
