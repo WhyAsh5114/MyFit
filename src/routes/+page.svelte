02dec3db8575bc4c1efd422a37837aef7b9d7aaf
WhyAsh5114 <whyash5114@gmail.com>
2023-06-08 14:09:34 +0530

    added loader to indicate loading mesos from db

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,10 @@
-<a href="/mesocycles" class="btn btn-primary">Mesocycles</a>
+<script>
+	import { navigating } from '$app/stores';
+</script>
+
+<a href="/mesocycles" class="btn btn-primary">
+	{#if $navigating?.to?.url.pathname === '/mesocycles'}
+		<span class="loading loading-spinner" />
+	{/if}
+	Mesocycles
+</a>
