b96364b51286e0e0911b73b2dc288275c48dc65b
WhyAsh5114 <whyash5114@gmail.com>
2023-06-08 13:44:06 +0530

    updates mesocycles page now shows previously made mesos

--- src/routes/mesocycles/+page.svelte
+++ src/routes/mesocycles/+page.svelte
@@ -1,1 +1,31 @@
-<a href="/mesocycles/new/basics" class="btn btn-primary">Create new mesocycle</a>
+<script>
+	export let data;
+</script>
+
+{#if data.mesocycles}
+	<ul class="flex flex-col gap-2 h-px grow w-full overflow-y-auto mb-2">
+		{#each data.mesocycles as meso}
+			<a
+				class="btn flex-col btn-primary normal-case rounded-lg w-full p-2 flex-nowrap h-fit gap-3"
+				href="/mesocycles/view/{meso.name}"
+			>
+				<div class="flex justify-between items-center w-full">
+					<h3 class="text-lg">{meso.name}</h3>
+					<p class="badge badge-secondary ml-auto">{meso.duration} weeks</p>
+				</div>
+				<div class="flex w-full gap-0.5 overflow-x-auto">
+					{#each meso.splitSchedule as workout}
+						{#if workout !== ''}
+							<p class="font-normal badge">{workout}</p>
+						{:else}
+							<p class="font-normal badge badge-accent badge-outline">Rest</p>
+						{/if}
+					{/each}
+				</div>
+			</a>
+		{/each}
+	</ul>
+{:else}
+	<p>You haven't created any mesocycles</p>
+{/if}
+<a href="/mesocycles/new/basics" class="btn btn-accent mt-auto btn-block">Create new mesocycle</a>
