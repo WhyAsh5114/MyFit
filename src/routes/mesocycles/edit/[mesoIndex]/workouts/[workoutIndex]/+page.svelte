e09e9d9181c87ed1e5469c2628df391d95539f62
WhyAsh5114 <whyash5114@gmail.com>
2023-06-08 22:53:40 +0530

    working on workout edit page

---
+++ src/routes/mesocycles/edit/[mesoIndex]/workouts/[workoutIndex]/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import SplitExercisesTable from '$lib/SplitExercisesTable.svelte';
+	import { days } from '$lib/commonDB.js';
+
+	export let data;
+</script>
+
+<SplitExercisesTable
+	bind:currentDay={days[data.workoutIndex]}
+	bind:workoutName={data.splitSchedule[data.workoutIndex]}
+    bind:splitExercises={data.splitExercises}
+/>
