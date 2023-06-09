1810c5d7ed25d20da3e8924bfbbb23a9d96aa18c
WhyAsh5114 <whyash5114@gmail.com>
2023-06-09 10:29:35 +0530

    added schema and server route for updating workout

---
+++ src/routes/api/mesocycles/updateWorkout/+server.ts
@@ -0,0 +1,54 @@
+import type { RequestHandler } from '@sveltejs/kit';
+import Ajv from 'ajv';
+import updateWorkoutSchema from './updateWorkoutSchema.json';
+import clientPromise from '$lib/mongodb';
+import type { SplitExercise } from '$lib/global';
+
+type reqBodyType = {
+	mesoIndex: number;
+	workoutIndex: number;
+	splitExercises: SplitExercise[];
+};
+
+const ajv = new Ajv({ removeAdditional: true });
+const validate = ajv.compile(updateWorkoutSchema);
+
+export const POST: RequestHandler = async ({ request, locals }) => {
+	const session = await locals.getSession();
+	if (!session) {
+		return new Response('Invalid session', {
+			status: 403
+		});
+	}
+
+	const body: reqBodyType = await request.json();
+	const valid = validate(body);
+	if (!valid) {
+		return new Response('Invalid JSON format updating workout', {
+			status: 400
+		});
+	}
+
+	const client = await clientPromise;
+	try {
+		await client
+			.db()
+			.collection('users')
+			.updateOne(
+				{ email: session.user?.email },
+				{
+					$set: {
+						[`mesocycles.${body.mesoIndex}.mesocycles.${body.workoutIndex}`]: body.splitExercises
+					}
+				}
+			);
+
+		return new Response('Mesocycle updated successfully', {
+			status: 200
+		});
+	} catch (e) {
+		return new Response(JSON.stringify(e), {
+			status: 500
+		});
+	}
+};
