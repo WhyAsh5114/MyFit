44afacd02b32f1506ae36293bb0afe9ca14faf17
WhyAsh5114 <whyash5114@gmail.com>
2023-07-13 15:51:33 +0530

    changed pageData to activeMesocycle

--- src/routes/workouts/+page.server.ts
+++ src/routes/workouts/+page.server.ts
@@ -1,7 +1,6 @@
 import type { PageServerLoad } from './$types';
 import clientPromise from '$lib/mongodb';
 import { error } from '@sveltejs/kit';
-import type { Mesocycle } from '$lib/global';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.getSession();
@@ -14,6 +13,6 @@ export const load: PageServerLoad = async ({ locals }) => {
 
 	return {
 		mesocycles: userData?.mesocycles as (Mesocycle | null)[] | null,
-		activeMesoIndex: userData?.activeMesoIndex as number | null
+		activeMesocycle: userData?.activeMesocycle as ActiveMesocycle | null
 	};
 };
