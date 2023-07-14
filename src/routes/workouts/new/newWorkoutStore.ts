9d26db9cab7438ffea3e834f5685b6a7a7f2210b
WhyAsh5114 <whyash5114@gmail.com>
2023-07-14 19:20:38 +0530

    added a reference workout store

--- src/routes/workouts/new/newWorkoutStore.ts
+++ src/routes/workouts/new/newWorkoutStore.ts
@@ -4,3 +4,4 @@ export const workoutDay: Writable<number> = writable();
 export const plannedRIR: Writable<number> = writable();
 export const muscleTargetsAndSets: Writable<Record<string, number>> = writable({});
 export const weekNumber: Writable<number> = writable();
+export const referenceWorkout: Writable<null | number> = writable(null);
