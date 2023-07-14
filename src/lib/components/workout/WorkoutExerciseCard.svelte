ca96dde601b29a1fb9b2a5b3b071e6038f560513
WhyAsh5114 <whyash5114@gmail.com>
2023-07-14 19:36:50 +0530

    removed edit function from workoutExercise card and sets display

--- src/lib/components/workout/WorkoutExerciseCard.svelte
+++ src/lib/components/workout/WorkoutExerciseCard.svelte
@@ -1,43 +1,27 @@
 <script lang="ts">
 	import { flip } from 'svelte/animate';
 	import { scale, slide } from 'svelte/transition';
-	import EditExerciseModal from '../EditExerciseModal.svelte';
 	import DeleteExerciseModal from '../DeleteExerciseModal.svelte';
 
-	export let splitExercises: SplitExercise[];
-
-	let editExerciseModal: HTMLDialogElement;
-	let currentlyEditingExerciseNumber: number;
-	let currentlyEditingExercise: SplitExercise;
-	function editExercise(i: number, exercise: SplitExercise) {
-		currentlyEditingExerciseNumber = i;
-		currentlyEditingExercise = exercise;
-		editExerciseModal.show();
-	}
+	export let workoutExercises: WorkoutExercise[];
 
 	let deleteExerciseModal: HTMLDialogElement;
 	let deletingExerciseName: string;
 	let indexOfExerciseToDelete: number;
 	function deleteExercise(i: number) {
-		deletingExerciseName = splitExercises[i].name as string;
+		deletingExerciseName = workoutExercises[i].name as string;
 		indexOfExerciseToDelete = i;
 		deleteExerciseModal.show();
 	}
 </script>
 
-<EditExerciseModal
-	bind:editExerciseModal
-	bind:splitExercises
-	bind:oldExercise={currentlyEditingExercise}
-	bind:i={currentlyEditingExerciseNumber}
-/>
 <DeleteExerciseModal
 	bind:deleteExerciseModal
 	bind:exerciseName={deletingExerciseName}
-	bind:splitExercises
+	bind:exercises={workoutExercises}
 	bind:indexOfExerciseToDelete
 />
-{#each splitExercises as exercise, i (exercise.name)}
+{#each workoutExercises as exercise, i (exercise.name)}
 	<li
 		class="flex flex-col bg-secondary w-full rounded-lg text-black p-3 h-fit"
 		animate:flip={{ duration: 200 }}
@@ -53,14 +37,6 @@
 				<ul class="menu dropdown-content p-2 shadow-black bg-base-100 rounded-md shadow-md">
 					<li>
 						<button
-							class="text-white uppercase font-semibold"
-							on:click={() => {
-								editExercise(i, exercise);
-							}}>Edit</button
-						>
-					</li>
-					<li>
-						<button
 							class="text-error uppercase font-semibold"
 							on:click={() => {
 								deleteExercise(i);
@@ -72,7 +48,6 @@
 		</div>
 		<h6 class="capitalize text-sm font-semibold italic">{exercise.setType} sets</h6>
 		<div class="flex justify-between mt-2.5 text-sm">
-			<p>{exercise.sets} sets of {exercise.repRangeStart} to {exercise.repRangeEnd} reps</p>
 			<span class="badge badge-error text-white">{exercise.muscleTarget}</span>
 		</div>
 	</li>
