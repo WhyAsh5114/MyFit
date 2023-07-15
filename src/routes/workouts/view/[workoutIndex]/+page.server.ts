6d836366ddba732ff066cdcde5e21ed6c50e825a
WhyAsh5114 <whyash5114@gmail.com>
2023-07-15 22:31:17 +0530

    added reference workout prop to pagedata

--- src/routes/workouts/view/[workoutIndex]/+page.server.ts
+++ src/routes/workouts/view/[workoutIndex]/+page.server.ts
@@ -13,14 +13,20 @@ export const load: PageServerLoad = async ({ locals, params }) => {
 	const client = await clientPromise;
 	const userData = await client.db().collection('users').findOne({ email: session.user?.email });
 	const workout = userData?.workouts[parseInt(params.workoutIndex)] as Workout | null;
+	
+	let referenceWorkout : null | undefined | Workout = undefined;
+	if (workout?.referenceWorkout) {
+		referenceWorkout = userData?.workouts[workout.referenceWorkout] as Workout | null;
+	}
 
 	if (!workout) {
 		throw error(404, 'Workout not found');
 	}
 
 	return {
-		workout: workout as Workout,
+		workout,
 		workoutIndex: parseInt(params.workoutIndex),
-		parentMesocycle: userData?.mesocycles[workout.mesoID] as Mesocycle | null
+		parentMesocycle: userData?.mesocycles[workout.mesoID] as Mesocycle | null,
+		referenceWorkout
 	};
 };
