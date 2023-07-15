34aafd6be56b811765b2edfda3552e3567dd5b9d
WhyAsh5114 <whyash5114@gmail.com>
2023-07-15 20:17:31 +0530

    added dateFormatter and UI for performedMesocycles

--- src/routes/workouts/+page.svelte
+++ src/routes/workouts/+page.svelte
@@ -4,18 +4,9 @@
 	export let data;
 
 	let selectedMesocycle: undefined | number;
-	let workoutsOfActiveMesocycleWithIndex: [Workout, number][] = [];
-
-	$: {
-		workoutsOfActiveMesocycleWithIndex = [];
-		if (selectedMesocycle && selectedMesocycle === data.activeMesocycle?.mesoID) {
-			data.activeMesocycle.workouts.forEach((workoutIndex) => {
-				if (data.workouts) {
-					workoutsOfActiveMesocycleWithIndex.push([data.workouts[workoutIndex], workoutIndex]);
-				}
-			});
-		}
-		workoutsOfActiveMesocycleWithIndex = workoutsOfActiveMesocycleWithIndex;
+	function dateFormatter(timestamp: number) {
+		const date = new Date(timestamp);
+		return date.toLocaleDateString(undefined, { day: '2-digit', month: 'short', year: 'numeric' });
 	}
 </script>
 
@@ -44,39 +35,62 @@
 	</div>
 </div>
 <ul class="grow w-full my-2 flex flex-col gap-2">
-	{#if workoutsOfActiveMesocycleWithIndex.length > 0 && data.activeMesocycle}
-		<h3 class="text-accent font-bold text-xl">
-			Active ({new Date(data.activeMesocycle?.startDate).toLocaleDateString()})
-		</h3>
-		{#each workoutsOfActiveMesocycleWithIndex as workout}
-			<a
-				class="btn relative flex-col btn-primary normal-case rounded-lg w-full p-2 flex-nowrap h-fit gap-1 items-start"
-				href="/workouts/view/{workout[1]}"
-			>
-				<h3 class="font-semibold text-left w-full text-base">
-					{new Date(workout[0].startTimestamp).toLocaleString()}
-				</h3>
-				{#if data.mesocycles && data.workouts}
-					<h4 class="font-normal text-base">
-						{data.mesocycles[workout[0].mesoID]?.splitSchedule[workout[0].dayNumber]}
-					</h4>
-				{/if}
-			</a>
-		{/each}
+	{#if data.activeMesocycle && data.activeMesocycle.workouts.length > 0 && data.activeMesocycle.mesoID === selectedMesocycle}
+		<div class="flex flex-col gap-1">
+			<h3 class="text-accent font-bold text-xl">
+				Active ({dateFormatter(data.activeMesocycle?.startDate)})
+			</h3>
+			{#each data.activeMesocycle.workouts.reverse() as workoutIndex}
+				<a
+					class="btn relative flex-col btn-primary normal-case rounded-lg w-full p-2 flex-nowrap h-fit gap-1 items-start"
+					href="/workouts/view/{workoutIndex}"
+				>
+					{#if data.mesocycles && data.workouts}
+						<h3 class="font-semibold text-left w-full text-base text-secondary">
+							{dateFormatter(data.workouts[workoutIndex].startTimestamp)}
+						</h3>
+						<h4 class="font-normal text-base">
+							{data.mesocycles[data.workouts[workoutIndex].mesoID]?.splitSchedule[
+								data.workouts[workoutIndex].dayNumber
+							]}
+						</h4>
+					{/if}
+				</a>
+			{/each}
+		</div>
 	{/if}
 
 	<!-- Older sections (with dates) -->
 	<!-- TODO: loop over data.performedMesocycles and if performedMesocycle.mesoID == selectedMesocycle -->
 	<!-- TODO: show section with heading of date range (startDate - endDate) -->
 	<!-- TODO: loop over the performedMesocycle.workouts and show them as cards -->
-	{#if data.performedMesocycles}
+	{#if data.performedMesocycles && data.workouts}
 		{#each data.performedMesocycles as performedMesocycle}
 			{#if performedMesocycle.mesoID === selectedMesocycle}
-				<h3 class="text-accent font-bold text-xl">
-					{new Date(performedMesocycle.startDate).toLocaleDateString()} - {new Date(
-						performedMesocycle.endDate
-					).toLocaleDateString()}
-				</h3>
+				<div class="flex flex-col gap-1">
+					<h3 class="font-bold text-xl">
+						{dateFormatter(performedMesocycle.startDate)} - {dateFormatter(
+							performedMesocycle.endDate
+						)}
+					</h3>
+					{#each performedMesocycle.workouts.reverse() as workoutIndex}
+						<a
+							class="btn relative flex-col btn-primary normal-case rounded-lg w-full p-2 flex-nowrap h-fit gap-1 items-start"
+							href="/workouts/view/{workoutIndex}"
+						>
+							<h3 class="font-semibold text-left w-full text-base text-secondary">
+								{dateFormatter(data.workouts[workoutIndex].startTimestamp)}
+							</h3>
+							{#if data.mesocycles && data.workouts}
+								<h4 class="font-normal text-base">
+									{data.mesocycles[data.workouts[workoutIndex].mesoID]?.splitSchedule[
+										data.workouts[workoutIndex].dayNumber
+									]}
+								</h4>
+							{/if}
+						</a>
+					{/each}
+				</div>
 			{/if}
 		{/each}
 	{/if}
