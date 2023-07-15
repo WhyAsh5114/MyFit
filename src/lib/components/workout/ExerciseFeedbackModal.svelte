4d40a463c5b2b1db68bb4cb1b2eb56757f963daa
WhyAsh5114 <whyash5114@gmail.com>
2023-07-15 12:14:24 +0530

    reduced UI redundancy and integrated feedback logic

--- src/lib/components/workout/ExerciseFeedbackModal.svelte
+++ src/lib/components/workout/ExerciseFeedbackModal.svelte
@@ -2,7 +2,41 @@
 	import MyModal from '../MyModal.svelte';
 
 	export let exerciseFeedbackModal: HTMLDialogElement;
-	export let selectedExercise: WorkoutExercise;
+	export let selectedExercise: WorkoutExercise | undefined;
+
+	const ratingMap: Record<number, string> = { 0: 'none', 1: 'moderate', 2: 'high' };
+
+	let feedbackSystem: Record<string, Record<string, 'good-btn' | 'ok-btn' | 'bad-btn'>> = {
+		'Joint pain rating': { 'No pain': 'good-btn', 'Some pain': 'ok-btn', 'That hurt': 'bad-btn' },
+		'Pump rating': { 'No pump': 'bad-btn', 'Decent pump': 'ok-btn', 'Amazing pump': 'good-btn' },
+		'Disruption rating': {
+			'No disruption': 'bad-btn',
+			'Some disruption': 'ok-btn',
+			'Lots of disruption': 'good-btn'
+		},
+		'Mind muscle connection rating': { None: 'bad-btn', Decent: 'ok-btn', Perfect: 'good-btn' }
+	};
+
+	let feedbackValues: Record<string, 'none' | 'moderate' | 'high' | undefined> = {
+		'Joint pain rating': undefined,
+		'Pump rating': undefined,
+		'Disruption rating': undefined,
+		'Mind muscle connection rating': undefined
+	};
+	function submitFeedback() {
+		if (!selectedExercise) {
+			return;
+		}
+		selectedExercise.jointPainRating = feedbackValues['Joint pain rating'];
+		selectedExercise.pumpRating = feedbackValues['Pump rating'];
+		selectedExercise.disruptionRating = feedbackValues['Disruption rating'];
+		selectedExercise.mindMuscleConnectionRating = feedbackValues['Mind muscle connection rating'];
+
+		exerciseFeedbackModal.close();
+		Object.keys(feedbackValues).forEach((value) => {
+			feedbackValues[value] = undefined;
+		});
+	}
 </script>
 
 <MyModal
@@ -10,111 +44,33 @@
 	title="Exercise feedback"
 	titleColor="text-accent"
 >
-	<p>Rate the exercise for appropriate adjustments in the next week</p>
-	<div class="flex flex-col mt-4 gap-1">
-		<h4 class="uppercase font-semibold">Joint pain rating</h4>
-		<div class="flex justify-between gap-1">
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="joint-pain"
-				aria-label="No pain"
-				id="good-btn"
-			/>
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="joint-pain"
-				aria-label="Some pain"
-				id="ok-btn"
-			/>
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="joint-pain"
-				aria-label="That hurt"
-				id="bad-btn"
-			/>
-		</div>
-	</div>
-	<div class="flex flex-col mt-4 gap-1">
-		<h4 class="uppercase font-semibold">Muscular pump rating</h4>
-		<div class="flex justify-between gap-1">
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="muscular-pump"
-				aria-label="No pump"
-				id="bad-btn"
-			/>
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="muscular-pump"
-				aria-label="Decent pump"
-				id="ok-btn"
-			/>
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="muscular-pump"
-				aria-label="Amazing pump"
-				id="good-btn"
-			/>
-		</div>
-	</div>
-	<div class="flex flex-col mt-4 gap-1">
-		<h4 class="uppercase font-semibold">Muscular disruption rating</h4>
-		<div class="flex justify-between gap-1">
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="muscular-disruption"
-				aria-label="None"
-				id="ok-btn"
-			/>
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="muscular-disruption"
-				aria-label="Somewhat"
-				id="good-btn"
-			/>
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="muscular-disruption"
-				aria-label="Too much"
-				id="bad-btn"
-			/>
-		</div>
-	</div>
-	<div class="flex flex-col mt-4 gap-1">
-		<h4 class="uppercase font-semibold">Mind muscle connection rating</h4>
-		<div class="flex justify-between gap-1">
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="mind-muscle-connection"
-				aria-label="None"
-				id="bad-btn"
-			/>
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="mind-muscle-connection"
-				aria-label="Somewhat"
-				id="ok-btn"
-			/>
-			<input
-				class="btn basis-1/3 grow-0 shrink-0 text-sm"
-				type="radio"
-				name="mind-muscle-connection"
-				aria-label="Perfect"
-				id="good-btn"
-			/>
-		</div>
-	</div>
+	<p>
+		Rate <span class="font-semibold italic">{selectedExercise?.name}</span> for appropriate adjustments
+		in the next week
+	</p>
+	<div class="h-px w-full bg-secondary mt-2 mb-4" />
+	<form class="flex flex-col gap-2" on:submit|preventDefault={submitFeedback}>
+		{#each Object.keys(feedbackSystem) as item}
+			<div class="flex flex-col">
+				<h3 class="font-semibold">{item}</h3>
+				<div class="grid grid-cols-3 gap-1 mt-1">
+					{#each Object.keys(feedbackSystem[item]) as choice, i}
+						<input
+							class="btn"
+							type="radio"
+							name={item}
+							aria-label={choice}
+							id={feedbackSystem[item][choice]}
+							bind:group={feedbackValues[item]}
+							value={ratingMap[i]}
+							required
+						/>
+					{/each}
+				</div>
+			</div>
+		{/each}
+		<button class="btn btn-block mt-2 btn-accent"> Submit feedback </button>
+	</form>
 </MyModal>
 
 <style>
