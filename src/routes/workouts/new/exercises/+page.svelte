0f694a3f5b09c9e428f6dd6eb006bc15137cca86
WhyAsh5114 <whyash5114@gmail.com>
2023-07-15 13:28:02 +0530

    added progress bar for workout completion status

--- src/routes/workouts/new/exercises/+page.svelte
+++ src/routes/workouts/new/exercises/+page.svelte
@@ -6,7 +6,8 @@
 		plannedRIR,
 		weekNumber,
 		muscleTargetsAndSets,
-		referenceWorkout
+		referenceWorkout,
+		workoutExercises
 	} from '../newWorkoutStore';
 	import { goto } from '$app/navigation';
 	import WorkoutExerciseCard from '$lib/components/workout/WorkoutExerciseCard.svelte';
@@ -44,21 +45,40 @@
 		return workoutExercise;
 	}
 
-	let workoutExercises: WorkoutExercise[] = [];
+	$workoutExercises = [];
 	if ($referenceWorkout === null && data.parentMesocycle.splitExercises[$workoutDay]) {
 		data.parentMesocycle.splitExercises[$workoutDay].forEach((exercise) => {
-			workoutExercises.push(splitExerciseToWorkoutExercise(exercise));
+			$workoutExercises.push(splitExerciseToWorkoutExercise(exercise));
 		});
 	} else {
 		// TODO: template from old workout and apply appropriate volume changes
 	}
+
+	let totalSets = 0;
+	$: {
+		totalSets = 0;
+		$workoutExercises.forEach((exercise) => {
+			totalSets += exercise.repsLoadRIR.length;
+		});
+	}
+	let setsPerformedPerExercise: number[];
+	let totalSetsPerformed: number;
+	$: try {
+		totalSetsPerformed = setsPerformedPerExercise.reduce((partialSum, num) => partialSum + num, 0);
+	} catch {
+		totalSetsPerformed = 0;
+	}
 </script>
 
 <div class="grow w-full bg-primary rounded-md flex flex-col">
-	<div class="collapse collapse-arrow rounded-t-md bg-accent rounded-b-none text-black shadow-md shadow-black">
+	<div
+		class="collapse collapse-arrow rounded-t-md bg-accent rounded-b-none text-black shadow-md shadow-black"
+	>
 		<input type="checkbox" class="p-0 min-h-0" checked />
 		<div class="collapse-title p-1.5 min-h-0 flex justify-between items-center">
-			<h2 class="text-xl font-bold">{data.parentMesocycle.splitSchedule[$workoutDay]} ({days[$workoutDay]})</h2>
+			<h2 class="text-xl font-bold">
+				{data.parentMesocycle.splitSchedule[$workoutDay]} ({days[$workoutDay]})
+			</h2>
 		</div>
 		<div class="collapse-content backdrop-brightness-90">
 			<div class="flex justify-between mt-2">
@@ -73,7 +93,31 @@
 		</div>
 	</div>
 	<ul class="h-px grow w-full overflow-y-auto p-2 gap-3 flex flex-col">
-		<WorkoutExerciseCard bind:workoutExercises />
+		<WorkoutExerciseCard bind:workoutExercises={$workoutExercises} bind:setsPerformedPerExercise />
 	</ul>
 </div>
-<button class="btn btn-block btn-accent mt-3"> Finish Workout </button>
+<button
+	class="btn btn-block btn-accent mt-3 flex flex-col disabled:bg-accent disabled:text-black"
+	disabled={totalSetsPerformed !== totalSets}
+	on:click={() => {goto('/workouts/new/overview')}}
+>
+	{#if totalSetsPerformed === totalSets}
+		Finish workout
+	{:else}
+		{Math.round(totalSetsPerformed / totalSets * 100)}%
+		<progress
+			class="progress w-full bg-secondary progress-primary"
+			value={totalSetsPerformed}
+			max={totalSets}
+		/>
+	{/if}
+</button>
+
+<style>
+	progress::-webkit-progress-value {
+		-webkit-transition: width 0.5s ease;
+		-moz-transition: width 0.5s ease;
+		-o-transition: width 0.5s ease;
+		transition: width 0.5s ease;
+	}
+</style>
