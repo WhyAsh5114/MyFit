026b5d85db0f60d621de724a4ad9607eb5a12521
WhyAsh5114 <whyash5114@gmail.com>
2023-07-17 22:15:21 +0530

    added soreness text

--- src/lib/components/workout/ExerciseFeedbackModal.svelte
+++ src/lib/components/workout/ExerciseFeedbackModal.svelte
@@ -82,7 +82,7 @@
 		titleColor="text-accent"
 	>
 		<p>
-			How much was the workload for the
+			How much was the workload & soreness for the
 			<span class="font-semibold italic">{selectedExercise.muscleTarget}</span> muscles in this workout?
 		</p>
 		<div class="h-px w-full bg-secondary mt-2 mb-4" />
