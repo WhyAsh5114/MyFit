e11f052e2dbddedad497bf8f3a32164496b2355a
WhyAsh5114 <whyash5114@gmail.com>
2023-07-19 15:33:51 +0530

    added UI and provider for GitHub login

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,17 +1,19 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GoogleProvider from '@auth/core/providers/google';
+import GitHub from '@auth/core/providers/github';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import clientPromise from '$lib/mongodb';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 
 export const handle = SvelteKitAuth({
 	// @ts-expect-error in beta
 	adapter: MongoDBAdapter(clientPromise),
 	providers: [
 		GoogleProvider({
-			clientId: GOOGLE_CLIENT_ID as string,
-			clientSecret: GOOGLE_CLIENT_SECRET as string
-		})
+			clientId: GOOGLE_CLIENT_ID,
+			clientSecret: GOOGLE_CLIENT_SECRET
+		}),
+		GitHub({ clientId: GITHUB_CLIENT_ID, clientSecret: GITHUB_CLIENT_SECRET })
 	],
 	callbacks: {
 		session: async ({ session }) => {
