0d95bfdc573a28445ccb2b6077ab29463e90e237
WhyAsh5114 <whyash5114@gmail.com>
2023-07-20 22:29:01 +0530

    doesn't show others section if no other mesocycles to show

--- src/routes/mesocycles/+page.svelte
+++ src/routes/mesocycles/+page.svelte
@@ -51,38 +51,40 @@
 			</p>
 		</div>
 	{/if}
-	<h3 class="text-left w-full text-xl font-bold mt-6 mb-2">Others</h3>
-	<ul class="flex flex-col gap-2 h-px grow w-full overflow-y-auto overflow-x-hidden mb-2">
-		{#each data.mesocycles as meso, i}
-			{#if meso && i !== data.activeMesocycle?.mesoID}
-				<li>
-					<a
-						class="btn relative flex-col btn-primary normal-case rounded-lg w-full p-2 flex-nowrap h-fit gap-3"
-						href="/mesocycles/view/{i}"
-					>
-						{#if $navigating?.to?.url.pathname === `/mesocycles/view/${i}`}
-							<div class="absolute h-full w-full bg-black bg-opacity-75 grid place-items-center rounded-lg">
-								<span class="loading loading-spinner loading-lg" />
+	{#if !(data.activeMesocycle?.mesoID !== undefined && data.mesocycles.length === 1)}
+		<h3 class="text-left w-full text-xl font-bold mt-6 mb-2">Others</h3>
+		<ul class="flex flex-col gap-2 h-px grow w-full overflow-y-auto overflow-x-hidden mb-2">
+			{#each data.mesocycles as meso, i}
+				{#if meso && i !== data.activeMesocycle?.mesoID}
+					<li>
+						<a
+							class="btn relative flex-col btn-primary normal-case rounded-lg w-full p-2 flex-nowrap h-fit gap-3"
+							href="/mesocycles/view/{i}"
+						>
+							{#if $navigating?.to?.url.pathname === `/mesocycles/view/${i}`}
+								<div class="absolute h-full w-full bg-black bg-opacity-75 grid place-items-center rounded-lg">
+									<span class="loading loading-spinner loading-lg" />
+								</div>
+							{/if}
+							<div class="flex justify-between items-center w-full">
+								<h3 class="ml-2 text-lg text-ellipsis">{meso.name}</h3>
+								<p class="badge badge-secondary ml-auto">{meso.duration} weeks</p>
 							</div>
-						{/if}
-						<div class="flex justify-between items-center w-full">
-							<h3 class="ml-2 text-lg text-ellipsis">{meso.name}</h3>
-							<p class="badge badge-secondary ml-auto">{meso.duration} weeks</p>
-						</div>
-						<div class="flex w-full gap-1 flex-wrap">
-							{#each meso.splitSchedule as workout}
-								{#if workout !== ''}
-									<p class="font-normal badge w-fit">{workout}</p>
-								{:else}
-									<p class="font-normal badge badge-accent" />
-								{/if}
-							{/each}
-						</div>
-					</a>
-				</li>
-			{/if}
-		{/each}
-	</ul>
+							<div class="flex w-full gap-1 flex-wrap">
+								{#each meso.splitSchedule as workout}
+									{#if workout !== ''}
+										<p class="font-normal badge w-fit">{workout}</p>
+									{:else}
+										<p class="font-normal badge badge-accent" />
+									{/if}
+								{/each}
+							</div>
+						</a>
+					</li>
+				{/if}
+			{/each}
+		</ul>
+	{/if}
 {:else}
 	<div class="grow flex items-center">
 		<p class="font-semibold text-lg">You haven't created any mesocycles</p>
