abe4e5d0ed715bc8508b226f6a4f303badf4f162
WhyAsh5114 <whyash5114@gmail.com>
2023-08-31 18:35:24 +0530

    restarting project

--- src/routes/profile/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import { page } from '$app/stores';
-
-	console.log($page.data.session)
-</script>
-
-<p>Hello {$page.data.session?.user?.name}</p>
-<p>{$page.data.session?.user?.email}</p>
