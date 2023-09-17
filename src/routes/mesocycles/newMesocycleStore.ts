00efb36d0582716f3dc2ca440e95d8ae8f636a5b
WhyAsh5114 <whyash5114@gmail.com>
2023-09-17 19:12:14 +0530

    moved data to stores

---
+++ src/routes/mesocycles/newMesocycleStore.ts
@@ -0,0 +1,6 @@
+import { writable } from "svelte/store";
+
+export const mesocycleName = writable("");
+export const mesocycleDuration = writable(8);
+export const mesocycleStartRIR = writable(3);
+export const mesocycleFatLossPhase = writable(false);
