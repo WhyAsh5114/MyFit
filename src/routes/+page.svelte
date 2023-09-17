9cc2f041fdd0f617384024bedb24c03532b1439e
WhyAsh5114 <whyash5114@gmail.com>
2023-09-17 14:03:43 +0530

    UI changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	export let data;
-	import { page } from '$app/stores';
 </script>
 
 {#if !data.loggedIn}
 	<p>Please login to continue</p>
 {:else}
-	<p class="font-semibold">Hi, {$page.data.session?.user?.name}</p>
 	{#if !data.mesocycles}
-		<p>You haven't created a mesocycle yet!</p>
-		<a class="btn btn-accent mt-1.5" href="/mesocycles/new">Create mesocycle</a>
+		<p class="my-auto">You haven't created a mesocycle yet!</p>
+		<a class="btn btn-primary btn-block" href="/mesocycles/new">Create mesocycle</a>
 	{/if}
 {/if}
