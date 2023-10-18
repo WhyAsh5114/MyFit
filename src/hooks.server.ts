b5295eb484e43060dc60613cd0574495fbcf222d
WhyAsh5114 <whyash5114@gmail.com>
2023-10-18 19:19:50 +0530

    updated deps

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,10 @@
 import { SvelteKitAuth } from '@auth/sveltekit';
 import GoogleProvider from '@auth/core/providers/google';
-import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import clientPromise from '$lib/mongodb';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
 
 export const handle = SvelteKitAuth({
-	// @ts-expect-error in beta
 	adapter: MongoDBAdapter(clientPromise),
 	providers: [
 		GoogleProvider({
