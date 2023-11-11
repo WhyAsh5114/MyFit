ba90fbebf247c74f44109ccb604c7934528af769
WhyAsh5114 <whyash5114@gmail.com>
2023-11-11 21:36:20 +0530

    created basic layout for meso creation

---
+++ src/routes/mesocycles/create/new/+layout.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+	const steps = ["basics", "split", "exercises", "overview"];
+	let currentPageIndex = steps.findIndex((step) => $page.url.pathname.endsWith(step));
+</script>
+
+<ul class="steps bg-primary rounded-md py-1 mb-5">
+	{#each steps as step, i}
+		<li class="step capitalize {i <= currentPageIndex ? 'step-accent' : ''}">{step}</li>
+	{/each}
+</ul>
+
+<slot />
