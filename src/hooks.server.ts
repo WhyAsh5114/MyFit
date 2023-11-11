7a798b8fbc93e5dfbb2b5fdb707afa26a54482c6
WhyAsh5114 <whyash5114@gmail.com>
2023-11-11 14:19:01 +0530

    added login page and server hooks for auth

---
+++ src/hooks.server.ts
@@ -0,0 +1,9 @@
+import { SvelteKitAuth } from "@auth/sveltekit";
+import Google from "@auth/core/providers/google";
+import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private";
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
+import clientPromise from "$lib/mongodb";
+
+export const handle = SvelteKitAuth({
+	providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })]
+});
