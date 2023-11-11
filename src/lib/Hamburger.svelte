337f0a4266cb38687ea15362e2733525c2255c76
WhyAsh5114 <whyash5114@gmail.com>
2023-11-11 13:44:17 +0530

    added hamburger icon

---
+++ src/lib/Hamburger.svelte
@@ -0,0 +1,13 @@
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	fill="none"
+	viewBox="0 0 24 24"
+	class="inline-block w-5 h-5 stroke-white"
+>
+	<path
+		stroke-linecap="round"
+		stroke-linejoin="round"
+		stroke-width="2"
+		d="M4 6h16M4 12h16M4 18h16"
+	/>
+</svg>
