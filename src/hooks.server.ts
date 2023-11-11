4f2b4754d530def78a4227049e3025cc89ae9a04
WhyAsh5114 <whyash5114@gmail.com>
2023-11-11 14:23:28 +0530

    updated database name

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -5,5 +5,8 @@ import { MongoDBAdapter } from "@auth/mongodb-adapter"
 import clientPromise from "$lib/mongodb";
 
 export const handle = SvelteKitAuth({
-	providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })]
+	providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
+	adapter: MongoDBAdapter(clientPromise, {
+		databaseName: 'MyFit-v2'
+	})
 });
