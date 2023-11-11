cb1a6d021c0f18c1e0aaa6cc36742ea1d78d6880
WhyAsh5114 <whyash5114@gmail.com>
2023-11-11 21:36:11 +0530

    UI changes

--- src/routes/mesocycles/create/+page.svelte
+++ src/routes/mesocycles/create/+page.svelte
@@ -4,19 +4,19 @@
     <a class="btn btn-primary w-full h-20" href="/mesocycles/create/new/basics">
         <div class="flex flex-col items-start w-full normal-case font-normal gap-1">
             <h3 class="text-xl font-semibold">Start from scratch</h3>
-            <p>Create a brand new mesocycle without a template</p>
+            <p class="text-left">Create a brand new mesocycle without a template</p>
         </div>
     </a>
     <a class="btn btn-primary w-full h-20 btn-disabled" href="/mesocycles/create/new/basics">
         <div class="flex flex-col items-start w-full normal-case font-normal gap-1">
             <h3 class="text-xl font-semibold">Template from pre-existing mesocycle</h3>
-            <p>Template from a mesocycle that you already created</p>
+            <p class="text-left">Template from a mesocycle that you already created</p>
         </div>
     </a>
     <a class="btn btn-primary w-full h-20 btn-disabled" href="/mesocycles/create/new/basics">
         <div class="flex flex-col items-start w-full normal-case font-normal gap-1">
             <h3 class="text-xl font-semibold">Use pre-defined mesocycles</h3>
-            <p>Start from a commonly used pre-built mesocycle and modify as per your need</p>
+            <p class="text-left">Start from a commonly used pre-built mesocycle and modify as per your need</p>
         </div>
     </a>
 </div>
