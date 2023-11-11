8ae80349919725e707875111db0e0bfc001ebbf6
WhyAsh5114 <whyash5114@gmail.com>
2023-11-11 12:12:13 +0530

    updated packages

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,29 +1,25 @@
-/** @type {import('tailwindcss').Config}*/
-const config = {
+/** @type {import('tailwindcss').Config} */
+export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
-
 	theme: {
 		extend: {}
 	},
-
 	plugins: [require('daisyui')],
 	daisyui: {
 		themes: [
 			{
-				mytheme: {
-					primary: '#303b55',
+				myTheme: {
+					primary: '#323b49',
 					secondary: '#f4f4f4',
 					accent: '#30c9b5',
 					neutral: '#292524',
 					'base-100': '#000000',
-					info: '#3abff8',
+					info: '#3ABFF8',
 					success: '#22c55e',
 					warning: '#facc15',
-					error: '#dd0000'
+					error: '#ff4747'
 				}
 			}
 		]
 	}
 };
-
-module.exports = config;
