1c15977626c14344ed9f08f080dbf81e16bb0964
WhyAsh5114 <whyash5114@gmail.com>
2023-11-11 14:37:59 +0530

    fixed env variable

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,11 +1,12 @@
 // This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
 import { MongoClient } from "mongodb";
+import { MONGODB_URI } from '$env/static/private';
 
-if (!process.env.MONGODB_URI) {
+if (!MONGODB_URI) {
 	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
 }
 
-const uri = process.env.MONGODB_URI;
+const uri = MONGODB_URI;
 const options = {};
 
 let client;
