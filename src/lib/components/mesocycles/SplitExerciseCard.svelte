52d79f3148645f101d7be51ac88dd6ab62cf9e49
WhyAsh5114 <whyash5114@gmail.com>
2023-11-12 17:18:11 +0530

    added split exercise card

---
+++ src/lib/components/mesocycles/SplitExerciseCard.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import Hamburger from "$lib/Hamburger.svelte";
+
+	export let exercise: SplitExercise;
+</script>
+
+<div class="flex flex-col bg-primary w-full h-fit rounded-md p-2 gap-1.5">
+	<div class="flex gap-2">
+		<p class="font-semibold grow">{exercise.name}</p>
+		<button><Hamburger /></button>
+	</div>
+	<div class="flex items-end justify-between text-sm">
+		<p>{exercise.sets} sets of {exercise.repRangeStart} to {exercise.repRangeEnd} reps</p>
+		<span class="badge badge-error text-black font-semibold">{exercise.targetMuscleGroup}</span>
+	</div>
+    {#if exercise.note}
+        <p class="bg-info bg-opacity-75 text-black px-1 text-sm rounded-sm">{exercise.note}</p>
+    {/if}
+</div>
