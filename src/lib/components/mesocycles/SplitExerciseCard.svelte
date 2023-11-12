e84393a7c36699108006c464b704edc60ed4e09f
WhyAsh5114 <whyash5114@gmail.com>
2023-11-12 17:59:52 +0530

    UI changes

--- src/lib/components/mesocycles/SplitExerciseCard.svelte
+++ src/lib/components/mesocycles/SplitExerciseCard.svelte
@@ -4,7 +4,7 @@
 	export let exercise: SplitExercise;
 </script>
 
-<div class="flex flex-col bg-primary w-full h-fit rounded-md p-2 gap-1.5">
+<div class="flex flex-col bg-primary w-full h-fit rounded-md p-2">
 	<div class="flex gap-2">
 		<p class="font-semibold grow">{exercise.name}</p>
 		<details class="dropdown dropdown-end">
@@ -20,6 +20,6 @@
 		<span class="badge badge-error text-black font-semibold">{exercise.targetMuscleGroup}</span>
 	</div>
 	{#if exercise.note}
-		<p class="bg-info bg-opacity-75 text-black px-1 text-sm rounded-sm">{exercise.note}</p>
+		<p class="bg-info bg-opacity-75 text-black px-1 text-sm rounded-sm mt-1.5">{exercise.note}</p>
 	{/if}
 </div>
