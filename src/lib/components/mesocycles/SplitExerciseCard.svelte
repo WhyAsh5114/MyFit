66a6d2659ed05f8b541d84d6a6b88e5d6f35b04b
WhyAsh5114 <whyash5114@gmail.com>
2023-11-12 19:48:22 +0530

    added delete function and transitions

--- src/lib/components/mesocycles/SplitExerciseCard.svelte
+++ src/lib/components/mesocycles/SplitExerciseCard.svelte
@@ -2,11 +2,12 @@
 	import Hamburger from "$lib/Hamburger.svelte";
 
 	export let exercise: SplitExercise;
-	export let editingExerciseName: undefined | string;
+	export let editingExerciseName: undefined | string = undefined;
 	export let editSplitExerciseModal: HTMLDialogElement;
+	export let deleteExercise: (name: string) => void;
 </script>
 
-<div class="flex flex-col bg-primary w-full h-fit rounded-md p-2">
+<li class="flex flex-col bg-primary w-full h-fit rounded-md p-2">
 	<div class="flex gap-2">
 		<p class="font-semibold grow">{exercise.name}</p>
 		<div class="dropdown dropdown-end">
@@ -25,7 +26,12 @@
 					<button class="join-item text-lg btn btn-primary btn-sm p-0 rounded-sm">↑</button>
 					<button class="join-item text-lg btn btn-primary btn-sm p-0 rounded-sm">↓</button>
 				</li>
-				<li><button class="btn btn-sm btn-error text-black rounded-sm">Delete</button></li>
+				<li>
+					<button
+						class="btn btn-sm btn-error text-black rounded-sm"
+						on:click={() => deleteExercise(exercise.name)}>Delete</button
+					>
+				</li>
 			</ul>
 		</div>
 	</div>
@@ -36,4 +42,4 @@
 	{#if exercise.note}
 		<p class="bg-info bg-opacity-75 text-black px-1 text-sm rounded-sm mt-1.5">{exercise.note}</p>
 	{/if}
-</div>
+</li>
--- src/lib/components/mesocycles/SplitExercisesTable.svelte
+++ src/lib/components/mesocycles/SplitExercisesTable.svelte
@@ -1,13 +1,19 @@
 <script lang="ts">
+	import { flip } from "svelte/animate";
 	import AddSplitExerciseModal from "./AddSplitExerciseModal.svelte";
 	import EditSplitExerciseModal from "./EditSplitExerciseModal.svelte";
 	import SplitExerciseCard from "./SplitExerciseCard.svelte";
+	import { slide } from "svelte/transition";
 	export let exercises: SplitExercise[];
 
 	let addSplitExerciseModal: HTMLDialogElement;
-	let editSplitExerciseModal: HTMLDialogElement;
 
+	let editSplitExerciseModal: HTMLDialogElement;
 	let editingExerciseName: undefined | string = undefined;
+
+	function deleteExercise(name: string) {
+		exercises = exercises.filter((exercise) => exercise.name !== name);
+	}
 </script>
 
 <AddSplitExerciseModal bind:dialogElement={addSplitExerciseModal} bind:exercises />
@@ -17,11 +23,18 @@
 	bind:exerciseOriginalName={editingExerciseName}
 />
 
-<div class="flex flex-col gap-1 h-px grow overflow-y-auto">
-	{#each exercises as exercise}
-		<SplitExerciseCard bind:exercise bind:editingExerciseName bind:editSplitExerciseModal />
+<ul class="flex flex-col gap-1 h-px grow overflow-y-auto">
+	{#each exercises as exercise (exercise.name)}
+		<div transition:slide|local={{ duration: 200 }} animate:flip={{ duration: 200 }}>
+			<SplitExerciseCard
+				bind:exercise
+				bind:editingExerciseName
+				bind:editSplitExerciseModal
+				{deleteExercise}
+			/>
+		</div>
 	{/each}
-</div>
+</ul>
 <div class="join my-2 w-full grid grid-cols-2">
 	<button class="btn btn-primary join-item">Functions</button>
 	<button class="btn btn-primary join-item" on:click={() => addSplitExerciseModal.show()}>
