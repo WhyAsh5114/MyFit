ea628185edb68ebea1f26088b8f5cf45dab5a033
WhyAsh5114 <whyash5114@gmail.com>
2023-11-12 10:07:58 +0530

    added transition, error checking, and next buttons also added progression store

--- src/routes/mesocycles/create/new/basics/+page.svelte
+++ src/routes/mesocycles/create/new/basics/+page.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { mesocycleName, mesocycleDuration, mesocycleStartRIR } from "../newMesocycleStore";
-	let customizeRIRProgression = true;
-
-	type ProgressionData = { specificRIR: number; duration: number };
+	import MyModal from "$lib/components/MyModal.svelte";
+	import { slide } from "svelte/transition";
+	import { mesocycleName, mesocycleDuration, mesocycleStartRIR, mesocycleRIRProgression } from "../newMesocycleStore";
+	let customizeRIRProgression = false;
 
 	let RIRColors = ["range-error", "range-warning", "range-accent", "range-success"];
 	function calculateRIRProgression(totalDuration: number, startRIR: number) {
@@ -22,9 +22,9 @@
 
 		let progression: ProgressionData[] = [];
 		for (let i = startRIR; i >= 1; i--) {
-			progression.push({ specificRIR: i, duration: result[startRIR - i] });
+			progression.push({ specificRIR: i, durationInWeeks: result[startRIR - i] });
 		}
-		progression.push({ specificRIR: 0, duration: failureWeek ? 1 : 0 });
+		progression.push({ specificRIR: 0, durationInWeeks: failureWeek ? 1 : 0 });
 		return progression;
 	}
 
@@ -34,7 +34,7 @@
 		let prevWeeks = 0;
 		RIRProgression.forEach((progression) => {
 			if (RIR < progression.specificRIR) {
-				prevWeeks += progression.duration;
+				prevWeeks += progression.durationInWeeks;
 			}
 		});
 		return prevWeeks;
@@ -45,7 +45,7 @@
 		const p = RIRProgression.find(
 			(progression) => progression.specificRIR === RIR
 		) as ProgressionData;
-		p.duration = duration;
+		p.durationInWeeks = duration;
 
 		// If no upcoming RIRs, function complete
 		if (RIR === 0) {
@@ -59,90 +59,116 @@
 			$mesocycleDuration - previousWeeks(RIR) - duration,
 			RIR - 1
 		);
-		laterProgression.forEach(({ specificRIR, duration }) => {
+		laterProgression.forEach(({ specificRIR, durationInWeeks }) => {
 			const p = RIRProgression.find(
 				(originalProgression) => originalProgression.specificRIR === specificRIR
 			) as ProgressionData;
-			p.duration = duration;
+			p.durationInWeeks = duration;
 		});
 
 		// Update DOM
 		RIRProgression = RIRProgression;
 	}
+
+	let errorModal: HTMLDialogElement;
+	function validateProgression() {
+		// TODO: make sure total duration matches sum of all specific RIR durations
+		let totalDuration = 0;
+		RIRProgression.forEach(({ durationInWeeks }) => {
+			totalDuration += durationInWeeks;
+		});
+		if (totalDuration < $mesocycleDuration) {
+			errorModal.show();
+			return false;
+		}
+		$mesocycleRIRProgression = RIRProgression;
+	}
 </script>
 
-<form class="flex flex-col gap-6 w-full max-w-xs mx-auto">
-	<div class="form-control w-full mx-auto">
-		<label class="label" for="mesocycle-name">
-			<span class="label-text">Mesocycle name</span>
-		</label>
-		<input
-			type="text"
-			placeholder="Type here"
-			id="mesocycle-name"
-			class="input input-bordered w-full"
-			bind:value={$mesocycleName}
-		/>
-	</div>
-
-	<div class="form-control w-full">
-		<label class="label" for="mesocycle-duration">
-			<span class="label-text">Mesocycle duration</span>
-			<span class="label-text-alt">{$mesocycleDuration} weeks</span>
-		</label>
-		<input
-			type="range"
-			min="4"
-			max="20"
-			bind:value={$mesocycleDuration}
-			class="range range-secondary"
-			id="mesocycle-duration"
-		/>
-	</div>
-
-	<div class="form-control w-full">
-		<label class="label" for="mesocycle-start-RIR">
-			<span class="label-text">Start RIR</span>
-		</label>
-		<select class="select select-bordered" id="mesocycle-start-RIR" bind:value={$mesocycleStartRIR}>
-			<option value={3}>3 RIR</option>
-			<option value={2}>2 RIR</option>
-			<option value={1}>1 RIR</option>
-			<option value={0}>0 RIR</option>
-		</select>
-	</div>
-
-	<div class="form-control">
-		<label class="label cursor-pointer">
-			<span class="label-text">Customize RIR progression</span>
+<MyModal title="Error" titleColor="text-error" bind:dialogElement={errorModal}>
+	<p>
+		Weekly RIR duration is less than total mesocycle duration, re-adjust the sliders accordingly
+	</p>
+</MyModal>
+<form class="flex flex-col w-full grow" on:submit|preventDefault={validateProgression}>
+	<div class="flex flex-col my-auto">
+		<div class="form-control w-full max-w-xs mx-auto">
+			<label class="label" for="mesocycle-name">
+				<span class="label-text">Mesocycle name</span>
+			</label>
 			<input
-				type="checkbox"
-				class="toggle"
-				id="customize-RIR-progression"
-				bind:checked={customizeRIRProgression}
+				type="text"
+				placeholder="Type here"
+				id="mesocycle-name"
+				class="input input-bordered w-full"
+				bind:value={$mesocycleName}
+				required
 			/>
-		</label>
-	</div>
-
-	{#if customizeRIRProgression}
-		<div class="flex flex-col">
-			{#each RIRProgression as { specificRIR, duration }}
-				<div class="form-control w-full">
-					<label class="label" for={`${specificRIR}-RIR-duration`}>
-						<span class="label-text">{specificRIR} RIR</span>
-						<span class="label-text-alt">{duration} weeks</span>
-					</label>
-					<input
-						type="range"
-						min={0}
-						max={$mesocycleDuration - previousWeeks(specificRIR)}
-						value={duration}
-						on:input={(e) => modifyProgression(specificRIR, parseInt(e.currentTarget.value))}
-						class="range range-xs range-secondary {RIRColors[specificRIR]}"
-						id={`${specificRIR}-RIR-duration`}
-					/>
-				</div>
-			{/each}
 		</div>
-	{/if}
+		<div class="form-control w-full mt-6 max-w-xs mx-auto">
+			<label class="label" for="mesocycle-duration">
+				<span class="label-text">Mesocycle duration</span>
+				<span class="label-text-alt">{$mesocycleDuration} weeks</span>
+			</label>
+			<input
+				type="range"
+				min="4"
+				max="20"
+				bind:value={$mesocycleDuration}
+				class="range range-secondary"
+				id="mesocycle-duration"
+			/>
+		</div>
+		<div class="form-control w-full mt-6 max-w-xs mx-auto">
+			<label class="label" for="mesocycle-start-RIR">
+				<span class="label-text">Start RIR</span>
+			</label>
+			<select
+				class="select select-bordered"
+				id="mesocycle-start-RIR"
+				bind:value={$mesocycleStartRIR}
+			>
+				<option value={3}>3 RIR</option>
+				<option value={2}>2 RIR</option>
+				<option value={1}>1 RIR</option>
+				<option value={0}>0 RIR</option>
+			</select>
+		</div>
+		<div class="form-control mt-6 max-w-xs mx-auto w-full">
+			<label class="label cursor-pointer">
+				<span class="label-text">Customize RIR progression</span>
+				<input
+					type="checkbox"
+					class="toggle"
+					id="customize-RIR-progression"
+					bind:checked={customizeRIRProgression}
+				/>
+			</label>
+		</div>
+		{#if customizeRIRProgression}
+			<div class="flex flex-col" transition:slide={{ duration: 200 }}>
+				{#each RIRProgression as { specificRIR, durationInWeeks }}
+					<div class="form-control w-full max-w-xs mx-auto">
+						<label class="label" for={`${specificRIR}-RIR-duration`}>
+							<span class="label-text">{specificRIR} RIR</span>
+							<span class="label-text-alt">{durationInWeeks} weeks</span>
+						</label>
+						<input
+							type="range"
+							min={0}
+							max={$mesocycleDuration - previousWeeks(specificRIR)}
+							value={durationInWeeks}
+							on:input={(e) => modifyProgression(specificRIR, parseInt(e.currentTarget.value))}
+							class="range range-xs range-secondary {RIRColors[specificRIR]}"
+							id={`${specificRIR}-RIR-duration`}
+						/>
+					</div>
+				{/each}
+			</div>
+		{/if}
+	</div>
+	<div class="join grid grid-cols-2">
+		<button class="btn btn-accent join-item" disabled>Previous</button>
+		<a class="btn btn-accent join-item" href="/mesocycles/create/new/split">Next</a>
+	</div>
 </form>
--- src/routes/mesocycles/create/new/newMesocycleStore.ts
+++ src/routes/mesocycles/create/new/newMesocycleStore.ts
@@ -3,3 +3,4 @@ import { writable, type Writable } from "svelte/store";
 export const mesocycleName: Writable<string> = writable("");
 export const mesocycleDuration: Writable<number> = writable(6);
 export const mesocycleStartRIR: Writable<number> = writable(3);
+export const mesocycleRIRProgression: Writable<ProgressionData[]> = writable([]);
