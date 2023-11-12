77f1852cc21947dc02d199581e5dc9db19ffc717
WhyAsh5114 <whyash5114@gmail.com>
2023-11-12 10:01:36 +0530

    UI changes and index keeps updating

--- src/routes/mesocycles/create/new/+layout.svelte
+++ src/routes/mesocycles/create/new/+layout.svelte
@@ -1,10 +1,10 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	const steps = ["basics", "split", "exercises", "overview"];
-	let currentPageIndex = steps.findIndex((step) => $page.url.pathname.endsWith(step));
+	$: currentPageIndex = steps.findIndex((step) => $page.url.pathname.endsWith(step));
 </script>
 
-<ul class="steps bg-primary rounded-md py-1 mb-5">
+<ul class="steps bg-primary rounded-md py-1">
 	{#each steps as step, i}
 		<li class="step capitalize {i <= currentPageIndex ? 'step-accent' : ''}">{step}</li>
 	{/each}
