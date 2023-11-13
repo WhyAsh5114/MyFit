a39dab8df244a1e9a79451dbe9a836d66ab09fd7
WhyAsh5114 <whyash5114@gmail.com>
2023-11-13 22:14:23 +0530

    UI changes

--- src/routes/mesocycles/create/+page.svelte
+++ src/routes/mesocycles/create/+page.svelte
@@ -1,22 +1,22 @@
 <h2>Create new mesocycle</h2>
 
 <div class="flex flex-col gap-2">
-    <a class="btn btn-primary w-full h-20" href="/mesocycles/create/new/basics">
-        <div class="flex flex-col items-start w-full normal-case font-normal gap-1">
+    <a class="btn btn-primary w-full h-fit py-3" href="/mesocycles/create/new/basics">
+        <div class="flex flex-col w-full font-normal text-left gap-1">
             <h3 class="text-xl font-semibold">Start from scratch</h3>
-            <p class="text-left">Create a brand new mesocycle without a template</p>
+            <p>Create a brand new mesocycle without a template</p>
         </div>
     </a>
-    <a class="btn btn-primary w-full h-20 btn-disabled" href="/mesocycles/create/new/basics">
-        <div class="flex flex-col items-start w-full normal-case font-normal gap-1">
-            <h3 class="text-xl font-semibold">Template from pre-existing mesocycle</h3>
-            <p class="text-left">Template from a mesocycle that you already created</p>
+    <a class="btn btn-primary w-full h-fit py-3 btn-disabled" href="/mesocycles/create/new/basics">
+        <div class="flex flex-col w-full font-normal text-left gap-1">
+            <h3 class="text-xl font-semibold">Use template</h3>
+            <p>Template from a mesocycle that you already created</p>
         </div>
     </a>
-    <a class="btn btn-primary w-full h-20 btn-disabled" href="/mesocycles/create/new/basics">
-        <div class="flex flex-col items-start w-full normal-case font-normal gap-1">
-            <h3 class="text-xl font-semibold">Use pre-defined mesocycles</h3>
-            <p class="text-left">Start from a commonly used pre-built mesocycle and modify as per your need</p>
+    <a class="btn btn-primary w-full h-fit py-3 btn-disabled" href="/mesocycles/create/new/basics">
+        <div class="flex flex-col w-full font-normal text-left gap-1">
+            <h3 class="text-xl font-semibold">Use common mesocycles</h3>
+            <p>Start from a commonly used pre-built mesocycle and modify as per your need</p>
         </div>
     </a>
 </div>
