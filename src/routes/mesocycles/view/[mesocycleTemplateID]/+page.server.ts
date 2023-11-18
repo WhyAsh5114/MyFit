8e8ed326071f7d469d95f303476fa813e715d412
WhyAsh5114 <whyash5114@gmail.com>
2023-11-18 18:57:24 +0530

    added view mesocycle and delete functionality

---
+++ src/routes/mesocycles/view/[mesocycleTemplateID]/+page.server.ts
@@ -0,0 +1,31 @@
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import clientPromise from "$lib/mongo/mongodb";
+import { ObjectId } from "mongodb";
+
+export const load: PageServerLoad = async ({ locals, params }) => {
+	const session = await locals.getSession();
+	if (!session?.user?.id) {
+		throw error(403, "Not logged in");
+	}
+
+	if (params.mesocycleTemplateID.length !== 24) {
+		throw error(400, "Mesocycle template ID should be 24 character hex string");
+	}
+
+	const client = await clientPromise;
+	const mesocycleTemplateDocument = await client
+		.db()
+		.collection<MesocycleTemplate & { userId: ObjectId }>("mesocycleTemplates")
+		.findOne({ _id: new ObjectId(params.mesocycleTemplateID) });
+
+	if (mesocycleTemplateDocument === null) {
+		throw error(404, "Mesocycle not found");
+	} else if (mesocycleTemplateDocument.userId.toString() !== session.user.id) {
+		throw error(403, "Unauthorized");
+	}
+
+	const { _id, userId, ...mesocycleTemplateProps } = mesocycleTemplateDocument;
+	const mesocycleTemplate: MesocycleTemplate = { ...mesocycleTemplateProps };
+	return { mesocycleTemplate };
+};
