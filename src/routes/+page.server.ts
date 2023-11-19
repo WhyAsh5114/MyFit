ba0c47ba89b0d2ce6e0d661e86fac9613163e97a
WhyAsh5114 <whyash5114@gmail.com>
2023-11-19 16:20:54 +0530

    added activeMesocycle data to homepage

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,56 @@
+import type { PageServerLoad } from "./$types";
+import clientPromise from "$lib/mongo/mongodb";
+import { error } from "@sveltejs/kit";
+import { ObjectId, type WithId } from "mongodb";
+import type { ActiveMesocycleDocument, MesocycleTemplateDocument } from "$lib/types/documents";
+
+export const load: PageServerLoad = async ({ locals }) => {
+	const session = await locals.getSession();
+	if (!session?.user?.id) {
+		throw error(403, "Not logged in");
+	}
+
+	const client = await clientPromise;
+
+	let activeMesocycle: WithSerializedId<ActiveMesocycle> | null = null;
+	let activeMesocycleTemplate: WithSerializedId<MesocycleTemplate> | null = null;
+	const activeMesocycleDocument = await client
+		.db()
+		.collection<Omit<ActiveMesocycleDocument, "userId">>("activeMesocycles")
+		.findOne({ userId: new ObjectId(session.user.id) }, { projection: { userId: 0 } });
+	if (!activeMesocycleDocument) {
+		return {
+			activeMesocycle,
+			activeMesocycleDocument
+		}
+	}
+
+	const { _id, templateMesoId, workouts, startTimestamp } = activeMesocycleDocument;
+	activeMesocycle = {
+		id: _id.toString(),
+		templateMesoId: templateMesoId.toString(),
+		workouts: workouts.map((workout) => workout.toString()),
+		startTimestamp
+	};
+
+	const activeMesocycleTemplateDocument = (await client
+		.db()
+		.collection<Omit<MesocycleTemplateDocument, "userId">>("mesocycleTemplates")
+		.findOne(
+			{ _id: new ObjectId(activeMesocycle.templateMesoId) },
+			{ projection: { userId: 0 } }
+		)) as WithId<Omit<MesocycleTemplateDocument, "userId">>;
+	
+	{
+		const { _id, ...otherProps } = activeMesocycleTemplateDocument;
+		activeMesocycleTemplate = {
+			id: activeMesocycleTemplateDocument._id.toString(),
+			...otherProps
+		};
+	}
+
+	return {
+		activeMesocycle,
+		activeMesocycleTemplate
+	};
+};
