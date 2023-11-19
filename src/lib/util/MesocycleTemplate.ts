c3a37806150b7025137c5b779c07cb1bb81c4d61
WhyAsh5114 <whyash5114@gmail.com>
2023-11-19 22:41:31 +0530

    added more utility functions

--- src/lib/util/MesocycleTemplate.ts
+++ src/lib/util/MesocycleTemplate.ts
@@ -14,3 +14,35 @@ export function getTodaysWorkout(
 	const workout = exerciseSplit[workoutIdx];
 	return { workout, workoutIdx };
 }
+
+export function getMuscleGroups(exercises: SplitExercise[]) {
+	const muscleGroups: Set<MuscleGroup> = new Set();
+	exercises.forEach(({ targetMuscleGroup }) => {
+		muscleGroups.add(targetMuscleGroup);
+	});
+	return muscleGroups;
+}
+
+export function getMuscleGroupsAndSets(exercises: SplitExercise[]) {
+	const muscleGroupAndSets: { muscleGroup: MuscleGroup; sets: number }[] = [];
+	exercises.forEach((exercise) => {
+		const targetMuscleGroup = exercise.targetMuscleGroup;
+		const idx = muscleGroupAndSets.findIndex(
+			({ muscleGroup }) => targetMuscleGroup === muscleGroup
+		);
+		if (idx !== -1) {
+			muscleGroupAndSets[idx].sets += exercise.sets;
+		} else {
+			muscleGroupAndSets.push({ muscleGroup: targetMuscleGroup, sets: exercise.sets });
+		}
+	});
+	return muscleGroupAndSets;
+}
+
+export function getTotalSets(exercises: SplitExercise[]) {
+	let totalSets = 0;
+	exercises.forEach(({ sets }) => {
+		totalSets += sets;
+	});
+	return totalSets;
+}
