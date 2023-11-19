fe5d23347db415c7ac8302e032be7aecd6a5c22b
WhyAsh5114 <whyash5114@gmail.com>
2023-11-19 21:31:44 +0530

    UI changes

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,5 +4,5 @@
 
 <div class="flex flex-col grow justify-center items-center gap-5">
     <span class="font-extrabold text-error text-7xl">{$page.status}</span>
-    <span class="text-lg font-semibold">{$page.error?.message}</span>
+    <span class="text-lg">{$page.error?.message}</span>
 </div>
