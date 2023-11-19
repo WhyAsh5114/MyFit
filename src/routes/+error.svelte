7ef9cf2d6cc9d6f7febf0f544429c12ebb62ff35
WhyAsh5114 <whyash5114@gmail.com>
2023-11-19 19:06:04 +0530

    added an error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { page } from "$app/stores";
+</script>
+
+<div class="flex flex-col grow justify-center items-center gap-5">
+    <span class="font-extrabold text-error text-7xl">{$page.status}</span>
+    <span class="text-lg font-semibold">{$page.error?.message}</span>
+</div>
