aeb8a5988baf2d9a2ba04d2db72bf885ed406bde
WhyAsh5114 <whyash5114@gmail.com>
2023-11-22 14:31:37 +0530

    added page and data for new workout

---
+++ src/routes/workouts/new/exercises/+page.server.ts
@@ -0,0 +1,57 @@
+import { error } from "@sveltejs/kit";
+import type { PageServerLoad } from "./$types";
+import {
+	getCycleNumber,
+	getDayNumber,
+	getPlannedRIR,
+	getTodaysSplitWorkout
+} from "$lib/util/MesocycleTemplate";
+import { applyProgressiveOverload } from "$lib/util/ProgressiveOverload";
+import { splitExercisesToWorkoutExercise } from "$lib/util/CommonFunctions";
+
+export const load: PageServerLoad = async ({ locals, parent }) => {
+	const session = await locals.getSession();
+	if (!session?.user?.id) {
+		throw error(403, "Not logged in");
+	}
+
+	const { activeMesocycle, activeMesocycleTemplate, referenceWorkout } = await parent();
+	if (!activeMesocycle) {
+		throw error(404, "No active mesocycle found");
+	}
+
+	const todaysSplitWorkout = getTodaysSplitWorkout(
+		activeMesocycle.workouts,
+		activeMesocycleTemplate.exerciseSplit
+	);
+	if (todaysSplitWorkout === null) {
+		throw error(400, "No workout found for today");
+	}
+
+	const todaysWorkout: Partial<WorkoutBeingPerformed> = {
+		startTimestamp: +new Date(),
+		referenceWorkout: null,
+		dayNumber: getDayNumber(activeMesocycle.workouts, activeMesocycleTemplate.exerciseSplit),
+		cycleNumber: getCycleNumber(activeMesocycleTemplate.exerciseSplit, activeMesocycle.workouts),
+		templateMesoId: activeMesocycle.templateMesoId,
+		plannedRIR: getPlannedRIR(activeMesocycleTemplate, activeMesocycle.workouts),
+		deload: false
+	};
+
+	if (referenceWorkout) {
+		todaysWorkout.exercisesPerformed = applyProgressiveOverload(
+			referenceWorkout.exercisesPerformed
+		);
+	} else {
+		todaysWorkout.exercisesPerformed = splitExercisesToWorkoutExercise(
+			todaysSplitWorkout.exercises
+		);
+	}
+
+	return {
+		activeMesocycle,
+		activeMesocycleTemplate,
+		todaysWorkout: todaysWorkout as WorkoutBeingPerformed,
+		todaysSplitWorkout
+	};
+};
---
+++ src/routes/workouts/new/exercises/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	import {
+		getCycleNumber,
+		getDayNumber
+	} from "$lib/util/MesocycleTemplate";
+	export let data;
+
+	const { activeMesocycle, activeMesocycleTemplate, todaysWorkout, todaysSplitWorkout } = data;
+	const workoutIdx = getDayNumber(activeMesocycle.workouts, activeMesocycleTemplate.exerciseSplit);
+</script>
+
+<div class="collapse bg-primary collapse-arrow rounded-md">
+	<input type="checkbox" id="show-workout-details" checked />
+	<div class="collapse-title text-xl font-medium">
+		{todaysSplitWorkout.name}
+		<span class="text-sm font-normal">
+			Day {workoutIdx + 1}, Cycle {getCycleNumber(
+				activeMesocycleTemplate.exerciseSplit,
+				activeMesocycle.workouts
+			)}
+		</span>
+	</div>
+	<div class="collapse-content backdrop-brightness-50">
+		<div class="flex gap-1 mt-4 font-semibold justify-between items-center">
+			<progress class="progress progress-accent w-56 b" value="40" max="100"></progress>
+		</div>
+	</div>
+</div>
+
+<button class="btn btn-accent btn-block mt-auto"> Finish workout </button>
