885397466767e12ce4345c8065e75d99378a91af
WhyAsh5114 <whyash5114@gmail.com>
2023-11-24 11:58:40 +0530

    changed dropdown to input:text added set completion and edit functionality

--- src/lib/components/workouts/WorkoutExerciseSets.svelte
+++ src/lib/components/workouts/WorkoutExerciseSets.svelte
@@ -1,105 +1,65 @@
 <script lang="ts">
 	import DoneIcon from "virtual:icons/material-symbols/done";
-	import { range } from "$lib/util/CommonFunctions";
+	import EditIcon from "virtual:icons/material-symbols/edit-outline";
 	export let exercise: WorkoutExerciseWithoutSetNumbers;
 
-	function selectParam(
-		value: number,
-		setNumber: number,
-		currentTarget: EventTarget & HTMLButtonElement,
-		param: "reps" | "load" | "RIR"
-	) {
-		currentTarget.blur();
-		if (param === "reps") exercise.sets[setNumber].reps = value;
-		if (param === "load") exercise.sets[setNumber].load = value;
-		if (param === "RIR") exercise.sets[setNumber].RIR = value;
-	}
-
-	function weightString(weightType: ExerciseWeightType, value: number) {
-		if (weightType === "Weighted") return value.toString();
-		if (weightType === "Bodyweight") {
-			if (value > 0) {
-				return `BW+${value}`;
-			} else if (value < 0) {
-				return `BW${value}`;
-			} else {
-				return "BW";
-			}
-		}
-	}
+	let setsCompleted = Array(exercise.sets.length).fill(false);
 </script>
 
 {#each exercise.sets as { reps, load, RIR }, setNumber}
-	<div class="dropdown">
-		<button class="btn py-0 btn-sm rounded-md">
-			{#if reps !== null}
-				{reps}
-			{:else}
-				?
-			{/if}
-		</button>
-		<div class="dropdown-menu weight-picker-menu h-44 !flex-nowrap">
-			{#each range(1, 100, 1) as i}
-				<button
-					class="btn btn-sm"
-					on:click={(e) => {
-						selectParam(i, setNumber, e.currentTarget, "reps");
-					}}
-				>
-					{i}
-				</button>
-			{/each}
+	<form class="contents" on:submit|preventDefault={() => (setsCompleted[setNumber] = true)}>
+		<div class="flex">
+			<input
+				type="number"
+				id="{exercise.name}-set{setNumber}-reps"
+				class="input input-sm w-12"
+				bind:value={reps}
+				placeholder="?"
+				min={0}
+				required
+				disabled={setsCompleted[setNumber]}
+			/>
 		</div>
-	</div>
-	<div class="dropdown">
-		<button class="btn py-0 btn-sm rounded-md">
-			{#if load !== null}
-				{load}
-			{:else}
-				?
-			{/if}
-		</button>
-		<ul class="dropdown-menu weight-picker-menu h-44 !flex-nowrap !w-28">
-			{#each exercise.weightType === "Weighted" ? range(0, 1000, 2.5) : range(-100, 100, 2.5) as i}
-				<li>
-					<button
-						class="btn btn-sm"
-						on:click={(e) => selectParam(i, setNumber, e.currentTarget, "load")}
-					>
-						{weightString(exercise.weightType, i)}
-					</button>
-				</li>
-			{/each}
-		</ul>
-	</div>
-	<div class="dropdown">
-		<button class="btn py-0 btn-sm rounded-md">
-			{#if RIR !== null}
-				{RIR}
-			{:else}
-				?
-			{/if}
-		</button>
-		<ul class="dropdown-menu weight-picker-menu h-fit !flex-nowrap">
-			{#each range(4, 0, -1) as x, i}
-				<li>
-					<button
-						class="btn btn-sm"
-						on:click={(e) => selectParam(i, setNumber, e.currentTarget, "RIR")}
-					>
-						{i}
-					</button>
-				</li>
-			{/each}
-		</ul>
-	</div>
-	<button class="btn btn-xs btn-accent btn-circle" aria-label="mark-set-complete">
-		<DoneIcon />
-	</button>
+		<div class="flex">
+			<input
+				type="number"
+				id="{exercise.name}-set{setNumber}-load"
+				class="input input-sm w-12"
+				bind:value={load}
+				placeholder="?"
+				required
+				disabled={setsCompleted[setNumber]}
+			/>
+		</div>
+		<div class="flex">
+			<input
+				type="number"
+				id="{exercise.name}-set{setNumber}-RIR"
+				class="input input-sm w-12"
+				bind:value={RIR}
+				placeholder="?"
+				min={0}
+				max={10}
+				required
+				disabled={setsCompleted[setNumber]}
+			/>
+		</div>
+		{#if !setsCompleted[setNumber]}
+			<button
+				class="btn btn-xs btn-accent btn-circle"
+				aria-label="mark-set-complete"
+				disabled={setsCompleted[setNumber]}
+			>
+				<DoneIcon />
+			</button>
+		{:else}
+			<button
+				class="btn btn-xs btn-circle bg-base-100/25 border-0"
+				on:click={() => (setsCompleted[setNumber] = false)}
+				aria-label="edit-set"
+			>
+				<EditIcon />
+			</button>
+		{/if}
+	</form>
 {/each}
-
-<style lang="postcss">
-	.weight-picker-menu {
-		@apply mt-0.5 overflow-y-auto flex flex-col;
-	}
-</style>
