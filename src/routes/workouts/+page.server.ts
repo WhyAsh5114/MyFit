cc5cee36463a8429279c28da76c8d980016d9551
WhyAsh5114 <whyash5114@gmail.com>
2023-12-03 16:45:42 +0530

    mesocycles and templates also passed

--- src/routes/workouts/+page.server.ts
+++ src/routes/workouts/+page.server.ts
@@ -1,17 +1,59 @@
 import { error } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 import clientPromise from "$lib/mongo/mongodb";
-import type { WorkoutDocument } from "$lib/types/documents";
+import type {
+	MesocycleDocument,
+	MesocycleTemplateDocument,
+	WorkoutDocument
+} from "$lib/types/documents";
 import { ObjectId } from "mongodb";
 
 export const load: PageServerLoad = async ({ depends, locals }) => {
-	depends("workout:all");
+	depends("workout:all", "mesocycle:templates", "mesocycle:usages");
 	const session = await locals.getSession();
 	if (!session?.user?.id) {
 		throw error(403, "Not logged in");
 	}
 
 	const client = await clientPromise;
+	const mesocycleCursor = client
+		.db()
+		.collection<Omit<MesocycleDocument, "userId">>("mesocycles")
+		.find({ userId: new ObjectId(session.user.id) }, { projection: { userId: 0 } })
+		.map((mesocycleDocument) => {
+			const { _id, templateMesoId, workouts, ...otherProps } = mesocycleDocument;
+			const mesocycle: WithSerializedId<Mesocycle> = {
+				id: _id.toString(),
+				...otherProps,
+				templateMesoId: templateMesoId.toString(),
+				workouts: workouts.map((workout) => workout?.toString() ?? null)
+			};
+			return mesocycle;
+		});
+
+	const mesocyclesStreamArray = [];
+	while (await mesocycleCursor.hasNext()) {
+		mesocyclesStreamArray.push(mesocycleCursor.next());
+	}
+
+	const mesocycleTemplateCursor = client
+		.db()
+		.collection<Omit<MesocycleTemplateDocument, "userId">>("mesocycleTemplates")
+		.find({ userId: new ObjectId(session.user.id) }, { projection: { userId: 0 } })
+		.map((mesocycleTemplateDocument) => {
+			const { _id, ...otherProps } = mesocycleTemplateDocument;
+			const mesocycleTemplate: WithSerializedId<MesocycleTemplate> = {
+				id: mesocycleTemplateDocument._id.toString(),
+				...otherProps
+			};
+			return mesocycleTemplate;
+		});
+
+	const mesocycleTemplatesStreamArray = [];
+	while (await mesocycleTemplateCursor.hasNext()) {
+		mesocycleTemplatesStreamArray.push(mesocycleTemplateCursor.next());
+	}
+
 	const workoutsCursor = client
 		.db()
 		.collection<Omit<WorkoutDocument, "userId">>("workouts")
@@ -36,6 +78,8 @@ export const load: PageServerLoad = async ({ depends, locals }) => {
 
 	return {
 		streamed: {
+			mesocyclesStreamArray,
+			mesocycleTemplatesStreamArray,
 			workoutsStreamArray
 		}
 	};
