6014e525fd62d22bce66a45c1ad2559b94068440
WhyAsh5114 <whyash5114@gmail.com>
2023-12-03 17:57:50 +0530

    meso info given if filtered

--- src/routes/workouts/WorkoutCard.svelte
+++ src/routes/workouts/WorkoutCard.svelte
@@ -1,25 +1,28 @@
 <script lang="ts">
 	import { dateFormatter } from "$lib/util/CommonFunctions";
-	import { getMuscleGroupsAndSets } from "$lib/util/MesocycleTemplate";
 
 	export let workoutPromise: Promise<
 		(Workout & { id: string } & { performedMesocycleId: string }) | null
 	>;
+	export let mesocycleTemplate: WithSerializedId<MesocycleTemplate> | null;
 </script>
 
 {#await workoutPromise}
 	<div class="skeleton w-full h-20 bg-primary brightness-50 rounded-md"></div>
 {:then workout}
 	{#if workout}
-		<button class="btn btn-primary rounded-md justify-start px-0 font-normal h-fit">
+		<a
+			class="btn btn-primary rounded-md justify-start px-0 font-normal h-fit"
+			href="/workouts/view/{workout.id}"
+		>
 			<div class="flex flex-col p-2 w-full items-start">
-				<span class="text-lg font-semibold">{dateFormatter(workout.startTimestamp)}</span>
-				<div class="flex flex-wrap gap-1 mt-2">
-					{#each getMuscleGroupsAndSets(workout.exercisesPerformed) as { muscleGroup, sets }}
-						<span class="badge font-semibold">{muscleGroup} x {sets}</span>
-					{/each}
+				<div class="flex justify-between items-center w-full">
+					<span class="text-lg font-semibold">{dateFormatter(workout.startTimestamp)}</span>
+					{#if mesocycleTemplate}
+						{mesocycleTemplate.exerciseSplit[workout.dayNumber]?.name}, Cycle {workout.cycleNumber}
+					{/if}
 				</div>
 			</div>
-		</button>
+		</a>
 	{/if}
 {/await}
