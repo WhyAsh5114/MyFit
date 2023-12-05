174d48a8ef5fc8ea0839437c42b2f4cbf6ddb1a2
WhyAsh5114 <whyash5114@gmail.com>
2023-12-05 18:01:06 +0530

    updated env variable names

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,14 +2,14 @@ import { SvelteKitAuth } from '@auth/sveltekit';
 import GoogleProvider from '@auth/core/providers/google';
 import { MongoDBAdapter } from '@auth/mongodb-adapter';
 import clientPromise from '$lib/mongodb';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { GOOGLE_ID, GOOGLE_SECRET } from '$env/static/private';
 
 export const handle = SvelteKitAuth({
 	adapter: MongoDBAdapter(clientPromise),
 	providers: [
 		GoogleProvider({
-			clientId: GOOGLE_CLIENT_ID,
-			clientSecret: GOOGLE_CLIENT_SECRET
+			clientId: GOOGLE_ID,
+			clientSecret: GOOGLE_SECRET
 		})
 	],
 	callbacks: {
