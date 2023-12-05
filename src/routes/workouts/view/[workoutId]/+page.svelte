1b5cae4f2de1797209d7a46516661fd7d2352feb
WhyAsh5114 <whyash5114@gmail.com>
2023-12-05 23:34:40 +0530

    added workload and soreness data as well

--- src/routes/workouts/view/[workoutId]/+page.svelte
+++ src/routes/workouts/view/[workoutId]/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { page } from "$app/stores";
 	import WorkoutExercisesTable from "$lib/components/workouts/WorkoutExercisesTable.svelte";
+	import { sorenessFeedback, workloadFeedback } from "$lib/types/arrays.js";
 	import { dateFormatter } from "$lib/util/CommonFunctions.js";
 	import StarIcon from "virtual:icons/material-symbols/star";
 	export let data;
@@ -9,8 +10,8 @@
 <div class="collapse collapse-arrow bg-primary rounded-md">
 	<input type="checkbox" id="view-workout-details" checked />
 	<div class="collapse-title text-xl font-medium">Workout details</div>
-	<div class="collapse-content backdrop-brightness-75 px-1">
-		<div class="stats-vertical grid grid-cols-2 w-full max-h-64">
+	<div class="collapse-content backdrop-brightness-75 px-0 !pb-0">
+		<div class="stats-vertical grid grid-cols-2 w-full max-h-56 shadow-inner shadow-black">
 			<div class="stat col-span-2">
 				<div class="stat-title">Workout date</div>
 				<div class="stat-value">{dateFormatter(data.workout.startTimestamp)}</div>
@@ -61,21 +62,47 @@
 				</div>
 			</div>
 
-			<div class="stat col-span-2">
-				<div class="stat-title">Muscle group workloads</div>
-				<div class="flex flex-wrap mt-1 gap-1">
-					{#each Object.entries(data.workout.muscleGroupWorkloads) as [muscleGroup, workloadState]}
-						<div class="join text-sm">
-							<span class="join-item bg-base-200 px-2">{muscleGroup}</span>
-							<span class="join-item bg-error px-2">{workloadState}</span>
-						</div>
-					{/each}
+			{#if Object.keys(data.workout.muscleGroupWorkloads).length > 0}
+				<div class="stat col-span-2">
+					<div class="stat-title">Muscle group workloads</div>
+					<div class="flex flex-wrap mt-2 gap-1">
+						{#each Object.entries(data.workout.muscleGroupWorkloads) as [muscleGroup, workloadState]}
+							{@const workloadElement = workloadFeedback.find((e) => e.value === workloadState)}
+							<div class="join text-sm">
+								<span class="join-item bg-base-200 px-2 font-semibold">{muscleGroup}</span>
+								{#if workloadState !== null}
+									<span class="join-item px-2 capitalize text-black {workloadElement?.bgColor}">
+										{workloadElement?.name}
+									</span>
+								{:else}
+									<span class="join-item px-2 bg-base-200 italic">Not given</span>
+								{/if}
+							</div>
+						{/each}
+					</div>
 				</div>
-			</div>
+			{/if}
 
-			<div class="stat col-span-2">
-				<div class="stat-title">Soreness to next workouts</div>
-			</div>
+			{#if Object.keys(data.workout.muscleSorenessToNextWorkout).length > 0}
+				<div class="stat col-span-2">
+					<div class="stat-title">Soreness to next workouts</div>
+					<div class="flex flex-wrap mt-2 gap-1">
+						{#each Object.entries(data.workout.muscleSorenessToNextWorkout) as [muscleGroup, sorenessState]}
+							{@const sorenessElement = sorenessFeedback.find((e) => e.value === sorenessState)}
+							<div class="join text-sm">
+								<span class="join-item bg-base-200 px-2 font-semibold">{muscleGroup}</span>
+								{#if sorenessState !== null}
+									<span class="join-item px-2 capitalize text-black {sorenessElement?.bgColor}">
+										{sorenessElement?.name}
+									</span>
+								{:else}
+									<span class="join-item px-2 bg-base-200 italic">Not given</span>
+								{/if}
+							</div>
+						{/each}
+					</div>
+				</div>
+			{/if}
 		</div>
 	</div>
 </div>
