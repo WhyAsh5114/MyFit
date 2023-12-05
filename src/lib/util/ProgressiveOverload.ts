921296d881cf0c199773c52064d4463e9009362b
WhyAsh5114 <whyash5114@gmail.com>
2023-12-05 14:26:48 +0530

    applies bodyweight changes to the new workout

--- src/lib/util/ProgressiveOverload.ts
+++ src/lib/util/ProgressiveOverload.ts
@@ -1,3 +1,12 @@
-export function applyProgressiveOverload(workout: WorkoutExercise[]) {
+export function applyProgressiveOverload(
+	workout: WorkoutExercise[],
+	newBodyweight: number | null = null
+) {
+	workout = JSON.parse(JSON.stringify(workout));
+	workout.forEach((exercise) => {
+		if (exercise.bodyweight !== null && newBodyweight !== null) {
+			exercise.bodyweight = newBodyweight;
+		}
+	});
 	return workout;
 }
