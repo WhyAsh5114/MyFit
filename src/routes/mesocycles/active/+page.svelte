0e11caf399ed44cca2efef477b4b63f3337d8f5f
WhyAsh5114 <whyash5114@gmail.com>
2023-12-06 11:36:04 +0530

    added workouts in active mesocycle section

--- src/routes/mesocycles/active/+page.svelte
+++ src/routes/mesocycles/active/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { goto, invalidate } from "$app/navigation";
 	import MyModal from "$lib/components/MyModal.svelte";
+	import { dateFormatter } from "$lib/util/CommonFunctions.js";
 	export let data;
 
 	let modal: HTMLDialogElement;
@@ -67,6 +68,41 @@
 		</button>
 	</div>
 </MyModal>
+
+<div class="stats stats-vertical">
+	<div class="stat">
+		<div class="stat-title">Mesocycle template</div>
+		<a class="stat-value link truncate" href="/mesocycles/view/{data.activeMesocycleTemplate.id}">
+			{data.activeMesocycleTemplate.name}
+		</a>
+	</div>
+	<div class="stat">
+		<div class="stat-title">Started at</div>
+		<div class="stat-value">{dateFormatter(data.activeMesocycle.startTimestamp)}</div>
+	</div>
+	<div class="stat">
+		<div class="stat-title">Workouts</div>
+		<div class="flex flex-col max-h-32 overflow-y-auto mt-2 gap-1">
+			{#each data.streamed.workoutsStreamArray as workoutPromise}
+				{#await workoutPromise}
+					<div class="skeleton h-8 w-full bg-primary brightness-50 rounded-md"></div>
+				{:then workout}
+					{#if workout}
+						<a class="btn h-8 btn-sm" href="/workouts/view/{workout.id}">
+							<div class="flex w-full justify-between items-center">
+								<span>{dateFormatter(workout.startTimestamp)}</span>
+								<span class="font-normal text-sm">
+									{data.activeMesocycleTemplate.exerciseSplit[workout.dayNumber]?.name},
+									Cycle {workout.cycleNumber}
+								</span>
+							</div>
+						</a>
+					{/if}
+				{/await}
+			{/each}
+		</div>
+	</div>
+</div>
 
 <button
 	class="btn btn-block btn-warning mt-auto"
