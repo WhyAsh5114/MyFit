975b25204fc3055c562660ef4ffb47203cc90d51
WhyAsh5114 <whyash5114@gmail.com>
2023-12-11 14:20:26 +0530

    added prop

--- src/lib/types/workouts.ts
+++ src/lib/types/workouts.ts
@@ -40,6 +40,7 @@ interface Workout {
   plannedRIR: number;
   muscleSorenessToNextWorkout: Partial<Record<MuscleGroup, SorenessState | null>>;
   deload: boolean;
+  performedMesocycleId: string;
 }
 
 const JointPainFeedback = [
