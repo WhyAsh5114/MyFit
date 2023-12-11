b24c4ee29f5bfc65880efaf0ecd5be2d2eeede92
WhyAsh5114 <whyash5114@gmail.com>
2023-12-11 15:06:55 +0530

    updated mesocycles page

--- src/routes/mesocycles/+page.server.ts
+++ src/routes/mesocycles/+page.server.ts
@@ -1,39 +1,22 @@
-import clientPromise from "$lib/mongo/mongodb";
 import { error } from "@sveltejs/kit";
-import { ObjectId } from "mongodb";
-import type { MesocycleTemplateDocument } from "$lib/types/documents";
 
-export const load = async ({ locals, parent, depends }) => {
-  depends("mesocycle:templates");
+export const load = async ({ locals, parent, fetch }) => {
   const session = await locals.getSession();
   if (!session?.user?.id) {
     throw error(403, "Not logged in");
   }
 
-  const { activeMesocycle, activeMesocycleTemplate } = await parent(),
-    client = await clientPromise,
-    mesocycleTemplateDocuments = client
-      .db()
-      .collection<Omit<MesocycleTemplateDocument, "userId">>("mesocycleTemplates")
-      .find({ userId: new ObjectId(session.user.id) }, { projection: { userId: 0 } })
-      .map((mesocycleTemplateDocument) => {
-        const { _id, ...otherProps } = mesocycleTemplateDocument,
-          mesocycleTemplate: WithSerializedId<MesocycleTemplate> = {
-            id: mesocycleTemplateDocument._id.toString(),
-            ...otherProps
-          };
-        return mesocycleTemplate;
-      }),
-    mesocycleTemplatesStreamArray = [];
-  while (await mesocycleTemplateDocuments.hasNext()) {
-    mesocycleTemplatesStreamArray.push(mesocycleTemplateDocuments.next());
-  }
+  const { activeMesocycle, activeMesocycleTemplate } = await parent();
+  const getAllMesocycleTemplatesResponse = await fetch(
+    "/api/mesocycles/getAllMesocycleTemplates?page=0"
+  );
+  const { mesocycleTemplates, mesocycleTemplatesCount } =
+    await getAllMesocycleTemplatesResponse.json();
 
   return {
     activeMesocycle,
     activeMesocycleTemplate,
-    streamed: {
-      mesocycleTemplatesStreamArray
-    }
+    mesocycleTemplates,
+    mesocycleTemplatesCount
   };
 };
--- src/routes/mesocycles/+page.svelte
+++ src/routes/mesocycles/+page.svelte
@@ -3,8 +3,23 @@
   import ActiveMesocycleCard from "./ActiveMesocycleCard.svelte";
   import MesocycleTemplateCard from "./MesocycleTemplateCard.svelte";
 
-  const totalMesocycleTemplates = data.streamed.mesocycleTemplatesStreamArray.length,
-    { activeMesocycle, activeMesocycleTemplate } = data;
+  const totalMesocycleTemplates = data.mesocycleTemplates.length;
+  const { activeMesocycle, activeMesocycleTemplate } = data;
+
+  let { mesocycleTemplates, mesocycleTemplatesCount } = data;
+
+  let loadingMore = false;
+  async function loadMore() {
+    const pageToLoad = Math.floor(data.mesocycleTemplates.length / 10);
+    let params = "?page=" + pageToLoad;
+
+    loadingMore = true;
+    const response = await fetch("/api/mesocycles/getAllMesocycleTemplates" + params);
+    const { mesocycleTemplates: newTemplates } = await response.json();
+    loadingMore = false;
+
+    mesocycleTemplates = [...mesocycleTemplates, ...newTemplates];
+  }
 </script>
 
 <h2>Mesocycles</h2>
@@ -22,9 +37,18 @@
 <h3 class="text-xl mb-1 mt-3">Mesocycle templates</h3>
 <div class="flex flex-col gap-1.5 h-px grow overflow-y-auto">
   {#if totalMesocycleTemplates > 0}
-    {#each data.streamed.mesocycleTemplatesStreamArray as mesocycleTemplatePromise}
-      <MesocycleTemplateCard {mesocycleTemplatePromise} />
+    {#each mesocycleTemplates as mesocycleTemplate}
+      <MesocycleTemplateCard {mesocycleTemplate} />
     {/each}
+    {#if mesocycleTemplates.length < mesocycleTemplatesCount}
+      <button class="btn btn-outline" on:click={loadMore}>
+        {#if loadingMore}
+          <span class="loading loading-bars"></span>
+        {:else}
+          Load more
+        {/if}
+      </button>
+    {/if}
   {:else}
     <div class="flex flex-col bg-primary rounded-md p-2">
       <span class="text-warning">No mesocycle created</span>
