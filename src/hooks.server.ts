3f624f52256ac0719c5d6ecfbecc0ddf58ab53b7
WhyAsh5114 <whyash5114@gmail.com>
2023-12-16 18:17:39 +0530

    added a github provider

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,14 +1,21 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/core/providers/google";
-import { GOOGLE_ID, GOOGLE_SECRET } from "$env/static/private";
+import GitHub from "@auth/core/providers/github";
+import { GOOGLE_ID, GOOGLE_SECRET, GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "$lib/mongo/mongodb";
 
 export const handle = SvelteKitAuth({
-  providers: [Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET })],
+  providers: [
+    Google({ clientId: GOOGLE_ID, clientSecret: GOOGLE_SECRET }),
+    GitHub({ clientId: GITHUB_ID, clientSecret: GITHUB_SECRET })
+  ],
   adapter: MongoDBAdapter(clientPromise, {
     databaseName: "MyFit_v2"
   }),
+  pages: {
+    signIn: "/login"
+  },
   callbacks: {
     // Attach mongoDB user document ID for easier queries
     async session({ session, user }) {
