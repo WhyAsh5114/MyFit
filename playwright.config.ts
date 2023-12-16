8847e0bcfc29bbd7043f25980d064d497fa7ea72
WhyAsh5114 <whyash5114@gmail.com>
2023-12-16 18:15:52 +0530

    updated config only one worker for one, will add more later as I get more accounts

--- playwright.config.ts
+++ playwright.config.ts
@@ -12,13 +12,13 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   /* Run tests in files in parallel */
-  fullyParallel: true,
+  fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 1 : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
@@ -27,7 +27,8 @@ export default defineConfig({
     baseURL: 'http://localhost:4173',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry"
+    trace: "on-first-retry",
+    video: 'on'
   },
 
   /* Configure projects for major browsers */
