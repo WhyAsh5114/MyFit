0a731d592732639a79c96cef455e0b14dd2a4095
WhyAsh5114 <whyash5114@gmail.com>
2023-12-20 21:47:32 +0530

    updated playwright config for parallelism

--- playwright.config.ts
+++ playwright.config.ts
@@ -17,8 +17,8 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 1,
+  /* Opt out of parallel tests on CI. Max 6, only 6 test account present */
+  workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
