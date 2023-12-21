6bcaed5b6b5b5e19ada5a1c0e5a255adc53c45ce
WhyAsh5114 <whyash5114@gmail.com>
2023-12-21 10:22:45 +0530

    better test IDs

--- src/lib/components/mesocycles/SplitExerciseCard.svelte
+++ src/lib/components/mesocycles/SplitExerciseCard.svelte
@@ -12,7 +12,7 @@
 
 <li class="flex flex-col bg-primary w-full h-fit rounded-md p-2">
   <div class="flex gap-2">
-    <p class="font-semibold grow">{exercise.name}</p>
+    <p class="font-semibold grow" data-testid="exercise{idx + 1}-name">{exercise.name}</p>
     <div class="dropdown dropdown-end">
       <button class="btn p-0 btn-xs btn-ghost" aria-label="exercise-menu"><Hamburger /></button>
       <ul
@@ -62,10 +62,22 @@
     </div>
   </div>
   <div class="flex items-end justify-between text-sm">
-    <p>{exercise.sets} sets of {exercise.repRangeStart} to {exercise.repRangeEnd} reps</p>
-    <span class="badge badge-error text-black font-semibold">{exercise.targetMuscleGroup}</span>
+    <p data-testid="exercise{idx + 1}-set-and-rep-range">
+      {exercise.sets} sets of {exercise.repRangeStart} to {exercise.repRangeEnd} reps
+    </p>
+    <span
+      class="badge badge-error text-black font-semibold"
+      data-testid="exercise{idx + 1}-target-muscle-group"
+    >
+      {exercise.targetMuscleGroup}
+    </span>
   </div>
   {#if exercise.note}
-    <p class="bg-info bg-opacity-75 text-black px-1 text-sm rounded-sm mt-1.5">{exercise.note}</p>
+    <p
+      class="bg-info bg-opacity-75 text-black px-1 text-sm rounded-sm mt-1.5"
+      data-testid="exercise{idx + 1}-note"
+    >
+      {exercise.note}
+    </p>
   {/if}
 </li>
