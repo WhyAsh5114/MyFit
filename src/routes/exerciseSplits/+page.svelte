fcf02c7c36082c41f9d799d52e9563d03aef29ec
WhyAsh5114 <whyash5114@gmail.com>
2023-12-24 20:18:49 +0530

    added basic pages for exercise splits

---
+++ src/routes/exerciseSplits/+page.svelte
@@ -0,0 +1,4 @@
+<h2>Exercise splits</h2>
+
+<div class="h-px flex flex-col grow overflow-y-auto"></div>
+<a class="btn btn-accent" href="/exerciseSplits/templates">Create new split</a>
---
+++ src/routes/exerciseSplits/templates/+page.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  // TODO: implement the disabled ones...
+  const options = [
+    {
+      title: "Start from scratch",
+      description: "Create a brand new exercise split from scratch",
+      link: "/exerciseSplits/new",
+      disabled: false
+    },
+    {
+      title: "Clone old splits",
+      description: "Use one of your old splits as a template",
+      link: "/exerciseSplits/templates/clone",
+      disabled: true
+    },
+    {
+      title: "Use common splits",
+      description: "Start from commonly used splits",
+      link: "/exerciseSplits/templates/common",
+      disabled: true
+    }
+  ];
+</script>
+
+<h2>New exercise split</h2>
+{#each options as { title, description, link, disabled }}
+  <a class="btn btn-primary mb-1 h-fit py-4" class:btn-disabled={disabled} href={link}>
+    <div class="flex flex-col w-full text-left gap-1">
+      <span class="text-lg">{title}</span>
+      <p class="font-normal">{description}</p>
+    </div>
+  </a>
+{/each}
