36c455f39db93a252564fa5ef5f4a5e424b3dc1f
WhyAsh5114 <whyash5114@gmail.com>
2023-12-26 14:42:46 +0530

    add labels and implement editing

--- src/routes/exerciseSplits/[mode]/exercises/AddEditExerciseModal.svelte
+++ src/routes/exerciseSplits/[mode]/exercises/AddEditExerciseModal.svelte
@@ -21,16 +21,22 @@
   $: mode = editingExercise ? "edit" : "add";
 
   let modalExercise: Nullable<ExerciseTemplate>;
-  $: modalExercise = editingExercise
-    ? JSON.parse(JSON.stringify(editingExercise))
-    : JSON.parse(JSON.stringify(emptyExercise));
+  $: updateModalExercise(editingExercise);
+  function updateModalExercise(_exercise: ExerciseTemplate | null) {
+    if (_exercise) {
+      modalExercise = JSON.parse(JSON.stringify(_exercise));
+    } else {
+      modalExercise = JSON.parse(JSON.stringify(emptyExercise));
+    }
+  }
+
   function addEditExercise() {
     const validExercise = JSON.parse(JSON.stringify(modalExercise)) as ExerciseTemplate;
     let success = true;
     if (mode === "add") {
       success = addExercise(validExercise);
     } else if (mode === "edit" && editingExerciseIdx) {
-      success = editExercise(editingExerciseIdx, validExercise) === false;
+      success = editExercise(editingExerciseIdx, validExercise);
     }
 
     if (!success) {
@@ -44,79 +50,104 @@
 </script>
 
 <MyModal title={editingExercise ? "Edit exercise" : "Add exercise"} bind:dialogElement={modal}>
-  <form
-    id="{mode}-exercise-form"
-    class="flex flex-col gap-1.5"
-    on:submit|preventDefault={addEditExercise}
-  >
-    <input
-      id="{mode}-exercise-name"
-      class="input input-bordered"
-      placeholder="Exercise name"
-      required
-      type="text"
-      bind:value={modalExercise.name}
-    />
-    <div class="form-control bg-base-100 rounded-md px-3 py-1">
+  <form id="{mode}-exercise-form" class="flex flex-col" on:submit|preventDefault={addEditExercise}>
+    <label class="form-control">
+      <div class="label">
+        <span class="label-text">Exercise name</span>
+      </div>
+      <input
+        id="{mode}-exercise-name"
+        class="input input-bordered"
+        placeholder="Type here"
+        required
+        type="text"
+        bind:value={modalExercise.name}
+      />
+    </label>
+    <div class="flex gap-2">
+      <label class="form-control w-full max-w-xs">
+        <div class="label">
+          <span class="label-text">Muscle group</span>
+        </div>
+        <select
+          id="{mode}-exercise-target-muscle-group"
+          class="select grow"
+          required
+          bind:value={modalExercise.targetMuscleGroup}
+        >
+          <option disabled selected value={null}>Select a muscle group</option>
+          {#each muscleGroups as muscleGroup}
+            <option>{muscleGroup}</option>
+          {/each}
+        </select>
+      </label>
+      <label class="form-control w-1/3">
+        <div class="label">
+          <span class="label-text">Sets</span>
+        </div>
+        <input
+          id="{mode}-exercise-sets"
+          class="input input-bordered"
+          min={1}
+          placeholder="Type here"
+          required
+          type="number"
+          bind:value={modalExercise.sets}
+        />
+      </label>
+    </div>
+    <div class="flex gap-2">
+      <label class="form-control grow">
+        <div class="label">
+          <span class="label-text">Rep range start</span>
+        </div>
+        <input
+          id="{mode}-exercise-rep-range-start"
+          class="input input-bordered w-full"
+          min={1}
+          placeholder="From"
+          required
+          type="number"
+          bind:value={modalExercise.repRangeStart}
+        />
+      </label>
+      <label class="form-control grow">
+        <div class="label">
+          <span class="label-text">Rep range end</span>
+        </div>
+        <input
+          id="{mode}-exercise-rep-range-end"
+          class="input input-bordered w-full"
+          min={(modalExercise.repRangeStart ?? 0) + 1}
+          placeholder="To"
+          required
+          type="number"
+          bind:value={modalExercise.repRangeEnd}
+        />
+      </label>
+    </div>
+    <label class="form-control">
+      <div class="label">
+        <span class="label-text">Notes</span>
+      </div>
+      <textarea
+        id="{mode}-exercise-note"
+        class="textarea textarea-bordered"
+        placeholder="Type here"
+        bind:value={modalExercise.note}
+      ></textarea>
+    </label>
+    <div class="form-control rounded-md bg-base-100 my-2 px-3">
       <label class="label cursor-pointer">
-        <span class="label-text text-secondary">Involves bodyweight?</span>
+        <span class="label-text">Involves bodyweight?</span>
         <input
           id="{mode}-exercise-involves-bodyweight"
-          class="checkbox border-secondary/50 border-2"
+          class="checkbox border-secondary/50"
           type="checkbox"
           bind:checked={modalExercise.involvesBodyweight}
         />
       </label>
     </div>
-    <div class="flex gap-2">
-      <select
-        id="{mode}-exercise-target-muscle-group"
-        class="select grow"
-        aria-label="{mode}-exercise-target-muscle-group"
-        required
-        bind:value={modalExercise.targetMuscleGroup}
-      >
-        <option disabled selected value={null}>Select a muscle group</option>
-        {#each muscleGroups as muscleGroup}
-          <option>{muscleGroup}</option>
-        {/each}
-      </select>
-      <input
-        id="{mode}-exercise-sets"
-        class="input input-bordered w-1/3"
-        min={1}
-        placeholder="Sets"
-        required
-        type="number"
-        bind:value={modalExercise.sets}
-      />
-    </div>
-    <div class="flex gap-2">
-      <input
-        id="{mode}-exercise-rep-range-start"
-        class="input input-bordered w-full max-w-xs"
-        min={1}
-        placeholder="Rep range start"
-        required
-        type="number"
-        bind:value={modalExercise.repRangeStart}
-      />
-      <input
-        id="{mode}-exercise-rep-range-end"
-        class="input input-bordered w-full max-w-xs"
-        min={(modalExercise.repRangeStart ?? 0) + 1}
-        placeholder="Rep range end"
-        required
-        type="number"
-        bind:value={modalExercise.repRangeEnd}
-      />
-    </div>
-    <textarea
-      id="{mode}-exercise-note"
-      class="textarea textarea-bordered"
-      placeholder="Notes"
-      bind:value={modalExercise.note}
-    ></textarea>
     {#if errorMsg}
       <p class="bg-error/50 text-sm p-2 rounded-md">
         {errorMsg}
--- src/routes/exerciseSplits/[mode]/exercises/ExerciseSplitTable.svelte
+++ src/routes/exerciseSplits/[mode]/exercises/ExerciseSplitTable.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import AddEditExerciseModal from "./AddEditExerciseModal.svelte";
   import AddIcon from "virtual:icons/material-symbols/add";
+  import MenuIcon from "virtual:icons/material-symbols/menu";
+  import CloseIcon from "virtual:icons/material-symbols/close";
+  import EditIcon from "virtual:icons/material-symbols/edit-outline";
+  import DeleteIcon from "virtual:icons/material-symbols/delete-outline";
+  import UpArrow from "virtual:icons/mdi/arrow-up";
+  import DownArrow from "virtual:icons/mdi/arrow-down";
 
   export let exerciseTemplates: ExerciseTemplate[];
   let editingExercise: ExerciseTemplate | null = null;
@@ -8,21 +14,47 @@
   let addEditExerciseModal: HTMLDialogElement;
 
   function addExercise(exercise: ExerciseTemplate) {
-    if (exerciseTemplates.find((_exercise) => _exercise.name === exercise.name)) {
+    if (exerciseTemplates.find((_ex) => _ex.name === exercise.name)) {
       return false;
     }
     exerciseTemplates = [...exerciseTemplates, exercise];
     return true;
   }
-  function editExercise() {
-    return false;
+  function editExercise(idx: number, exercise: ExerciseTemplate) {
+    if (exerciseTemplates.find((_ex, _idx) => _ex.name === exercise.name && _idx !== idx)) {
+      return false;
+    }
+    exerciseTemplates.splice(idx, 1, exercise);
+    exerciseTemplates = exerciseTemplates;
+    return true;
   }
 
   function showAddExerciseModal() {
     editingExercise = null;
     addEditExerciseModal.show();
   }
-  function showEditExerciseModal() {}
+  function showEditExerciseModal(idx: number) {
+    editingExerciseIdx = idx;
+    editingExercise = exerciseTemplates[idx];
+    addEditExerciseModal.show();
+  }
+
+  function closeOtherMenus(e: Event & { currentTarget: EventTarget & HTMLDetailsElement }) {
+    const summary = e.currentTarget.firstChild as Element;
+    summary.removeChild(summary.firstElementChild as Element);
+    if (e.currentTarget.open) {
+      new CloseIcon({ target: summary });
+    } else {
+      new MenuIcon({ target: summary });
+      return;
+    }
+    const allDetails = document.querySelectorAll("details");
+    allDetails.forEach((detail) => {
+      if (detail !== e.target) {
+        detail.open = false;
+      }
+    });
+  }
 </script>
 
 <AddEditExerciseModal
@@ -34,13 +66,62 @@
 />
 
 <div class="flex flex-col gap-1 h-px grow overflow-y-auto">
-  {#each exerciseTemplates as exerciseTemplate}
-    <div class="flex flex-col bg-primary rounded-sm p-2">
-      <div class="flex">
-        <span class="font-semibold">{exerciseTemplate.name}</span>
-        {#if exerciseTemplate.involvesBodyweight}
+  {#each exerciseTemplates as exercise, exerciseIdx}
+    <div class="flex flex-col bg-primary rounded-md p-2">
+      <div class="flex items-center gap-2">
+        <span class="font-semibold">{exercise.name}</span>
+        {#if exercise.involvesBodyweight}
           <span class="badge font-semibold">BW</span>
         {/if}
+        <details class="dropdown dropdown-end ml-auto" on:toggle={closeOtherMenus}>
+          <summary class="btn btn-sm p-0 btn-ghost">
+            <MenuIcon />
+          </summary>
+          <ul
+            class="p-2 shadow-lg grid grid-cols-2 dropdown-content z-[1] bg-neutral rounded-md w-32 gap-1"
+          >
+            <li>
+              <button
+                class="btn btn-sm btn-primary rounded-sm p-0 w-full"
+                aria-label="Move exercise up"
+              >
+                <UpArrow />
+              </button>
+            </li>
+            <li>
+              <button
+                class="btn btn-sm btn-primary rounded-sm p-0 w-full"
+                aria-label="Move exercise down"
+              >
+                <DownArrow />
+              </button>
+            </li>
+            <li>
+              <button
+                class="btn btn-sm btn-primary rounded-sm p-0 w-full"
+                aria-label="Edit exercise"
+                on:click={() => showEditExerciseModal(exerciseIdx)}
+              >
+                <EditIcon />
+              </button>
+            </li>
+            <li>
+              <button
+                class="btn btn-sm btn-error rounded-sm p-0 w-full"
+                aria-label="Delete exercise"
+              >
+                <DeleteIcon />
+              </button>
+            </li>
+          </ul>
+        </details>
+      </div>
+      <div class="flex items-center justify-between">
+        <p class="text-sm">
+          {exercise.sets} sets of {exercise.repRangeStart} to {exercise.repRangeEnd}
+          reps
+        </p>
+        <span class="badge badge-error font-semibold text-black">{exercise.targetMuscleGroup}</span>
       </div>
     </div>
   {/each}
