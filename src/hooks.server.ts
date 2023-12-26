18dcef32ae4362b4fecbb28452f78bf9aec0509d
WhyAsh5114 <whyash5114@gmail.com>
2023-12-26 19:40:32 +0530

    moved route protection to hooks

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,7 @@ import Google from "@auth/core/providers/google";
 import GitHub from "@auth/core/providers/github";
 import { MongoDBAdapter } from "@auth/mongodb-adapter";
 import clientPromise from "$lib/mongo/mongodb";
+import { sequence } from "@sveltejs/kit/hooks";
 import {
   AUTH_GOOGLE_ID,
   AUTH_GOOGLE_SECRET,
@@ -10,26 +11,40 @@ import {
   AUTH_GITHUB_SECRET,
   AUTH_SECRET
 } from "$env/static/private";
+import { redirect } from "@sveltejs/kit";
 
-export const handle = SvelteKitAuth({
-  providers: [
-    Google({ clientId: AUTH_GOOGLE_ID, clientSecret: AUTH_GOOGLE_SECRET }),
-    GitHub({ clientId: AUTH_GITHUB_ID, clientSecret: AUTH_GITHUB_SECRET })
-  ],
-  adapter: MongoDBAdapter(clientPromise, {
-    databaseName: "MyFit_v2"
-  }),
-  session: { strategy: "database" },
-  callbacks: {
-    // Attach mongoDB user document ID for easier queries
-    // @ts-expect-error strategy: "database" is set, still giving error?
-    async session({ session, user }) {
-      if (session.user) {
-        session.user.id = user.id;
+const unprotectedRoutes = ["/", "/login", "/offline", "/exerciseSplits/templates"];
+
+export const handle = sequence(
+  SvelteKitAuth({
+    providers: [
+      Google({ clientId: AUTH_GOOGLE_ID, clientSecret: AUTH_GOOGLE_SECRET }),
+      GitHub({ clientId: AUTH_GITHUB_ID, clientSecret: AUTH_GITHUB_SECRET })
+    ],
+    adapter: MongoDBAdapter(clientPromise, {
+      databaseName: "MyFit_v2"
+    }),
+    session: { strategy: "database" },
+    callbacks: {
+      // Attach mongoDB user document ID for easier queries
+      // @ts-expect-error strategy: "database" is set, still giving error?
+      async session({ session, user }) {
+        if (session.user) {
+          session.user.id = user.id;
+        }
+        return session;
       }
-      return session;
+    },
+    secret: AUTH_SECRET,
+    trustHost: true
+  }),
+  async function authorization({ event, resolve }) {
+    if (!unprotectedRoutes.includes(event.url.pathname)) {
+      const session = await event.locals.getSession();
+      if (!session) {
+        throw redirect(303, `/login?callbackURL=${event.url.pathname}`);
+      }
     }
-  },
-  secret: AUTH_SECRET,
-  trustHost: true
-});
+    return resolve(event);
+  }
+);
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,12 +1,4 @@
-import { redirect } from "@sveltejs/kit";
-
-const unprotectedRoutes = ["/", "/login", "/offline"];
-
 export const load = async ({ locals, url }) => {
   const session = await locals.getSession();
-  if (!session && !unprotectedRoutes.includes(url.pathname)) {
-    redirect(303, `/login?callbackURL=${url.pathname}`);
-  }
-
   return { session };
 };
