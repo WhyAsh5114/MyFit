b1f36cb977deeed72c0d22dbe35cfc5b750f9c97
WhyAsh5114 <whyash5114@gmail.com>
2023-12-26 20:48:05 +0530

    created POST endpoint for exerciseSplits

--- src/routes/api/exerciseSplits/+server.ts
+++ src/routes/api/exerciseSplits/+server.ts
@@ -1,2 +1,26 @@
+import clientPromise from "$lib/mongo/mongodb.js";
+import type { WithUserId } from "$lib/types/arrays";
+import { ObjectId } from "mongodb";
+
 export const GET = () => {};
-export const POST = () => {};
+
+export const POST = async ({ locals, request }) => {
+  const session = await locals.getSession();
+  if (!session?.user?.id) {
+    return new Response("Not logged in", { status: 403 });
+  }
+
+  const exerciseSplit: ExerciseSplit = await request.json();
+  const client = await clientPromise;
+  
+  try {
+    await client
+      .db()
+      .collection<WithUserId<ExerciseSplit>>("exerciseSplits")
+      .insertOne({ ...exerciseSplit, userId: new ObjectId(session.user.id) });
+
+    return new Response("Exercise split created successfully", { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+};
