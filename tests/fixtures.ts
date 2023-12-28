498d27d7eb7f9c3b87403bbca336591896730426
WhyAsh5114 <whyash5114@gmail.com>
2023-12-28 22:17:07 +0530

    added test scoped data deletion to fixtures each test will start from an empty user

--- tests/fixtures.ts
+++ tests/fixtures.ts
@@ -7,6 +7,7 @@ import clientPromise from "$lib/mongo/mongodb";
 import { randomUUID } from "crypto";
 import { ObjectId } from "bson";
 import type { AdapterSession, AdapterUser } from "@auth/core/adapters";
+import { type WithUserId } from "$lib/types/arrays";
 dotenv.config();
 const adapter = MongoDBAdapter(clientPromise, { databaseName: "MyFit_v3" });
 
@@ -33,24 +34,47 @@ async function deleteTestUserAndSession(user: AdapterUser, session: AdapterSessi
   await adapter.deleteSession(session.sessionToken);
   // @ts-expect-error idk why, always works though
   await adapter.deleteUser(user.id);
-  // TODO: Go through all the collections and delete user.id === userId documents as well
+}
+
+async function deleteUserData(user: AdapterUser) {
+  const client = await clientPromise;
+  await client
+    .db()
+    .collection<WithUserId<ExerciseSplit>>("exerciseSplits")
+    .deleteMany({ userId: new ObjectId(user.id) });
+  await client
+    .db()
+    .collection<WithUserId<Mesocycle>>("mesocycles")
+    .deleteMany({ userId: new ObjectId(user.id) });
+
+  // TODO: change type to Workout
+  await client
+    .db()
+    .collection<WithUserId<ExerciseSplit>>("workouts")
+    .deleteMany({ userId: new ObjectId(user.id) });
 }
 
 export * from "@playwright/test";
-export const test = baseTest.extend<{}, { workerStorageState: string }>({
+export const test = baseTest.extend<
+  { autoTestFixture: string },
+  { workerStorageState: string; userAndSession: { user: AdapterUser; session: AdapterSession } }
+>({
   // Use the same storage state for all tests in this worker.
   storageState: ({ workerStorageState }, use) => use(workerStorageState),
+  userAndSession: [await createTestUserAndSession(), { scope: "worker" }],
 
   // Authenticate once per worker with a worker-scoped fixture.
   workerStorageState: [
-    async ({ browser }, use) => {
+    async ({ browser, userAndSession }, use) => {
+      // Get from userAndSession (different account for each worker)
+      const { user, session } = userAndSession;
+
       // Use parallelIndex as a unique identifier for each worker.
       const id = test.info().parallelIndex;
       const fileName = path.resolve(test.info().project.outputDir, `.auth/${id}.json`);
 
       // Important: make sure we authenticate in a clean environment by unsetting storage state.
       const page = await browser.newPage({ storageState: undefined });
-      const { user, session } = await createTestUserAndSession();
 
       // Perform authentication steps. Just set a test user's cookie.
       await page.context().addCookies([
@@ -72,8 +96,19 @@ export const test = baseTest.extend<{}, { workerStorageState: string }>({
       await page.close();
       await use(fileName);
 
+      // Delete the account and session
       await deleteTestUserAndSession(user, session);
     },
     { scope: "worker" }
+  ],
+
+  autoTestFixture: [
+    async ({ userAndSession }, use) => {
+      // Clear user data for each test, before AND after
+      await deleteUserData(userAndSession.user);
+      await use("autoTestFixture");
+      await deleteUserData(userAndSession.user);
+    },
+    { scope: "test", auto: true }
   ]
 });
