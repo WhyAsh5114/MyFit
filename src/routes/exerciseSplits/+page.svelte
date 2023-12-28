314fa67f36c460310507dddc96b9503bbd1fb373
WhyAsh5114 <whyash5114@gmail.com>
2023-12-28 20:58:59 +0530

    added a card for no splits created as a new user

--- src/routes/exerciseSplits/+page.svelte
+++ src/routes/exerciseSplits/+page.svelte
@@ -6,27 +6,34 @@
 
 <h2>Exercise splits</h2>
 
-<div class="h-px flex flex-col grow overflow-y-auto gap-1">
-  {#each data.exerciseSplits as exerciseSplit}
-    <a class="btn rounded-md btn-primary h-fit" href="/exerciseSplits/view/{exerciseSplit._id}">
-      <div class="flex flex-col gap-1 py-2 w-full">
-        <div class="flex justify-between items-center">
-          <span class="text-lg font-semibold">{exerciseSplit.name}</span>
-          <span class="font-normal">
-            {getTotalSetsOfSplit(exerciseSplit.splitDays)} sets
-          </span>
+{#if data.exerciseSplits.length > 0}
+  <div class="h-px flex flex-col grow overflow-y-auto gap-1">
+    {#each data.exerciseSplits as exerciseSplit}
+      <a class="btn rounded-md btn-primary h-fit" href="/exerciseSplits/view/{exerciseSplit._id}">
+        <div class="flex flex-col gap-1 py-2 w-full">
+          <div class="flex justify-between items-center">
+            <span class="text-lg font-semibold">{exerciseSplit.name}</span>
+            <span class="font-normal">
+              {getTotalSetsOfSplit(exerciseSplit.splitDays)} sets
+            </span>
+          </div>
+          <div class="flex flex-wrap w-full gap-1">
+            {#each exerciseSplit.splitDays as splitDay}
+              {#if splitDay}
+                <span class="badge">{splitDay.name}</span>
+              {:else}
+                <span class="badge badge-accent"></span>
+              {/if}
+            {/each}
+          </div>
         </div>
-        <div class="flex flex-wrap w-full gap-1">
-          {#each exerciseSplit.splitDays as splitDay}
-            {#if splitDay}
-              <span class="badge">{splitDay.name}</span>
-            {:else}
-              <span class="badge badge-accent"></span>
-            {/if}
-          {/each}
-        </div>
-      </div>
-    </a>
-  {/each}
-</div>
+      </a>
+    {/each}
+  </div>
+{:else}
+  <div class="flex flex-col bg-primary p-2 mb-auto rounded-md">
+    <span class="font-semibold text-warning text-lg">No splits created</span>
+    <span>Create one by clicking the button below</span>
+  </div>
+{/if}
 <a class="btn btn-accent mt-2" href="/exerciseSplits/templates">Create new split</a>
