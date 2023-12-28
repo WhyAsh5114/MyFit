4faa1f4ad64369ccddef5356ca78b9555d5738c6
WhyAsh5114 <whyash5114@gmail.com>
2023-12-28 20:32:59 +0530

    created separate stats and exercises components

---
+++ src/routes/exerciseSplits/view/[id]/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import ViewSplitExercises from "./ViewSplitExercises.svelte";
+  import ViewSplitStats from "./ViewSplitStats.svelte";
+
+  export let data;
+  $: ({ exerciseSplit } = data);
+
+  let currentSection: "Stats" | "Exercises" = "Stats";
+</script>
+
+<h2>View split</h2>
+
+<div class="join w-full grid grid-cols-2 mb-2">
+  <input
+    name="view"
+    class="join-item btn btn-primary checked:!bg-accent checked:!text-black"
+    aria-label="Stats"
+    checked
+    type="radio"
+    value="Stats"
+    bind:group={currentSection}
+  />
+  <input
+    name="view"
+    class="join-item btn btn-primary checked:!bg-accent checked:!text-black"
+    aria-label="Exercises"
+    type="radio"
+    value="Exercises"
+    bind:group={currentSection}
+  />
+</div>
+
+{#if currentSection === "Stats"}
+  <ViewSplitStats {exerciseSplit} />
+{:else}
+  <ViewSplitExercises {exerciseSplit} />
+{/if}
+
+<div class="join grid grid-cols-2 mt-auto">
+  <button class="join-item btn btn-error">Delete</button>
+  <button class="join-item btn btn-primary">Edit</button>
+</div>
---
+++ src/routes/exerciseSplits/view/[id]/ViewSplitExercises.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+  import ExerciseSplitTable from "../../ExerciseSplitTable.svelte";
+  import SplitDaySelector from "../../SplitDaySelector.svelte";
+
+  export let exerciseSplit: ExerciseSplit;
+  let selectedSplitDayIndex: number;
+  $: selectedSplitDayExercises = exerciseSplit.splitDays[selectedSplitDayIndex]?.exerciseTemplates;
+</script>
+
+<div class="flex flex-col h-px grow overflow-y-auto shadow-xl shadow-black -mt-2">
+  <SplitDaySelector exerciseSplitDays={exerciseSplit.splitDays} bind:selectedSplitDayIndex />
+  {#if selectedSplitDayExercises}
+    <ExerciseSplitTable exerciseTemplates={selectedSplitDayExercises} readOnly />
+  {/if}
+</div>
