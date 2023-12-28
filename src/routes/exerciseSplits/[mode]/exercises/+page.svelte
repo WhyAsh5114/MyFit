29a8df5befdc333c2d68ef1fcdd99138e137470b
WhyAsh5114 <whyash5114@gmail.com>
2023-12-28 15:43:32 +0530

    made the day selector a separate component

--- src/routes/exerciseSplits/[mode]/exercises/+page.svelte
+++ src/routes/exerciseSplits/[mode]/exercises/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
-  import ExerciseSplitTable from "./ExerciseSplitTable.svelte";
+  import SplitDaySelector from "../../SplitDaySelector.svelte";
+  import ExerciseSplitTable from "../../ExerciseSplitTable.svelte";
   import { exerciseSplitDays, splitName } from "../splitStore";
   import CutIcon from "virtual:icons/material-symbols/cut";
   import CopyIcon from "virtual:icons/material-symbols/content-copy";
@@ -106,69 +107,36 @@
 <h2><span class="capitalize">{params.mode}</span> exercise split</h2>
 <h3>Exercises</h3>
 
-<div class="collapse collapse-arrow rounded-md bg-primary my-2">
-  <input id="show-all-days" aria-label="show-all-days" checked type="checkbox" />
-  <div class="collapse-title text-xl font-semibold">
-    D{selectedSplitDayIndex + 1}
-    <span class="text-base font-normal ml-2">{selectedSplitDay?.name}</span>
-  </div>
-  <div class="collapse-content backdrop-brightness-50">
-    <div class="flex flex-wrap justify-center items-center gap-2 mt-4">
-      {#each $exerciseSplitDays as exerciseSplitDay, i}
-        <div class="join">
-          <span class="join-item btn btn-sm">D{i + 1}</span>
-          {#if exerciseSplitDay === null}
-            <input
-              name="options"
-              class="join-item btn btn-sm"
-              aria-label="Rest"
-              disabled
-              type="radio"
-            />
-          {:else}
-            <input
-              name="options"
-              class="join-item btn btn-sm btn-secondary checked:!text-black checked:!bg-accent"
-              aria-label={exerciseSplitDay.name}
-              type="radio"
-              value={i}
-              bind:group={selectedSplitDayIndex}
-            />
-          {/if}
-        </div>
-      {/each}
-    </div>
-  </div>
-</div>
+<SplitDaySelector exerciseSplitDays={$exerciseSplitDays} bind:selectedSplitDayIndex />
 
-{#key selectedSplitDayIndex}
-  {#if selectedSplitDay}
+{#if selectedSplitDay}
+  {#key selectedSplitDayIndex}
     <ExerciseSplitTable bind:exerciseTemplates={selectedSplitDay.exerciseTemplates} />
-    <div class="join grid grid-cols-3 mt-1 gap-1">
-      <button
-        class="join-item btn btn-error"
-        disabled={selectedSplitDay.exerciseTemplates.length === 0}
-        on:click={() => cutExercises()}
-      >
-        <CutIcon /> Cut
-      </button>
-      <button
-        class="join-item btn btn-primary"
-        disabled={selectedSplitDay.exerciseTemplates.length === 0}
-        on:click={() => copyExercises()}
-      >
-        <CopyIcon /> Copy
-      </button>
-      <button
-        class="join-item btn btn-primary"
-        disabled={copiedExercises.length === 0 || selectedSplitDay.exerciseTemplates.length !== 0}
-        on:click={() => pasteExercises()}
-      >
-        <PasteIcon /> Paste
-      </button>
-    </div>
-  {/if}
-{/key}
+  {/key}
+  <div class="join grid grid-cols-3 mt-1 gap-1">
+    <button
+      class="join-item btn btn-error"
+      disabled={selectedSplitDay.exerciseTemplates.length === 0}
+      on:click={() => cutExercises()}
+    >
+      <CutIcon /> Cut
+    </button>
+    <button
+      class="join-item btn btn-primary"
+      disabled={selectedSplitDay.exerciseTemplates.length === 0}
+      on:click={() => copyExercises()}
+    >
+      <CopyIcon /> Copy
+    </button>
+    <button
+      class="join-item btn btn-primary"
+      disabled={copiedExercises.length === 0 || selectedSplitDay.exerciseTemplates.length !== 0}
+      on:click={() => pasteExercises()}
+    >
+      <PasteIcon /> Paste
+    </button>
+  </div>
+{/if}
 
 <button class="btn btn-accent btn-block mt-2" on:click={submitSplit}>
   Create exercise split
