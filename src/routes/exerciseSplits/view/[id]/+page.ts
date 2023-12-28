e9765c85d9cc89c3dfec668554ffa341aef56563
WhyAsh5114 <whyash5114@gmail.com>
2023-12-28 19:54:16 +0530

    fixed type

--- src/routes/exerciseSplits/view/[id]/+page.ts
+++ src/routes/exerciseSplits/view/[id]/+page.ts
@@ -5,7 +5,7 @@ export const load = async ({ params, fetch }) => {
   const response = await fetch(`/api/exerciseSplits/${exerciseSplitId}`);
 
   if (response.ok) {
-    const exerciseSplit = (await response.json()) as ExerciseSplit;
+    const exerciseSplit = (await response.json()) as WithSID<ExerciseSplit>;
     return { exerciseSplit };
   } else {
     const statusCode = response.status as NumericRange<400, 599>;
