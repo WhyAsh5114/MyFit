084f5c12b85eb03553236bbfc63bc30943f88151
WhyAsh5114 <whyash5114@gmail.com>
2023-12-29 21:30:23 +0530

    added split deletion UI and functionality

--- src/routes/exerciseSplits/view/[id]/+page.svelte
+++ src/routes/exerciseSplits/view/[id]/+page.svelte
@@ -1,12 +1,60 @@
 <script lang="ts">
+  import { goto, invalidate } from "$app/navigation";
+  import MyModal from "$lib/components/MyModal.svelte";
   import ViewSplitExercises from "./ViewSplitExercises.svelte";
   import ViewSplitStats from "./ViewSplitStats.svelte";
 
   export let data;
   $: ({ exerciseSplit } = data);
 
+  let modal: HTMLDialogElement;
+  let modalTitle = "";
+  let modalText = "";
+  let modalOnClose = () => {};
+
+  let confirmDeleteModal: HTMLDialogElement;
   let currentSection: "Stats" | "Exercises" = "Stats";
+  let callingEndpoint = false;
+
+  async function deleteSplit() {
+    callingEndpoint = true;
+    const response = await fetch(`/api/exerciseSplits/${exerciseSplit._id}`, { method: "DELETE" });
+    modalTitle = response.ok ? "Success" : "Error";
+    modalText = await response.text();
+
+    if (response.ok) {
+      modalOnClose = async () => {
+        await invalidate(`/api/exerciseSplits/${exerciseSplit._id}`);
+        await goto("/exerciseSplits");
+      };
+    }
+
+    confirmDeleteModal.close();
+    modal.show();
+    callingEndpoint = false;
+  }
 </script>
+
+<MyModal {modalOnClose} title={modalTitle} bind:dialogElement={modal}>
+  {modalText}
+</MyModal>
+
+<MyModal title="Delete split?" bind:dialogElement={confirmDeleteModal}>
+  <p>
+    Are you sure you want to delete the split:
+    <span class="font-semibold">{exerciseSplit.name}</span>
+  </p>
+  <div class="join grid grid-cols-2 mt-4">
+    <button class="join-item btn">No, cancel</button>
+    <button class="join-item btn btn-error" disabled={callingEndpoint} on:click={deleteSplit}>
+      {#if !callingEndpoint}
+        Yes, delete
+      {:else}
+        <span class="loading loading-bars"></span>
+      {/if}
+    </button>
+  </div>
+</MyModal>
 
 <h2>View split</h2>
 
@@ -37,6 +85,6 @@
 {/if}
 
 <div class="join grid grid-cols-2 mt-auto">
-  <button class="join-item btn btn-error">Delete</button>
+  <button class="join-item btn btn-error" on:click={() => confirmDeleteModal.show()}>Delete</button>
   <button class="join-item btn btn-primary">Edit</button>
 </div>
