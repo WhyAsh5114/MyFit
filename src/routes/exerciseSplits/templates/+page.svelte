500ce8fff7583775d9152cd8d43b6ad9108bd870
WhyAsh5114 <whyash5114@gmail.com>
2024-01-01 20:10:14 +0530

    removed to-be-implemented attr and TODO

--- src/routes/exerciseSplits/templates/+page.svelte
+++ src/routes/exerciseSplits/templates/+page.svelte
@@ -1,30 +1,26 @@
 <script lang="ts">
-  // TODO: implement the disabled ones...
   const options = [
     {
       title: "Start from scratch",
       description: "Create a brand new exercise split from scratch",
-      link: "/exerciseSplits/new",
-      disabled: false
+      link: "/exerciseSplits/new"
     },
     {
       title: "Clone old splits",
       description: "Use one of your old splits as a template",
-      link: "/exerciseSplits/templates/clone",
-      disabled: true
+      link: "/exerciseSplits/templates/clone"
     },
     {
       title: "Use common splits",
       description: "Start from commonly used splits",
-      link: "/exerciseSplits/templates/common",
-      disabled: false
+      link: "/exerciseSplits/templates/common"
     }
   ];
 </script>
 
 <h2>New exercise split</h2>
-{#each options as { title, description, link, disabled }}
-  <a class="btn btn-primary mb-1 h-fit py-4" class:btn-disabled={disabled} href={link}>
+{#each options as { title, description, link }}
+  <a class="btn btn-primary mb-1 h-fit py-4" href={link}>
     <div class="flex flex-col w-full text-left gap-1">
       <span class="text-lg">{title}</span>
       <p class="font-normal">{description}</p>
