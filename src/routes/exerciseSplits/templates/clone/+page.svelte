93aecb8f4bec6a172a340dc32c57434c10f00f84
WhyAsh5114 <whyash5114@gmail.com>
2024-01-01 20:10:29 +0530

    added cloning page load and UI

---
+++ src/routes/exerciseSplits/templates/clone/+page.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+  import { getTotalSetsOfSplit } from "$lib/utils/exerciseSplits.js";
+  import SplitDaysSchedule from "../../SplitDaysSchedule.svelte";
+
+  export let data;
+  $: ({ exerciseSplits } = data);
+</script>
+
+<h2>Clone old splits</h2>
+<h3>Use one of your old splits as a template</h3>
+
+<div class="flex flex-col h-px grow overflow-y-auto gap-1">
+  {#each exerciseSplits as exerciseSplit}
+    <a
+      class="btn btn-primary rounded-md h-fit"
+      href="/exerciseSplits/new?cloneId={exerciseSplit._id}"
+    >
+      <div class="flex flex-col gap-1 py-2 w-full">
+        <div class="flex justify-between items-center">
+          <span class="text-lg font-semibold">{exerciseSplit.name}</span>
+          <span class="font-normal">
+            {getTotalSetsOfSplit(exerciseSplit.splitDays)} sets
+          </span>
+        </div>
+        <SplitDaysSchedule splitDays={exerciseSplit.splitDays} />
+      </div></a
+    >
+  {/each}
+</div>
