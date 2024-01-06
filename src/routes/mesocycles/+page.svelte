d9993673233a040a3a1b2b9f120eecd2c53fa431
WhyAsh5114 <whyash5114@gmail.com>
2024-01-06 18:03:25 +0530

    basic skeleton for mesocycles page

---
+++ src/routes/mesocycles/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  export let data;
+</script>
+
+<div class="flex flex-col gap-1 h-px grow">
+  {#each data.mesocycles as mesocycle}
+    {mesocycle}
+  {/each}
+</div>
+
+<a class="btn btn-accent mt-2" href="/mesocycles/new">Create new mesocycle</a>
