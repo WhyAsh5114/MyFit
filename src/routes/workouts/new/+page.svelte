7dc2c37986047fdc01902361a36b91817e483842
Yash Kolekar <71895020+WhyAsh5114@users.noreply.github.com>
2024-01-08 16:57:42 +0530

    bugfix typo

--- src/routes/workouts/new/+page.svelte
+++ src/routes/workouts/new/+page.svelte
@@ -56,7 +56,7 @@
   }
 
   let bodyweightInputValue = data.userBodyweight;
-  let editingBodyweightValue = bodyweightInputValue === null && bodyweightExercise;
+  let editingBodyweightValue = bodyweightInputValue === null && bodyweightExercises;
   async function submitForm() {
     if (todaysWorkout) {
       await goto("/workouts/new/exercises");
