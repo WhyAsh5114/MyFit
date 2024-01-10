db09f8c354681d42cfd6d1a81d5adb210575d1b0
WhyAsh5114 <whyash5114@gmail.com>
2024-01-10 19:50:56 +0530

    created POST endpoint for mesocycles

--- src/routes/api/mesocycles/+server.ts
+++ src/routes/api/mesocycles/+server.ts
@@ -1,2 +1,30 @@
+import clientPromise from "$lib/mongo/mongodb.js";
+import type { WithUserId } from "$lib/types/arrays";
+import { ObjectId } from "mongodb";
+
+const client = await clientPromise;
+
 export const GET = () => {};
-export const POST = () => {};
+
+export const POST = async ({ locals, request }) => {
+  const session = await locals.getSession();
+  if (!session?.user?.id) {
+    return new Response("Not logged in", { status: 403 });
+  }
+
+  const mesocycleWithoutTimestamp: Omit<Mesocycle, "startTimestamp"> = await request.json();
+  try {
+    await client
+      .db()
+      .collection<WithUserId<Mesocycle>>("mesocycles")
+      .insertOne({
+        ...mesocycleWithoutTimestamp,
+        userId: new ObjectId(session.user.id),
+        startTimestamp: Number(new Date())
+      });
+
+    return new Response("Mesocycle created successfully", { status: 200 });
+  } catch (error) {
+    return new Response(JSON.stringify(error), { status: 500 });
+  }
+};
