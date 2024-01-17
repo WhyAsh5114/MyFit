a3b6673a5fb9aacc9cee5f0933daaf44bd5d999b
WhyAsh5114 <whyash5114@gmail.com>
2024-01-17 20:15:10 +0530

    npm update

--- src/routes/exerciseSplits/+page.ts
+++ src/routes/exerciseSplits/+page.ts
@@ -1,6 +1,6 @@
 export const load = async ({ fetch }) => {
   let exerciseSplits: WithSID<ExerciseSplit>[] = [];
-  
+
   const response = await fetch("/api/exerciseSplits");
   if (response.ok) {
     exerciseSplits = (await response.json()) as WithSID<ExerciseSplit>[];
