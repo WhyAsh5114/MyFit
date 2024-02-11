2ac745676cae49df14f21c76126c554e70ba1b33
WhyAsh5114 <whyash5114@gmail.com>
2024-02-11 21:30:22 +0530

    added a comparer and emptySplit

--- src/routes/exerciseSplits/[mode]/splitStore.ts
+++ src/routes/exerciseSplits/[mode]/splitStore.ts
@@ -1,25 +1,41 @@
 import { persisted } from "svelte-persisted-store";
-import type { Writable } from "svelte/store";
+import { get, type Writable } from "svelte/store";
 
+export const emptyExerciseSplitStore = {
+  name: "",
+  splitDays: Array.from({ length: 7 }, () => ({ name: "", exerciseTemplates: [] }))
+};
+
+export const originalExerciseSplit: Writable<ExerciseSplit> = persisted(
+  "originalExerciseSplit",
+  emptyExerciseSplitStore
+);
 export const splitName = persisted("splitName", "");
 export const splitStructure: Writable<(string | null)[]> = persisted(
   "splitStructure",
   Array(7).fill("")
 );
 export const exerciseSplitDays: Writable<ExerciseSplit["splitDays"]> = persisted(
-  "exerciseSplit",
-  []
+  "exerciseSplitDays",
+  Array.from({ length: 7 }, () => ({ name: "", exerciseTemplates: [] }))
 );
 export const editingSplitId: Writable<string | null> = persisted("editingSplitId", null);
 
-export function clearExerciseSplitStores() {
-  splitName.set("");
-  splitStructure.set(Array(7).fill(""));
-  exerciseSplitDays.set([]);
-}
-
 export function setExerciseSplitStores(exerciseSplit: ExerciseSplit) {
+  originalExerciseSplit.set(exerciseSplit);
   splitName.set(exerciseSplit.name);
   splitStructure.set(exerciseSplit.splitDays.map((splitDay) => splitDay?.name ?? null));
   exerciseSplitDays.set(exerciseSplit.splitDays);
+}
+
+export function didExerciseSplitStoresChange() {
+  const exerciseSplit = get(originalExerciseSplit);
+  const _splitStructure = exerciseSplit.splitDays.map((splitDay) => splitDay?.name ?? null);
+
+  if (get(splitName) !== exerciseSplit.name) return true;
+  if (JSON.stringify(get(splitStructure)) !== JSON.stringify(_splitStructure)) return true;
+  if (JSON.stringify(get(exerciseSplitDays)) !== JSON.stringify(exerciseSplit.splitDays))
+    return true;
+
+  return false;
 }
