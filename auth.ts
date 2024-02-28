903ca64320db85ee7f60c60f024bed53eaac9daf
WhyAsh5114 <whyash5114@gmail.com>
2024-02-28 21:29:55 +0530

    attached id to session

--- auth.ts
+++ auth.ts
@@ -1,8 +1,18 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import Google from "@auth/sveltekit/providers/google";
+import { MongoDBAdapter } from "@auth/mongodb-adapter";
+import clientPromise from "$lib/mongo/mongodb";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
+  adapter: MongoDBAdapter(clientPromise),
   basePath: "/auth",
   providers: [Google],
+  callbacks: {
+    // Attach mongoDB user document ID for easier queries
+    async session({ session, user }) {
+      session.user.id = user.id;
+      return session;
+    }
+  },
   trustHost: true
 });
