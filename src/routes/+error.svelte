9f6fa5b22214b3d047e7259f9625df77c0737904
WhyAsh5114 <whyash5114@gmail.com>
2024-03-02 16:31:32 +0530

    changed text-error to text-red-500

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -4,6 +4,6 @@
 </script>
 
 <div class="flex flex-col grow justify-center items-center gap-5">
-  <span class="font-extrabold text-error text-7xl">{status}</span>
+  <span class="font-extrabold text-red-500 text-7xl">{status}</span>
   <span class="text-lg">{error?.message}</span>
 </div>
--- src/routes/offline/+page.svelte
+++ src/routes/offline/+page.svelte
@@ -1,2 +1,2 @@
-<h2 class="text-error">You're offline!</h2>
+<h2 class="text-red-500">You're offline!</h2>
 <h3>Some features of the app might not work as expected</h3>
