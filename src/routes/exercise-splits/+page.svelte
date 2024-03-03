c08544c55c89b9ec39efceff0382b0ae5f6b7237
WhyAsh5114 <whyash5114@gmail.com>
2024-03-03 15:14:06 +0530

    added GET UI for exercise splits

--- src/routes/exercise-splits/+page.svelte
+++ src/routes/exercise-splits/+page.svelte
@@ -3,17 +3,55 @@
   import type { WithId } from "mongodb";
   import { Button } from "$lib/components/ui/button";
   import * as Drawer from "$lib/components/ui/drawer";
+  import { Badge } from "$lib/components/ui/badge";
+  import { getTotalSetsOfSplit } from "$lib/utils/exerciseSplits";
+  import { Skeleton } from "$lib/components/ui/skeleton";
 
-  let exerciseSplits: WithId<ExerciseSplit>[] = [];
+  let exerciseSplits: WithId<ExerciseSplit>[];
   onMount(async () => {
-    const response = await fetch("/api/exercise-splits");
-    exerciseSplits = await response.json();
+    try {
+      const response = await fetch("/api/exercise-splits");
+      exerciseSplits = await response.json();
+    } catch (error) {
+      exerciseSplits = [];
+    }
   });
 </script>
 
 <h2>Exercise splits</h2>
 
 <div class="h-px flex flex-col grow overflow-y-auto gap-1">
+  {#if exerciseSplits}
+    {#each exerciseSplits as exerciseSplit}
+      <div class="border p-2 rounded-md flex flex-col gap-2">
+        <div class="flex items-center justify-between">
+          <span class="text-lg font-semibold">{exerciseSplit.name}</span>
+          <Badge>{getTotalSetsOfSplit(exerciseSplit.splitDays)} sets</Badge>
+        </div>
+        <div class="flex w-full flex-wrap gap-1">
+          {#each exerciseSplit.splitDays as splitDay}
+            <Badge variant={splitDay ? "secondary" : "outline"}>
+              {splitDay?.name ?? "Rest"}
+            </Badge>
+          {/each}
+        </div>
+      </div>
+    {/each}
+  {:else}
+    {#each Array(5) as i}
+      <div class="border p-2 rounded-md flex flex-col gap-2">
+        <div class="flex items-center justify-between">
+          <Skeleton class="h-7 w-28" />
+          <Skeleton class="h-[22px] w-16 rounded-full" />
+        </div>
+        <div class="flex w-full flex-wrap gap-1">
+          {#each Array(7) as j}
+            <Skeleton class="h-[22px] w-16 rounded-full" />
+          {/each}
+        </div>
+      </div>
+    {/each}
+  {/if}
 </div>
 
 <Drawer.Root>
