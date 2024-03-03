bea8d05db3f5bd65af23f1361deb700349dac85a
WhyAsh5114 <whyash5114@gmail.com>
2024-03-03 15:13:51 +0530

    temporarily remove prerender

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,6 +1,7 @@
-export const prerender = true;
+// TODO: enable again after issue https://github.com/nextauthjs/next-auth/issues/9809 fixed
+export const prerender = false;
 
 export const load = async ({ locals }) => {
-  // const session = await locals.auth();
-  return { session: null };
+  const session = await locals.auth();
+  return { session };
 };
