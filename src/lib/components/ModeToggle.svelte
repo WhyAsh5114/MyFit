f9a6dbf46b9f2eaddee71902e2bbf0c45f222bd1
WhyAsh5114 <whyash5114@gmail.com>
2024-03-05 22:02:54 +0530

    UI changes

--- src/lib/components/ModeToggle.svelte
+++ src/lib/components/ModeToggle.svelte
@@ -9,7 +9,7 @@
 
 <DropdownMenu.Root>
 	<DropdownMenu.Trigger asChild let:builder>
-		<Button builders={[builder]} variant="outline" size="icon">
+		<Button builders={[builder]} variant="ghost" size="icon">
 			<Sun
 				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
 			/>
