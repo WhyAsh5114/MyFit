d81c44d779afb4c48a686a1e01baa7728e10089a
WhyAsh5114 <whyash5114@gmail.com>
2024-03-06 21:50:26 +0530

    added login and logout functionality for large navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -7,6 +7,15 @@
 	import Menu from 'lucide-svelte/icons/menu';
 	import ModeToggle from '$lib/components/ModeToggle.svelte';
 	import { Button } from '$lib/components/ui/button';
+	import { page } from '$app/stores';
+	import { signIn, signOut } from '@auth/sveltekit/client';
+	import GoogleIcon from 'virtual:icons/mdi/google';
+	import GitHubIcon from 'virtual:icons/mdi/github';
+
+	const providerList = [
+		{ name: 'google', logo: GoogleIcon },
+		{ name: 'github', logo: GitHubIcon }
+	];
 </script>
 
 <ModeWatcher />
@@ -35,8 +44,6 @@
 			</DropdownMenu.Trigger>
 			<DropdownMenu.Content>
 				<DropdownMenu.Group>
-					<DropdownMenu.Label>My Account</DropdownMenu.Label>
-					<DropdownMenu.Separator />
 					<DropdownMenu.Item>Profile</DropdownMenu.Item>
 					<DropdownMenu.Item>Settings</DropdownMenu.Item>
 					<DropdownMenu.Item class="text-red-500">Log out</DropdownMenu.Item>
@@ -44,7 +51,7 @@
 			</DropdownMenu.Content>
 		</DropdownMenu.Root>
 	</div>
-	<div class="hidden h-screen flex-col p-10 lg:flex w-full">
+	<div class="hidden h-screen w-full flex-col p-10 lg:flex">
 		<Button variant="link" class="justify-start">
 			<a class="flex items-center gap-2" href="/">
 				<img src="/favicon.webp" alt="MyFit logo" width={72} height={72} />
@@ -68,24 +75,54 @@
 				</Button>
 			</li>
 		</ul>
-		<DropdownMenu.Root>
-			<DropdownMenu.Trigger asChild let:builder>
-				<Button builders={[builder]} variant="outline" class="h-14 justify-evenly">
-					<Avatar.Root class="h-9 w-9">
-						<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
-						<Avatar.Fallback>CN</Avatar.Fallback>
-					</Avatar.Root>
-					<span class="text-base font-medium">User name</span>
-				</Button>
-			</DropdownMenu.Trigger>
-			<DropdownMenu.Content align="start">
-				<DropdownMenu.Group>
-					<DropdownMenu.Item>Profile</DropdownMenu.Item>
-					<DropdownMenu.Item>Settings</DropdownMenu.Item>
-					<DropdownMenu.Item class="text-red-500">Log out</DropdownMenu.Item>
-				</DropdownMenu.Group>
-			</DropdownMenu.Content>
-		</DropdownMenu.Root>
+		{#if $page.data.session}
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger asChild let:builder>
+					<Button builders={[builder]} variant="secondary" class="h-14 justify-around">
+						<Avatar.Root class="h-9 w-9">
+							<Avatar.Image src={$page.data.session.user?.image} alt="Profile picture" />
+							<Avatar.Fallback>CN</Avatar.Fallback>
+						</Avatar.Root>
+						<span class="text-base font-semibold">{$page.data.session.user?.name}</span>
+					</Button>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content align="start">
+					<DropdownMenu.Group>
+						<DropdownMenu.Item>Profile</DropdownMenu.Item>
+						<DropdownMenu.Item>Settings</DropdownMenu.Item>
+						<DropdownMenu.Item class="text-red-500" on:click={() => signOut()}>
+							Log out
+						</DropdownMenu.Item>
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		{:else}
+			<DropdownMenu.Root>
+				<DropdownMenu.Trigger asChild let:builder>
+					<Button variant="secondary" builders={[builder]} class="h-14 justify-around text-base">
+						Log in
+					</Button>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content>
+					<DropdownMenu.Group>
+						{#each providerList as { name, logo }}
+							<DropdownMenu.Item>
+								<Button
+									variant="ghost"
+									class="gap-2"
+									on:click={() => {
+										signIn(name, { callbackUrl: $page.url.searchParams.get('callbackURL') || '' });
+									}}
+								>
+									<svelte:component this={logo} class="h-7 w-7" />
+									<span>Sign in with <span class="capitalize">{name}</span></span>
+								</Button>
+							</DropdownMenu.Item>
+						{/each}
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		{/if}
 	</div>
 </header>
 <main class="mx-auto flex h-px max-w-2xl grow flex-col overflow-y-auto px-2 py-6 lg:h-screen">
