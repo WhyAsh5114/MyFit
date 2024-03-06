36c78fec01dfd892c57ce1f111198df65f434a95
WhyAsh5114 <whyash5114@gmail.com>
2024-03-06 19:44:18 +0530

    setup basic large screen navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -6,6 +6,7 @@
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import Menu from 'lucide-svelte/icons/menu';
 	import ModeToggle from '$lib/components/ModeToggle.svelte';
+	import { Button } from '$lib/components/ui/button';
 </script>
 
 <ModeWatcher />
@@ -17,7 +18,7 @@
 			</Sheet.Trigger>
 			<Sheet.Content side="left">
 				<Sheet.Header>
-					<Sheet.Title>MyFit</Sheet.Title>
+					<Sheet.Title><h1>MyFit</h1></Sheet.Title>
 				</Sheet.Header>
 			</Sheet.Content>
 		</Sheet.Root>
@@ -43,7 +44,49 @@
 			</DropdownMenu.Content>
 		</DropdownMenu.Root>
 	</div>
-	<div class="hidden h-screen flex-col lg:flex">horizontal navbar</div>
+	<div class="hidden h-screen flex-col p-10 lg:flex w-full">
+		<Button variant="link" class="justify-start">
+			<a class="flex items-center gap-2" href="/">
+				<img src="/favicon.webp" alt="MyFit logo" width={72} height={72} />
+				<h1 class="text-4xl font-bold">MyFit</h1>
+			</a>
+		</Button>
+		<ul class="mb-auto mt-8">
+			<li>
+				<Button variant="link" class="text-base">
+					<a href="/exercise-splits">Workouts</a>
+				</Button>
+			</li>
+			<li>
+				<Button variant="link" class="text-base">
+					<a href="/exercise-splits">Mesocycles</a>
+				</Button>
+			</li>
+			<li>
+				<Button variant="link" class="text-base">
+					<a href="/exercise-splits">Exercise splits</a>
+				</Button>
+			</li>
+		</ul>
+		<DropdownMenu.Root>
+			<DropdownMenu.Trigger asChild let:builder>
+				<Button builders={[builder]} variant="outline" class="h-14 justify-evenly">
+					<Avatar.Root class="h-9 w-9">
+						<Avatar.Image src="https://github.com/shadcn.png" alt="@shadcn" />
+						<Avatar.Fallback>CN</Avatar.Fallback>
+					</Avatar.Root>
+					<span class="text-base font-medium">User name</span>
+				</Button>
+			</DropdownMenu.Trigger>
+			<DropdownMenu.Content align="start">
+				<DropdownMenu.Group>
+					<DropdownMenu.Item>Profile</DropdownMenu.Item>
+					<DropdownMenu.Item>Settings</DropdownMenu.Item>
+					<DropdownMenu.Item class="text-red-500">Log out</DropdownMenu.Item>
+				</DropdownMenu.Group>
+			</DropdownMenu.Content>
+		</DropdownMenu.Root>
+	</div>
 </header>
 <main class="mx-auto flex h-px max-w-2xl grow flex-col overflow-y-auto px-2 py-6 lg:h-screen">
 	<slot />
