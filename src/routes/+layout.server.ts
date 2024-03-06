1c5fe6e9f1d0dc05073a1a644d5b089cf1d287f3
WhyAsh5114 <whyash5114@gmail.com>
2024-03-06 21:43:27 +0530

    added session logic

---
+++ src/routes/+layout.server.ts
@@ -0,0 +1,7 @@
+// TODO: enable again after issue https://github.com/nextauthjs/next-auth/issues/9809 fixed
+export const prerender = false;
+
+export const load = async ({ locals }) => {
+	const session = await locals.auth();
+	return { session };
+};
