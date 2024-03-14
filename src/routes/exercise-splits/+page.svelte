74d8e6a916897ec567617c3aa85a09cee7d7f837
WhyAsh5114 <whyash5114@gmail.com>
2024-03-14 21:35:53 +0530

    setup basic UI for exercise-splits page

---
+++ src/routes/exercise-splits/+page.svelte
@@ -0,0 +1,47 @@
+<script lang="ts">
+	import H2 from '$lib/components/ui/typography/H2.svelte';
+	import { Input } from '$lib/components/ui/input';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Pagination from '$lib/components/ui/pagination';
+	import AddIcon from 'virtual:icons/material-symbols/add';
+	import ChevronLeft from 'lucide-svelte/icons/chevron-left';
+	import ChevronRight from 'lucide-svelte/icons/chevron-right';
+</script>
+
+<H2 text="Exercise splits" />
+<div class="flex grow flex-col gap-2">
+	<div class="flex gap-2">
+		<Input id="search-exercise-splits" placeholder="Search" />
+		<Button><AddIcon class="h-5 w-5" /></Button>
+	</div>
+	<div class="flex h-px grow flex-col overflow-y-auto border"></div>
+	<Pagination.Root count={50} perPage={5} siblingCount={0} let:pages let:currentPage>
+		<Pagination.Content>
+			<Pagination.Item>
+				<Pagination.PrevButton>
+					<ChevronLeft class="h-4 w-4" />
+					<span class="hidden sm:block">Previous</span>
+				</Pagination.PrevButton>
+			</Pagination.Item>
+			{#each pages as page (page.key)}
+				{#if page.type === 'ellipsis'}
+					<Pagination.Item>
+						<Pagination.Ellipsis />
+					</Pagination.Item>
+				{:else}
+					<Pagination.Item>
+						<Pagination.Link {page} isActive={currentPage == page.value}>
+							{page.value}
+						</Pagination.Link>
+					</Pagination.Item>
+				{/if}
+			{/each}
+			<Pagination.Item>
+				<Pagination.NextButton>
+					<span class="hidden sm:block">Next</span>
+					<ChevronRight class="h-4 w-4" />
+				</Pagination.NextButton>
+			</Pagination.Item>
+		</Pagination.Content>
+	</Pagination.Root>
+</div>
