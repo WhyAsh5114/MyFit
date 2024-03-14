604b1f4b8c15a28fbb37460791a64e8382607fd2
WhyAsh5114 <whyash5114@gmail.com>
2024-03-14 21:01:41 +0530

    UI changes added some bottom margin for H2

--- src/lib/components/ui/typography/H2.svelte
+++ src/lib/components/ui/typography/H2.svelte
@@ -3,7 +3,7 @@
 </script>
 
 <h2
-	class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+	class="scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0 mb-4"
 >
 	{text}
 </h2>
