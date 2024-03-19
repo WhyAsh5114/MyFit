3d95f8aa2dc9ce384d5f1604c2517f7446113f4f
WhyAsh5114 <whyash5114@gmail.com>
2024-03-19 13:33:30 +0530

    added types file

---
+++ src/lib/types.ts
@@ -0,0 +1,42 @@
+const MuscleGroups = [
+	'Chest',
+	'Front delts',
+	'Side delts',
+	'Rear delts',
+	'Lats',
+	'Traps',
+	'Triceps',
+	'Biceps',
+	'Forearms',
+	'Quads',
+	'Hamstrings',
+	'Glutes',
+	'Calves',
+	'Abs'
+] as const;
+type MuscleGroup = (typeof MuscleGroups)[number];
+
+const SetTypes = ['straight', 'drop', 'down', 'top', 'myorep', 'myorep match', 'giant'] as const;
+
+type ExerciseSetType = (typeof SetTypes)[number];
+
+type ExerciseSplit = {
+	name: string;
+	splitDays: (ExerciseSplitDay | null)[];
+};
+
+type ExerciseSplitDay = {
+	name: string;
+	exerciseTemplates: ExerciseTemplate[];
+};
+
+type ExerciseTemplate = {
+	name: string;
+	sets: number;
+	targetMuscleGroup: MuscleGroup;
+	setType: ExerciseSetType;
+	repRangeStart: number;
+	repRangeEnd: number;
+	involvesBodyweight: boolean;
+	note?: string;
+};
