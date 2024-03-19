42564c3de8e7ea555f192be8a505f2797e206b1c
WhyAsh5114 <whyash5114@gmail.com>
2024-03-19 13:05:07 +0530

    added H3 and some new exercise splits page

---
+++ src/lib/components/ui/typography/H3.svelte
@@ -0,0 +1,3 @@
+<h3 class="-mt-3 scroll-m-20 text-2xl font-semibold tracking-tight mb-4">
+	<slot />
+</h3>
---
+++ src/routes/exercise-splits/new/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import H2 from '$lib/components/ui/typography/H2.svelte';
+	import H3 from '$lib/components/ui/typography/H3.svelte';
+</script>
+
+<H2>New exercise split</H2>
+<H3>Starting from scratch</H3>
+
+{#if true}
+	<Button class="mt-auto">Create new split</Button>
+{:else}
+	<div class="grid grid-cols-2 gap-2 mt-auto">
+		<Button variant="destructive">Overwrite changes</Button>
+		<Button>Continue editing</Button>
+	</div>
+{/if}
