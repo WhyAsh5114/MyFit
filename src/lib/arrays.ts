05d5a334105602b8d1bd18a84c9c5cb17a535705
WhyAsh5114 <whyash5114@gmail.com>
2024-03-19 16:24:05 +0530

    added arrays file

---
+++ src/lib/arrays.ts
@@ -0,0 +1,26 @@
+export const muscleGroups: typeof MuscleGroups = [
+	'Chest',
+	'Front delts',
+	'Side delts',
+	'Rear delts',
+	'Lats',
+	'Traps',
+	'Triceps',
+	'Biceps',
+	'Forearms',
+	'Quads',
+	'Hamstrings',
+	'Glutes',
+	'Calves',
+	'Abs'
+] as const;
+
+export const setTypes: typeof SetTypes = [
+	'straight',
+	'drop',
+	'down',
+	'top',
+	'myorep',
+	'myorep match',
+	'giant'
+] as const;
