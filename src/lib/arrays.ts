36ef3dbc7f9f1151ae548fa27afa4b5a4808e07e
WhyAsh5114 <whyash5114@gmail.com>
2024-03-19 20:41:00 +0530

    added sample exercises

--- src/lib/arrays.ts
+++ src/lib/arrays.ts
@@ -24,3 +24,277 @@ export const setTypes: typeof SetTypes = [
 	'myorep match',
 	'giant'
 ] as const;
+
+export const exerciseListByMuscleGroup: {
+	muscleGroup: MuscleGroup;
+	exercises: ExerciseTemplate[];
+}[] = [
+	{
+		muscleGroup: 'Chest',
+		exercises: [
+			{
+				name: 'Barbell bench press',
+				repRangeStart: 5,
+				repRangeEnd: 10,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Chest',
+				involvesBodyweight: false
+			},
+			{
+				name: 'Dumbbell flyes',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Chest',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Front delts',
+		exercises: [
+			{
+				name: 'Overhead dumbbell press',
+				repRangeStart: 5,
+				repRangeEnd: 10,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Front delts',
+				involvesBodyweight: false
+			},
+			{
+				name: 'Front raise',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Front delts',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Side delts',
+		exercises: [
+			{
+				name: 'Lateral raises',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Side delts',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Rear delts',
+		exercises: [
+			{
+				name: 'Face pulls',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Rear delts',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Lats',
+		exercises: [
+			{
+				name: 'Pull-ups',
+				repRangeStart: 5,
+				repRangeEnd: 10,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Lats',
+				involvesBodyweight: true
+			},
+			{
+				name: 'Lat pulldowns',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Lats',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Traps',
+		exercises: [
+			{
+				name: 'Barbell shrugs',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Traps',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Triceps',
+		exercises: [
+			{
+				name: 'Close-grip bench press',
+				repRangeStart: 5,
+				repRangeEnd: 10,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Triceps',
+				involvesBodyweight: false
+			},
+			{
+				name: 'Tricep dips',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Triceps',
+				involvesBodyweight: true
+			}
+		]
+	},
+	{
+		muscleGroup: 'Biceps',
+		exercises: [
+			{
+				name: 'Barbell curls',
+				repRangeStart: 5,
+				repRangeEnd: 10,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Biceps',
+				involvesBodyweight: false
+			},
+			{
+				name: 'Dumbbell hammer curls',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Biceps',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Forearms',
+		exercises: [
+			{
+				name: 'Wrist curls',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Forearms',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Quads',
+		exercises: [
+			{
+				name: 'Barbell squats',
+				repRangeStart: 5,
+				repRangeEnd: 10,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Quads',
+				involvesBodyweight: false
+			},
+			{
+				name: 'Leg press',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Quads',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Hamstrings',
+		exercises: [
+			{
+				name: 'Romanian deadlifts',
+				repRangeStart: 5,
+				repRangeEnd: 10,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Hamstrings',
+				involvesBodyweight: false
+			},
+			{
+				name: 'Leg curls',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Hamstrings',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Glutes',
+		exercises: [
+			{
+				name: 'Hip thrusts',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Glutes',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Calves',
+		exercises: [
+			{
+				name: 'Standing calf raises',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Calves',
+				involvesBodyweight: false
+			}
+		]
+	},
+	{
+		muscleGroup: 'Abs',
+		exercises: [
+			{
+				name: 'Crunches',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Abs',
+				involvesBodyweight: false
+			},
+			{
+				name: 'Leg raises',
+				repRangeStart: 10,
+				repRangeEnd: 20,
+				setType: 'straight',
+				sets: 3,
+				targetMuscleGroup: 'Abs',
+				involvesBodyweight: false
+			}
+		]
+	}
+];
