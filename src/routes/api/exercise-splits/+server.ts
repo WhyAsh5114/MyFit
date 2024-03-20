520ca6c092bc8bbac6450414ca18019a7c0e64e8
WhyAsh5114 <whyash5114@gmail.com>
2024-03-20 14:51:32 +0530

    implemented pagination

--- src/routes/api/exercise-splits/+server.ts
+++ src/routes/api/exercise-splits/+server.ts
@@ -1,20 +1,31 @@
+import { EXERCISE_SPLITS_PER_PAGE } from '$lib/constants.js';
 import clientPromise from '$lib/mongo/mongodb.js';
 const client = await clientPromise;
 
-export const GET = async ({ locals }) => {
+export const GET = async ({ locals, url }) => {
 	const session = await locals.auth();
 	if (!session?.user?.id) {
 		return new Response('Not logged in', { status: 403 });
 	}
 
+	const pageNumber = parseInt(url.searchParams.get('page') ?? '') - 1;
+	if (isNaN(pageNumber) || pageNumber < 0)
+		return new Response('Invalid page number', { status: 400 });
+
 	try {
+		const exerciseSplitsCount = await client.db().collection('exerciseSplits').countDocuments();
 		const exerciseSplits = await client
 			.db()
 			.collection<WithUID<ExerciseSplit>>('exerciseSplits')
 			.find({ userId: session.user.id })
+			.skip(EXERCISE_SPLITS_PER_PAGE * pageNumber)
+			.limit(EXERCISE_SPLITS_PER_PAGE)
 			.toArray();
 
-		return new Response(JSON.stringify(exerciseSplits), { status: 200 });
+		return new Response(
+			JSON.stringify({ exerciseSplits, exerciseSplitsCount }),
+			{ status: 200 }
+		);
 	} catch (error) {
 		return new Response(JSON.stringify(error), { status: 500 });
 	}
--- src/routes/exercise-splits/+page.svelte
+++ src/routes/exercise-splits/+page.svelte
@@ -12,13 +12,23 @@
 	import { onMount } from 'svelte';
 	import { toast } from 'svelte-sonner';
 	import { getTotalSetsOfSplit } from '$lib/utils/exerciseSplits';
+	import { EXERCISE_SPLITS_PER_PAGE } from '$lib/constants';
 
-	let exerciseSplits: WithSID<ExerciseSplit>[];
-	onMount(async () => {
-		const response = await fetch('/api/exercise-splits');
-		if (response.ok) exerciseSplits = await response.json();
-		else toast.error('Error', { description: await response.text() });
-	});
+	let exerciseSplitsCount: number;
+	let exerciseSplits: WithSID<ExerciseSplit>[] | null | undefined;
+
+	onMount(() => loadExerciseSplits(1));
+
+	async function loadExerciseSplits(pageNumber: number) {
+		exerciseSplits = undefined;
+		const response = await fetch(`/api/exercise-splits?page=${pageNumber}`);
+		if (response.ok) {
+			({ exerciseSplits, exerciseSplitsCount } = await response.json());
+		} else {
+			toast.error(`Error ${response.status}`, { description: await response.text() });
+			exerciseSplits = null;
+		}
+	}
 </script>
 
 <H2>Exercise splits</H2>
@@ -46,9 +56,9 @@
 				<div class="flex flex-col gap-2 rounded-md border p-2">
 					<div class="flex items-center justify-between">
 						<span class="text-lg font-semibold">{exerciseSplit.name}</span>
-						<span class="text-sm text-muted-foreground"
-							>{getTotalSetsOfSplit(exerciseSplit.splitDays)} sets</span
-						>
+						<span class="text-sm text-muted-foreground">
+							{getTotalSetsOfSplit(exerciseSplit.splitDays)} sets
+						</span>
 					</div>
 					<div class="flex w-full flex-wrap gap-1">
 						{#each exerciseSplit.splitDays as splitDay}
@@ -59,15 +69,15 @@
 					</div>
 				</div>
 			{/each}
-		{:else}
-			{#each Array(5) as i}
+		{:else if exerciseSplits === undefined}
+			{#each Array(EXERCISE_SPLITS_PER_PAGE) as _}
 				<div class="flex flex-col gap-2 rounded-md border p-2">
 					<div class="flex items-center justify-between">
 						<Skeleton class="h-7 w-28" />
 						<Skeleton class="h-[22px] w-16 rounded-full" />
 					</div>
 					<div class="flex w-full flex-wrap gap-1">
-						{#each Array(7) as j}
+						{#each Array(7) as __}
 							<Skeleton class="h-[22px] w-16 rounded-full" />
 						{/each}
 					</div>
@@ -75,33 +85,45 @@
 			{/each}
 		{/if}
 	</div>
-	<Pagination.Root count={50} perPage={5} siblingCount={0} let:pages let:currentPage>
-		<Pagination.Content>
-			<Pagination.Item>
-				<Pagination.PrevButton>
-					<ChevronLeft class="h-4 w-4" />
-					<span class="hidden sm:block">Previous</span>
-				</Pagination.PrevButton>
-			</Pagination.Item>
-			{#each pages as page (page.key)}
-				{#if page.type === 'ellipsis'}
-					<Pagination.Item>
-						<Pagination.Ellipsis />
-					</Pagination.Item>
-				{:else}
-					<Pagination.Item>
-						<Pagination.Link {page} isActive={currentPage == page.value}>
-							{page.value}
-						</Pagination.Link>
-					</Pagination.Item>
-				{/if}
-			{/each}
-			<Pagination.Item>
-				<Pagination.NextButton>
-					<span class="hidden sm:block">Next</span>
-					<ChevronRight class="h-4 w-4" />
-				</Pagination.NextButton>
-			</Pagination.Item>
-		</Pagination.Content>
-	</Pagination.Root>
+
+	{#if exerciseSplitsCount !== undefined}
+		<Pagination.Root
+			count={exerciseSplitsCount}
+			perPage={EXERCISE_SPLITS_PER_PAGE}
+			siblingCount={0}
+			onPageChange={(pageNumber) => {
+				loadExerciseSplits(pageNumber);
+			}}
+			let:pages
+			let:currentPage
+		>
+			<Pagination.Content>
+				<Pagination.Item>
+					<Pagination.PrevButton>
+						<ChevronLeft class="h-4 w-4" />
+						<span class="hidden sm:block">Previous</span>
+					</Pagination.PrevButton>
+				</Pagination.Item>
+				{#each pages as page (page.key)}
+					{#if page.type === 'ellipsis'}
+						<Pagination.Item>
+							<Pagination.Ellipsis />
+						</Pagination.Item>
+					{:else}
+						<Pagination.Item>
+							<Pagination.Link {page} isActive={currentPage == page.value}>
+								{page.value}
+							</Pagination.Link>
+						</Pagination.Item>
+					{/if}
+				{/each}
+				<Pagination.Item>
+					<Pagination.NextButton>
+						<span class="hidden sm:block">Next</span>
+						<ChevronRight class="h-4 w-4" />
+					</Pagination.NextButton>
+				</Pagination.Item>
+			</Pagination.Content>
+		</Pagination.Root>
+	{/if}
 </div>
