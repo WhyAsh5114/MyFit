ecc7156d21742323a61300c829f78b87423caf98
WhyAsh5114 <whyash5114@gmail.com>
2024-03-20 17:37:46 +0530

    added readOnly option and prop to TemplateCard

--- src/routes/exercise-splits/(components)/ExerciseTemplateCard.svelte
+++ src/routes/exercise-splits/(components)/ExerciseTemplateCard.svelte
@@ -5,6 +5,7 @@
 	import EditIcon from 'virtual:icons/material-symbols/edit';
 	import DeleteIcon from 'virtual:icons/material-symbols/delete';
 
+	export let readOnly = false;
 	export let idx: number;
 	export let exerciseTemplate: ExerciseTemplate;
 	export let openEditExercise: (idx: number) => void;
@@ -19,36 +20,38 @@
 <div class="flex flex-col gap-0.5 rounded-md border bg-background/50 p-2 backdrop-blur-sm">
 	<div class="flex items-center gap-0.5">
 		<span class="mr-auto truncate text-sm">{exerciseTemplate.name}</span>
-		<DropdownMenu.Root open={isContextMenuOpen} onOpenChange={(v) => (isContextMenuOpen = v)}>
-			<DropdownMenu.Trigger asChild let:builder>
-				<button
-					use:builder.action
-					{...builder}
-					class="px-0.5 py-0"
-					on:mousedown={startDrag}
-					on:touchstart={startDrag}
-					on:keydown={handleKeyDown}
-				>
-					<MenuIcon class="h-4 w-4" />
-				</button>
-			</DropdownMenu.Trigger>
-			<DropdownMenu.Content align="end">
-				<DropdownMenu.Group>
-					<DropdownMenu.Item on:click={() => openEditExercise(idx)} class="gap-2">
-						<EditIcon /> Edit
-					</DropdownMenu.Item>
-					<DropdownMenu.Item
-						class="gap-2 text-red-500"
-						on:click={() => {
-							deleteExercise(idx);
-							isContextMenuOpen = false;
-						}}
+		{#if !readOnly}
+			<DropdownMenu.Root open={isContextMenuOpen} onOpenChange={(v) => (isContextMenuOpen = v)}>
+				<DropdownMenu.Trigger asChild let:builder>
+					<button
+						use:builder.action
+						{...builder}
+						class="px-0.5 py-0"
+						on:mousedown={startDrag}
+						on:touchstart={startDrag}
+						on:keydown={handleKeyDown}
 					>
-						<DeleteIcon /> Delete
-					</DropdownMenu.Item>
-				</DropdownMenu.Group>
-			</DropdownMenu.Content>
-		</DropdownMenu.Root>
+						<MenuIcon class="h-4 w-4" />
+					</button>
+				</DropdownMenu.Trigger>
+				<DropdownMenu.Content align="end">
+					<DropdownMenu.Group>
+						<DropdownMenu.Item on:click={() => openEditExercise(idx)} class="gap-2">
+							<EditIcon /> Edit
+						</DropdownMenu.Item>
+						<DropdownMenu.Item
+							class="gap-2 text-red-500"
+							on:click={() => {
+								deleteExercise(idx);
+								isContextMenuOpen = false;
+							}}
+						>
+							<DeleteIcon /> Delete
+						</DropdownMenu.Item>
+					</DropdownMenu.Group>
+				</DropdownMenu.Content>
+			</DropdownMenu.Root>
+		{/if}
 	</div>
 	<div class="flex items-center gap-0.5">
 		<span class="mr-auto text-xs text-muted-foreground">
