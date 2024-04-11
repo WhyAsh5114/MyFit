7f20f588f6afa999b044be5481ddf97d03d6496b
WhyAsh5114 <whyash5114@gmail.com>
2024-04-11 14:27:53 +0530

    removed unneeded variable

--- src/routes/api/workouts/getReferenceWorkout/+server.ts
+++ src/routes/api/workouts/getReferenceWorkout/+server.ts
@@ -3,7 +3,6 @@ import clientPromise from "$lib/mongo/mongodb";
 import { ObjectId } from "mongodb";
 import type {
   MesocycleDocument,
-  MesocycleTemplateDocument,
   WorkoutDocument
 } from "$lib/types/documents";
 
@@ -29,17 +28,6 @@ export const POST: RequestHandler = async ({ request, locals }) => {
     if (!activeMesocycle) {
       return new Response("No active mesocycle", { status: 400 });
     }
-
-    const { exerciseSplit } = (await client
-      .db()
-      .collection<MesocycleTemplateDocument>("mesocycleTemplates")
-      .findOne(
-        {
-          userId: new ObjectId(session.user.id),
-          _id: activeMesocycle.templateMesoId
-        },
-        { projection: { exerciseSplit: 1 } }
-      ))!;
 
     let referenceWorkoutDocument = await client
       .db()
