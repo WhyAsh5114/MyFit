b62a1995b49536f27c70d92d0dab91c14ae1f4f3
WhyAsh5114 <whyash5114@gmail.com>
2024-05-03 22:39:52 +0530

    adding layout components from v3-shadcn branch

---
+++ src/routes/(components)/NavLinks.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	export let sheetOpen: boolean | undefined = undefined;
+
+	const linkItems: { text: string; href: string }[] = [
+		{ text: 'Workouts', href: '/workouts' },
+		{ text: 'Mesocycles', href: '/mesocycles' },
+		{ text: 'Exercise splits', href: '/exercise-splits' },
+		{ text: 'Privacy policy', href: '/privacy-policy' }
+	];
+</script>
+
+<ul class="mb-auto lg:mt-8">
+	{#each linkItems as { text, href }}
+		<li>
+			<Button
+				variant="link"
+				class="text-foreground lg:text-base"
+				onclick={() => (sheetOpen = false)}
+			>
+				<a {href}>{text}</a>
+			</Button>
+		</li>
+	{/each}
+</ul>
---
+++ src/routes/(components)/UserAvatar.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import * as Avatar from '$lib/components/ui/avatar';
+	import type { Session } from '@auth/sveltekit';
+	export let session: Session;
+
+	function getInitials(name?: string | null) {
+		if (!name) return '';
+		const names = name.split(' ');
+		const initials = names.map((n) => n.charAt(0).toUpperCase()).join('');
+		return initials;
+	}
+</script>
+
+<Avatar.Root class="h-8 w-8">
+	<Avatar.Image src={session.user?.image} alt="Profile picture" referrerpolicy="no-referrer" />
+	<Avatar.Fallback>{getInitials(session.user?.name)}</Avatar.Fallback>
+</Avatar.Root>
