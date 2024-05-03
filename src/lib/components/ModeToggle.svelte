0d648cf845398b656197e26c8c68bfd56e5da8f3
WhyAsh5114 <whyash5114@gmail.com>
2024-05-03 12:00:32 +0530

    added mode toggle component and installed all dependencies

---
+++ src/lib/components/ModeToggle.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
+	import Sun from 'virtual:icons/lucide/sun';
+	import Moon from 'virtual:icons/lucide/moon';
+	import { setMode, resetMode } from 'mode-watcher';
+
+	export let size: 'icon' | 'lg' = 'icon';
+	export let variant: 'ghost' | 'outline' = 'ghost';
+</script>
+
+<DropdownMenu.Root>
+	<DropdownMenu.Trigger asChild let:builder>
+		<Button builders={[builder]} {variant} {size}>
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
+	</DropdownMenu.Trigger>
+	<DropdownMenu.Content align="end">
+		<DropdownMenu.Item on:click={() => setMode('light')}>Light</DropdownMenu.Item>
+		<DropdownMenu.Item on:click={() => setMode('dark')}>Dark</DropdownMenu.Item>
+		<DropdownMenu.Item on:click={() => resetMode()}>System</DropdownMenu.Item>
+	</DropdownMenu.Content>
+</DropdownMenu.Root>
