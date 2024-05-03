39e19dbdbfae71ea2ca140ea506ab4f90f55de2f
WhyAsh5114 <whyash5114@gmail.com>
2024-05-03 11:58:05 +0530

    added all previous components

---
+++ src/lib/components/ui/command/command-input.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { Command as CommandPrimitive } from 'cmdk-sv';
+	import Search from 'virtual:icons/lucide/search';
+	import { cn } from '$lib/utils.js';
+
+	type $$Props = CommandPrimitive.InputProps & { onFocus?: () => void; onBlur?: () => void };
+
+	let className: string | undefined | null = undefined;
+	export { className as class };
+	export let value: string = '';
+	export let onFocus = () => {};
+	export let onBlur = () => {};
+</script>
+
+<div class="flex items-center border-b px-2" data-cmdk-input-wrapper="">
+	<Search class="mr-2 h-4 w-4 shrink-0 opacity-50" />
+	<CommandPrimitive.Input
+		class={cn(
+			'flex h-11 w-full rounded-md bg-transparent py-3 text-sm outline-none placeholder:text-muted-foreground disabled:cursor-not-allowed disabled:opacity-50',
+			className
+		)}
+		{...$$restProps}
+		on:focus={onFocus}
+		on:blur={onBlur}
+		bind:value
+	/>
+</div>
---
+++ src/lib/components/ui/typography/H2.svelte
@@ -0,0 +1,5 @@
+<h2
+	class="mb-4 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+>
+	<slot />
+</h2>
