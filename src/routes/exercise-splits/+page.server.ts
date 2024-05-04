e730b081ef38f29bd48427b7c38d6a94e77cff60
WhyAsh5114 <whyash5114@gmail.com>
2024-05-04 18:50:18 +0530

    include the splitDays in prisma query

--- src/routes/exercise-splits/+page.server.ts
+++ src/routes/exercise-splits/+page.server.ts
@@ -4,7 +4,8 @@ export const load = async ({ parent }) => {
 	const { session } = await parent();
 
 	const exerciseSplits = prisma.exerciseSplit.findMany({
-		where: { userId: session?.user?.id }
+		where: { userId: session?.user?.id },
+		include: { exerciseSplitDays: { select: { name: true, isRestDay: true } } }
 	});
 	return { exerciseSplits };
 };
