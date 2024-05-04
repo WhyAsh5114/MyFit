4e4b21b876663ed50828b3055da9a43dbf3a4eaf
WhyAsh5114 <whyash5114@gmail.com>
2024-05-04 16:57:10 +0530

    npm run check

--- src/lib/components/ui/table/table-header.svelte
+++ src/lib/components/ui/table/table-header.svelte
@@ -8,7 +8,7 @@
 	export { className as class };
 </script>
 
-<!-- svelte-ignore a11y-no-noninteractive-element-interactions -->
+<!-- svelte-ignore a11y_no_noninteractive_element_interactions -->
 <thead class={cn('[&_tr]:border-b', className)} {...$$restProps} on:click on:keydown>
 	<slot />
 </thead>
