c493488b41489524c4a8939c26f3f728dedcffdd
WhyAsh5114 <whyash5114@gmail.com>
2024-05-05 16:26:14 +0530

    added drawer and cards

--- src/routes/exercise-splits/[mode]/exercises/+page.svelte
+++ src/routes/exercise-splits/[mode]/exercises/+page.svelte
@@ -3,34 +3,40 @@
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import H3 from '$lib/components/ui/typography/H3.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import AddIcon from 'virtual:icons/lucide/plus';
 	import CopyIcon from 'virtual:icons/lucide/copy';
 	import PasteIcon from 'virtual:icons/lucide/clipboard-paste';
 	import CutIcon from 'virtual:icons/lucide/scissors';
 	import MenuIcon from 'virtual:icons/lucide/menu';
 	import SwapIcon from 'virtual:icons/ph/swap';
-	import { exerciseSplit } from '../exerciseSplitRunes.svelte';
+	import { exerciseSplitRunes } from '../exerciseSplitRunes.svelte';
+	import ExerciseSplitDrawer from './ExerciseSplitDrawer.svelte';
+	import ExerciseTemplateCard from '../../(components)/ExerciseTemplateCard.svelte';
+
+	let selectedSplitDayIndex = $state(
+		exerciseSplitRunes.splitDays.findIndex((splitDay) => !splitDay.isRestDay)
+	);
+	let selectedSplitDayName = $derived(exerciseSplitRunes.splitDays[selectedSplitDayIndex].name);
 </script>
 
 <H3>Exercises</H3>
-<Tabs.Root class="w-full">
+<Tabs.Root class="w-full" value={selectedSplitDayName}>
 	<Tabs.List class="flex justify-start overflow-x-auto">
-		{#each exerciseSplit.splitExercises as _, idx}
-			{@const { name, isRestDay } = exerciseSplit.splitDays[idx]}
+		{#each exerciseSplitRunes.splitExercises as _, idx}
+			{@const { name, isRestDay } = exerciseSplitRunes.splitDays[idx]}
 			<Tabs.Trigger value={name} disabled={isRestDay} class="px-4">
 				{name !== '' ? name : 'Rest'}
 			</Tabs.Trigger>
 		{/each}
 	</Tabs.List>
-	{#each exerciseSplit.splitExercises as _, idx}
-		{@const splitDay = exerciseSplit.splitDays[idx]}
+	{#each exerciseSplitRunes.splitExercises as _, idx}
+		{@const splitDay = exerciseSplitRunes.splitDays[idx]}
 		<Tabs.Content value={splitDay.name}>
 			<div class="flex items-center gap-4">
 				<div class="mr-auto flex flex-col">
 					<span class="text-xl font-semibold">{splitDay.name}</span>
 					<span class="font-medium text-muted-foreground">Day {idx + 1}</span>
 				</div>
-				<Button size="icon" variant="outline"><AddIcon /></Button>
+				<ExerciseSplitDrawer {selectedSplitDayName} />
 				<DropdownMenu.Root>
 					<DropdownMenu.Trigger asChild let:builder>
 						<Button builders={[builder]} size="icon" variant="outline"><MenuIcon /></Button>
@@ -53,6 +59,9 @@
 					</DropdownMenu.Content>
 				</DropdownMenu.Root>
 			</div>
+			{#each exerciseSplitRunes.splitExercises[selectedSplitDayIndex] as exerciseTemplate, idx}
+				<ExerciseTemplateCard {exerciseTemplate} {idx} {selectedSplitDayName} dragDisabled />
+			{/each}
 		</Tabs.Content>
 	{/each}
 </Tabs.Root>
