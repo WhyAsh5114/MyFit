aff3e67e732f16af7e7a3fa7336cb89668f70058
WhyAsh5114 <whyash5114@gmail.com>
2024-06-02 12:42:02 +0530

    basic exercise split load function

--- src/routes/exercise-splits/[exerciseSplitId]/+page.server.ts
+++ src/routes/exercise-splits/[exerciseSplitId]/+page.server.ts
@@ -1,3 +1,14 @@
-export const load = async ({ params }) => {
-  
-}
+import prisma from '$lib/prisma.js';
+import { error } from '@sveltejs/kit';
+
+export const load = async ({ params, parent }) => {
+	const { session } = await parent();
+	if (!session?.user?.id) error(401, 'Not logged in');
+
+	const exerciseSplit = await prisma.exerciseSplit.findUnique({
+		where: { userId: session.user.id, id: parseInt(params.exerciseSplitId) },
+		include: { exerciseSplitDays: true }
+	});
+
+	return { exerciseSplit };
+};
