7d1c7406403a0a9483a1624c23e048093d9aebca
WhyAsh5114 <whyash5114@gmail.com>
2024-06-09 12:50:08 +0530

    added an edit_exercise_split action

--- src/routes/exercise-splits/[exerciseSplitId]/+page.server.ts
+++ src/routes/exercise-splits/[exerciseSplitId]/+page.server.ts
@@ -1,5 +1,6 @@
 import prisma from '$lib/prisma.js';
 import { error, fail } from '@sveltejs/kit';
+import type { ExerciseSplitRuneDataType } from '../+page.server';
 
 export const load = async ({ params, parent }) => {
 	const { session } = await parent();
@@ -25,5 +26,34 @@ export const actions = {
 			where: { userId: session.user.id, id: parseInt(params.exerciseSplitId) }
 		});
 		return { message: 'Exercise split deleted successfully' };
+	},
+	edit_exercise_split: async ({ locals, request, params }) => {
+		const session = await locals.auth();
+		if (!session?.user?.id) return fail(403, { message: 'Not logged in' });
+
+		const formData = await request.formData();
+		const data = formData.get('exerciseSplitRuneData');
+		if (!data) return fail(400, { message: "Exercise split wasn't received" });
+
+		const id = parseInt(params.exerciseSplitId);
+		const exerciseSplitRuneData = JSON.parse(data.toString()) as ExerciseSplitRuneDataType;
+
+		await prisma.$transaction([
+			prisma.exerciseSplit.delete({ where: { id, userId: session.user.id } }),
+			prisma.exerciseSplit.create({
+				data: {
+					id,
+					name: exerciseSplitRuneData.splitName,
+					userId: session.user.id,
+					exerciseSplitDays: {
+						create: exerciseSplitRuneData.splitDays.map((splitDay, idx) => ({
+							...splitDay,
+							exercises: { createMany: { data: exerciseSplitRuneData.splitExercises[idx] } }
+						}))
+					}
+				}
+			})
+		]);
+		return { success: true, message: 'Exercise split edited successfully' };
 	}
 };
