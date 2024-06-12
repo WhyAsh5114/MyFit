dbc07dbde543098fc2bb5b3ed3dfd0fca689fdc4
WhyAsh5114 <whyash5114@gmail.com>
2024-06-12 12:12:53 +0530

    moved distribute tab to component

--- src/routes/mesocycles/manage/volume/+page.svelte
+++ src/routes/mesocycles/manage/volume/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import * as Card from '$lib/components/ui/card';
 	import * as Tabs from '$lib/components/ui/tabs';
 	import H3 from '$lib/components/ui/typography/H3.svelte';
 	import { onMount } from 'svelte';
@@ -7,24 +6,27 @@
 	import type { FullExerciseSplit } from '../../../exercise-splits/manage/exerciseSplitRunes.svelte';
 	import { mesocycleRunes } from '../mesocycleRunes.svelte';
 	import MesocycleStartVolumesExercisesTabs from './(components)/MesocycleStartVolumesExercisesTabs.svelte';
-	import MesocycleVolumeSetupTable from './(components)/MesocycleVolumeSetupTable.svelte';
+	import MesocycleStartVolumesSetupTable from './(components)/MesocycleStartVolumesSetupTable.svelte';
+	import MesocycleStartVolumesDistributeCard from './(components)/MesocycleStartVolumesDistributeCard.svelte';
 
 	let { data } = $props();
 	let exerciseSplit: FullExerciseSplit | 'loading' = $state('loading');
 
 	onMount(async () => {
 		const serverExerciseSplit = await data.exerciseSplit;
-		if (!serverExerciseSplit) toast.error('Exercise split not found');
-		else {
-			exerciseSplit = serverExerciseSplit;
-			mesocycleRunes.selectedExerciseSplit = exerciseSplit;
+		if (!serverExerciseSplit) {
+			toast.error('Exercise split not found');
+			return;
+		} else if (mesocycleRunes.selectedExerciseSplit?.id !== serverExerciseSplit.id) {
+			mesocycleRunes.selectedExerciseSplit = serverExerciseSplit;
 		}
+		exerciseSplit = serverExerciseSplit;
 	});
 </script>
 
 <H3>Volume</H3>
 {#if exerciseSplit !== 'loading'}
-	<Tabs.Root value="setup" class="flex w-full grow flex-col">
+	<Tabs.Root value="distribute" class="flex w-full grow flex-col">
 		<Tabs.List class="grid grid-cols-3">
 			<Tabs.Trigger value="setup">Setup</Tabs.Trigger>
 			<Tabs.Trigger value="distribute">Distribute</Tabs.Trigger>
@@ -32,22 +34,16 @@
 		</Tabs.List>
 		<Tabs.Content value="setup" class="grow">
 			<div class="flex h-full flex-col">
-				<MesocycleVolumeSetupTable />
+				<MesocycleStartVolumesSetupTable />
 			</div>
 		</Tabs.Content>
 		<Tabs.Content value="distribute" class="grow">
-			<Card.Root>
-				<Card.Header>
-					<Card.Title>Distribute volume</Card.Title>
-					<Card.Description>
-						Use this dialog to spread volumes across exercises in the exercise split
-					</Card.Description>
-				</Card.Header>
-				<Card.Content>Card content</Card.Content>
-			</Card.Root>
+			<MesocycleStartVolumesDistributeCard />
 		</Tabs.Content>
 		<Tabs.Content value="exercises" class="grow">
 			<MesocycleStartVolumesExercisesTabs />
 		</Tabs.Content>
 	</Tabs.Root>
+{:else}
+	<!-- TODO: loaders-->
 {/if}
