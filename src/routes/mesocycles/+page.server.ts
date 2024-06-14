4b646174e04767e8a5e32f790a8ae58b54583fed
WhyAsh5114 <whyash5114@gmail.com>
2024-06-14 07:24:27 +0530

    created form action for creating mesocycle

--- src/routes/mesocycles/+page.server.ts
+++ src/routes/mesocycles/+page.server.ts
@@ -1,6 +1,22 @@
 import prisma from '$lib/prisma.js';
 import { error, fail } from '@sveltejs/kit';
-import { Prisma } from '@prisma/client';
+import { Prisma, type ExerciseSplit, type ExerciseSplitDay } from '@prisma/client';
+import type {
+	MesocycleCyclicSetChangesWithoutIDs,
+	MesocycleExerciseTemplateWithoutIDs,
+	MesocycleRuneType
+} from './manage/mesocycleRunes.svelte.js';
+
+type ExerciseSplitWithSplitDays = ExerciseSplit & {
+	exerciseSplitDays: Omit<ExerciseSplitDay, 'id' | 'exerciseSplitId' | 'exercises'>[];
+};
+
+export type CreateMesocycleActionDataType = {
+	mesocycle: MesocycleRuneType;
+	mesocycleCyclicSetChanges: MesocycleCyclicSetChangesWithoutIDs[];
+	mesocycleExerciseTemplates: MesocycleExerciseTemplateWithoutIDs[][];
+	exerciseSplit: ExerciseSplitWithSplitDays;
+};
 
 const take = 10;
 
@@ -36,5 +52,34 @@ export const actions = {
 			orderBy: { id: 'desc' },
 			cursor: { id: parseInt(data.cursorId) }
 		});
+	},
+	create_mesocycle: async ({ request, locals }) => {
+		const session = await locals.auth();
+		if (!session?.user?.id) return fail(403, { message: 'Not logged in' });
+
+		const formData = await request.formData();
+		const data = formData.get('mesocycleRuneData');
+		if (!data) return fail(400, { message: "Mesocycle wasn't received" });
+
+		const mesocycleCreateData = JSON.parse(data.toString()) as CreateMesocycleActionDataType;
+		await prisma.mesocycle.create({
+			data: {
+				...mesocycleCreateData.mesocycle,
+				userId: session.user.id,
+				exerciseSplitId: mesocycleCreateData.exerciseSplit.id,
+				mesocycleCyclicSetChanges: {
+					createMany: { data: mesocycleCreateData.mesocycleCyclicSetChanges }
+				},
+				mesocycleExerciseSplitDays: {
+					create: mesocycleCreateData.exerciseSplit.exerciseSplitDays.map((splitDay, idx) => ({
+						...splitDay,
+						mesocycleSplitDayExercises: {
+							createMany: { data: mesocycleCreateData.mesocycleExerciseTemplates[idx] }
+						}
+					}))
+				}
+			}
+		});
+		return { success: true, message: 'Mesocycle created successfully' };
 	}
 };
--- src/routes/mesocycles/manage/volume/+page.svelte
+++ src/routes/mesocycles/manage/volume/+page.svelte
@@ -8,9 +8,13 @@
 	import { mesocycleRunes } from '../mesocycleRunes.svelte';
 	import MesocycleStartVolumesSetupTable from './(components)/MesocycleStartVolumesSetupTable.svelte';
 	import { ScrollArea } from '$lib/components/ui/scroll-area';
+	import { enhance } from '$app/forms';
+	import type { CreateMesocycleActionDataType } from '../../+page.server';
+	import { goto } from '$app/navigation';
 
 	let { data } = $props();
 	let exerciseSplit: FullExerciseSplit | 'loading' = $state('loading');
+	let savingMesocycle = $state(false);
 
 	onMount(async () => {
 		const serverExerciseSplit = await data.exerciseSplit;
@@ -22,21 +26,61 @@
 		}
 		exerciseSplit = serverExerciseSplit;
 	});
-
-	function submitCyclicSetChanges(e: SubmitEvent) {
-		e.preventDefault();
-	}
 </script>
 
 <H3>Volume</H3>
 {#if exerciseSplit !== 'loading'}
-	<form class="contents" onsubmit={submitCyclicSetChanges}>
+	<form
+		method="POST"
+		class="contents"
+		action="/mesocycles?/create_mesocycle"
+		use:enhance={({ formData }) => {
+			savingMesocycle = true;
+			mesocycleRunes.distributeStartVolumes();
+			const exerciseSplit = mesocycleRunes.selectedExerciseSplit as FullExerciseSplit;
+			const exerciseSplitWithoutExercises = {
+				...exerciseSplit,
+				exerciseSplitDays: exerciseSplit.exerciseSplitDays.map((splitDay) => {
+					const { exercises, id, exerciseSplitId, ...rest } = splitDay;
+					return rest;
+				})
+			};
+			const mesocycleCyclicSetChanges = mesocycleRunes.mesocycleCyclicSetChanges.map((setChange) => {
+				const { startVolume, inSplit, ...rest } = setChange;
+				return rest;
+			})
+			const mesocycleRuneData = JSON.stringify({
+				mesocycle: mesocycleRunes.mesocycle,
+				mesocycleCyclicSetChanges,
+				mesocycleExerciseTemplates: mesocycleRunes.mesocycleExerciseTemplates,
+				exerciseSplit: exerciseSplitWithoutExercises
+			} satisfies CreateMesocycleActionDataType);
+
+			formData.set('mesocycleRuneData', mesocycleRuneData);
+			return async ({ result }) => {
+				if (result.type === 'success') {
+					toast.success(result.data?.message as string);
+					await goto('/mesocycles');
+					mesocycleRunes.resetStores();
+				} else if (result.type === 'failure') {
+					toast.error(result.data?.message as string);
+				}
+				savingMesocycle = false;
+			};
+		}}
+	>
 		<ScrollArea orientation="both" class="h-px grow">
 			<MesocycleStartVolumesSetupTable />
 		</ScrollArea>
 		<div class="grid grid-cols-2 gap-1">
 			<Button variant="secondary" href="./exercise-split">Previous</Button>
-			<Button>Next</Button>
+			<Button type="submit" disabled={savingMesocycle}>
+				{#if savingMesocycle}
+					<LoaderCircle class="animate-spin" />
+				{:else}
+					Save
+				{/if}
+			</Button>
 		</div>
 	</form>
 {:else}
