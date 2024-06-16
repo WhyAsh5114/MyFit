bdda0e7d5907562fb07e0fa41e97a99b9847148a
Yash Kolekar <71895020+WhyAsh5114@users.noreply.github.com>
2024-06-16 23:57:26 +0530

    Update +error.svelte
    UI changes

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
 	import { page } from '$app/stores';
 </script>
 
-<div class="flex w-full grow flex-col items-center justify-center gap-2">
+<div class="flex w-full h-px grow flex-col items-center justify-center gap-2">
 	<span class="text-4xl font-bold">Error {$page.status}</span>
 	<h3 class="font-light">{$page.error?.message}</h3>
 </div>
