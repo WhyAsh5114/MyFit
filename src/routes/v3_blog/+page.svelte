c8ce3ea96a217c913d6bb5e73304c7ba8917dfb4
WhyAsh5114 <whyash5114@gmail.com>
2024-06-16 17:37:30 +0530

    initial draft

--- src/routes/v3_blog/+page.svelte
+++ src/routes/v3_blog/+page.svelte
@@ -1,4 +1,45 @@
 <article class="prose">
+  <h2>16th June, 2024</h2>
+  <h3>Update on the status of v3</h3>
+  <p>The app now gets 4000+ views every week 🥳, thanks all!</p>
+  <p>
+    Now that my exams are over, and I have almost an entire month of holidays, I'm dedicating almost
+    the entire day towards v3. I'm learning new stuff and implementing features every day which
+    feels great! I'm almost done with the foundational stuff of exercises and mesocycles and will
+    begin working on workouts very soon. Most of the tech stack is now fixed and I doubt I'll have
+    to rewrite anything for at least the next few months.
+  </p>
+  <figure>
+    <img src="/v3_sneakPeek_1.jpg" alt="v3 sneak peek" />
+    <figcaption>v3 sneak peek</figcaption>
+  </figure>
+  <p>Some of the new and requested features that I've implemented are:</p>
+  <ul>
+    <li>Drag and drop for exercises</li>
+    <li>Exercise search list where you can add an exercise with one click</li>
+    <li>Ability to change exercise split of an ongoing mesocycle without losing progress</li>
+    <li>Actual and meaningful analytics</li>
+  </ul>
+  <p>
+    For the tech people who are interested, (these decisions don't really affect the actual end user
+    experience directly, mostly just UI stuff and a bit of speed improvements), here are some of the
+    major changes:
+  </p>
+  <ul>
+    <li>tRPC instead of REST API for type-safe database queries</li>
+    <li>shadcn-svelte with svelte 5 for UI components</li>
+    <li>Prisma with Neon PostgreSQL for efficient joins and type safety</li>
+  </ul>
+  <p>
+    Now that the basics are in place, I can soon start work on the progression algorithms. I just
+    need to write some tests to make sure stuff doesn't break later on when I add more features.
+    That's about it for this blog post, I'll try to post once every month to give updates on v3 and
+    cool upcoming stuff. Happy lifting!
+  </p>
+</article>
+
+<br /><br />
+<article class="prose">
   <h2>3rd May 2024</h2>
   <h3>About v3</h3>
   <p>
