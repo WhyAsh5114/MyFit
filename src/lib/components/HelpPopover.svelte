508778dccfbe7e199f3f07eddbeddfa6d79e6c3a
WhyAsh5114 <whyash5114@gmail.com>
2024-06-17 20:44:12 +0530

    more appropriate icon

--- src/lib/components/HelpPopover.svelte
+++ src/lib/components/HelpPopover.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import * as Popover from './ui/popover';
-	import HelpIcon from 'virtual:icons/lucide/circle-help';
+	import InfoIcon from 'virtual:icons/lucide/info';
 
 	type PropsType = { ariaLabel: string; text: string; triggerClasses?: string };
 	let { ariaLabel, text, triggerClasses = '' }: PropsType = $props();
@@ -8,7 +8,7 @@
 
 <Popover.Root>
 	<Popover.Trigger aria-label={ariaLabel} class={triggerClasses}>
-		<HelpIcon class="h-4 w-4 text-muted-foreground" />
+		<InfoIcon class="h-4 w-4 text-muted-foreground" />
 	</Popover.Trigger>
 	<Popover.Content class="w-56 text-sm text-muted-foreground" align="end">
 		{text}
