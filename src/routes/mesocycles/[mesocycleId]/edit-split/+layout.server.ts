474617d6091f9ee0adffd1a2481efcbcd89b34b3
WhyAsh5114 <whyash5114@gmail.com>
2024-06-22 22:48:02 +0530

    no need of server data, rely on localStorage runes

--- src/routes/mesocycles/[mesocycleId]/edit-split/+layout.server.ts
+++
@@ -1,8 +0,0 @@
-import { error } from '@sveltejs/kit';
-
-export const load = async ({ parent }) => {
-	const { mesocycle } = await parent();
-	const loadedMesocycle = await mesocycle;
-	if (!loadedMesocycle) error(404, 'Mesocycle not found');
-	return { mesocycle: loadedMesocycle };
-};
