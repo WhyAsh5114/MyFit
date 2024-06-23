8db6a669b1005200bb41c3f36fbd7f89b2d184fc
WhyAsh5114 <whyash5114@gmail.com>
2024-06-23 20:18:38 +0530

    placeholder overview page

---
+++ src/routes/mesocycles/[mesocycleId]/edit-split/overview/+page.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	import H3 from '$lib/components/ui/typography/H3.svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+
+  function saveMesocycleExerciseSplit() {}
+</script>
+
+<H3>Overview</H3>
+<div class="mt-2 grid grid-cols-2 gap-1">
+	<Button variant="secondary" href="./structure">Previous</Button>
+	<Button onclick={saveMesocycleExerciseSplit}>Save</Button>
+</div>
