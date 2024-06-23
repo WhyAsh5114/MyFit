1b745cd1f2cfcc3fbe6746e00f210fab8c2eb9ad
WhyAsh5114 <whyash5114@gmail.com>
2024-06-23 20:28:47 +0530

    update H2 component to allow fitting icon buttons

--- src/lib/components/ui/typography/H2.svelte
+++ src/lib/components/ui/typography/H2.svelte
@@ -1,5 +1,10 @@
+<script lang="ts">
+	import type { Snippet } from 'svelte';
+	let { children }: { children: Snippet } = $props();
+</script>
+
 <h2
-	class="mb-4 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
+	class="mb-4 flex h-10 scroll-m-20 items-end justify-between border-b pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0"
 >
-	<slot />
+	{@render children()}
 </h2>
