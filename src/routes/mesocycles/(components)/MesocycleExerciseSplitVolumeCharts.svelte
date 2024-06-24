834868800a48a8826c1981f4332a5e537901ec8d
WhyAsh5114 <whyash5114@gmail.com>
2024-06-24 13:03:44 +0530

    added charts to overview page

---
+++ src/routes/mesocycles/(components)/MesocycleExerciseSplitVolumeCharts.svelte
@@ -0,0 +1,134 @@
+<script lang="ts">
+	import * as Select from '$lib/components/ui/select';
+	import { arraySum, convertCamelCaseToNormal, groupBy } from '$lib/utils';
+	import { type Prisma } from '@prisma/client';
+	import type { Selected } from 'bits-ui';
+	import {
+		ArcElement,
+		BarController,
+		BarElement,
+		CategoryScale,
+		Chart,
+		Legend,
+		LinearScale,
+		PieController,
+		Tooltip
+	} from 'chart.js';
+	Chart.register(
+		Tooltip,
+		Legend,
+		BarController,
+		BarElement,
+		CategoryScale,
+		LinearScale,
+		PieController,
+		ArcElement
+	);
+
+	type PropsType = {
+		mesocycleSplitExercises: Prisma.MesocycleExerciseTemplateCreateWithoutMesocycleExerciseSplitDayInput[][];
+	};
+
+	let { mesocycleSplitExercises }: PropsType = $props();
+	let chartCanvas: HTMLCanvasElement;
+	let chart: Chart;
+
+	const muscleGroups = Array.from(
+		new Set(
+			mesocycleSplitExercises
+				.flat()
+				.map((exercise) =>
+					exercise.targetMuscleGroup === 'Custom'
+						? (exercise.customMuscleGroup as string)
+						: exercise.targetMuscleGroup
+				)
+		)
+	);
+	let selectedMuscleGroups: Selected<string>[] = $state(
+		muscleGroups
+			.slice(0, 3)
+			.map((muscleGroup) => ({ value: muscleGroup, label: convertCamelCaseToNormal(muscleGroup) }))
+	);
+
+	const chartTypes = [
+		'Microcycle volume distribution',
+		'Muscle group volume distribution'
+	] as const;
+	let selectedChartType: Selected<(typeof chartTypes)[number]> = $state({
+		value: 'Microcycle volume distribution',
+		label: 'Microcycle volume distribution'
+	});
+
+	$effect(() => {
+		const style = getComputedStyle(document.body);
+		const primaryColor = style.getPropertyValue('--primary').split(' ').join(', ');
+		const secondaryColor = style.getPropertyValue('--secondary').split(' ').join(', ');
+
+		if (chart) chart.destroy();
+		if (selectedChartType.value === 'Microcycle volume distribution') {
+			chart = new Chart(chartCanvas, {
+				type: 'bar',
+				data: {
+					labels: mesocycleSplitExercises.map((_, idx) => `D${idx + 1}`),
+					datasets: [
+						{
+							label: 'Volume',
+							data: mesocycleSplitExercises.map((dayExercises) =>
+								arraySum(dayExercises.map((exercise) => exercise.sets))
+							)
+						}
+					]
+				}
+			});
+		} else if (selectedChartType.value === 'Muscle group volume distribution') {
+			chart = new Chart(chartCanvas, {
+				type: 'bar',
+				data: {
+					labels: mesocycleSplitExercises.map((_, idx) => `D${idx + 1}`),
+					datasets: selectedMuscleGroups.map(({ value }, idx) => ({
+						label: value,
+						data: mesocycleSplitExercises.map((dayExercises) =>
+							arraySum(
+								dayExercises
+									.filter((e) => (e.customMuscleGroup ?? e.targetMuscleGroup) === value)
+									.map((exercise) => exercise.sets)
+							)
+						),
+						backgroundColor: idx % 2 === 0 ? `hsl(${primaryColor})` : `hsl(${secondaryColor})`
+					}))
+				}
+			});
+		}
+	});
+</script>
+
+<div class="mb-2 w-full grow">
+	<canvas bind:this={chartCanvas} id="chart-canvas"></canvas>
+</div>
+
+<div class="flex flex-col gap-1">
+	<Select.Root bind:selected={selectedChartType}>
+		<Select.Label class="p-0">Chart</Select.Label>
+		<Select.Trigger class="mb-2 w-full">
+			<Select.Value placeholder="Select chart" />
+		</Select.Trigger>
+		<Select.Content class="max-h-48 overflow-y-auto">
+			{#each chartTypes as chartType}
+				<Select.Item value={chartType}>{chartType}</Select.Item>
+			{/each}
+		</Select.Content>
+	</Select.Root>
+	{#if selectedChartType.value === 'Muscle group volume distribution'}
+		<Select.Root multiple bind:selected={selectedMuscleGroups}>
+			<Select.Label class="p-0">Selected muscle groups</Select.Label>
+			<Select.Trigger class="w-full">
+				<Select.Value placeholder="Select muscle groups" />
+			</Select.Trigger>
+			<Select.Content class="max-h-48 overflow-y-auto">
+				{#each muscleGroups as muscleGroup}
+					<Select.Item value={muscleGroup}>{convertCamelCaseToNormal(muscleGroup)}</Select.Item>
+				{/each}
+			</Select.Content>
+		</Select.Root>
+	{/if}
+</div>
--- src/routes/mesocycles/[mesocycleId]/edit-split/overview/+page.svelte
+++ src/routes/mesocycles/[mesocycleId]/edit-split/overview/+page.svelte
@@ -1,12 +1,46 @@
 <script lang="ts">
 	import H3 from '$lib/components/ui/typography/H3.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
+	import * as Tabs from '$lib/components/ui/tabs';
+	import { Root as Card } from '$lib/components/ui/card';
+	import { mesocycleExerciseSplitRunes } from '../mesocycleExerciseSplitRunes.svelte';
+	import ExerciseSplitExercisesCharts from '../../../../exercise-splits/(components)/ExerciseSplitExercisesCharts.svelte';
+	import ExerciseSplitMuscleGroupsCharts from '../../../../exercise-splits/(components)/ExerciseSplitMuscleGroupsCharts.svelte';
+	import MesocycleExerciseSplitVolumeCharts from '../../../(components)/MesocycleExerciseSplitVolumeCharts.svelte';
 
-  function saveMesocycleExerciseSplit() {}
+	function saveMesocycleExerciseSplit() {}
 </script>
 
 <H3>Overview</H3>
+
+<Tabs.Root value="volume" class="mb-auto w-full">
+	<Tabs.List class="grid grid-cols-3">
+		<Tabs.Trigger value="volume">Volume</Tabs.Trigger>
+		<Tabs.Trigger value="exercises">Exercises</Tabs.Trigger>
+		<Tabs.Trigger value="muscleGroups">MuscleGroups</Tabs.Trigger>
+	</Tabs.List>
+	<Tabs.Content value="volume">
+		<Card class="p-4">
+			<MesocycleExerciseSplitVolumeCharts
+				mesocycleSplitExercises={mesocycleExerciseSplitRunes.splitExercises}
+			/>
+		</Card>
+	</Tabs.Content>
+	<Tabs.Content value="exercises">
+		<Card class="p-4">
+			<ExerciseSplitExercisesCharts exercises={mesocycleExerciseSplitRunes.splitExercises.flat()} />
+		</Card>
+	</Tabs.Content>
+	<Tabs.Content value="muscleGroups">
+		<Card class="p-4">
+			<ExerciseSplitMuscleGroupsCharts
+				splitExercises={mesocycleExerciseSplitRunes.splitExercises}
+			/>
+		</Card>
+	</Tabs.Content>
+</Tabs.Root>
+
 <div class="mt-2 grid grid-cols-2 gap-1">
-	<Button variant="secondary" href="./structure">Previous</Button>
+	<Button variant="secondary" href="./exercises">Previous</Button>
 	<Button onclick={saveMesocycleExerciseSplit}>Save</Button>
 </div>
