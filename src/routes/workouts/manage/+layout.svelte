103aacfe29699eb36310c3eaf96946f04cf4e253
WhyAsh5114 <whyash5114@gmail.com>
2024-06-25 18:23:41 +0530

    placeholder layout page

---
+++ src/routes/workouts/manage/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import H2 from '$lib/components/ui/typography/H2.svelte';
+	import type { Snippet } from 'svelte';
+
+	let { children }: { children: Snippet } = $props();
+</script>
+
+<H2>New workout</H2>
+{@render children()}
