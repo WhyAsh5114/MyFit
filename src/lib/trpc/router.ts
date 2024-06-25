ee14dfa13f151ac435fc62aa47b0718bd21ab7c7
WhyAsh5114 <whyash5114@gmail.com>
2024-06-25 19:31:45 +0530

    created workouts trpc route

--- src/lib/trpc/router.ts
+++ src/lib/trpc/router.ts
@@ -1,10 +1,12 @@
 import { t } from '$lib/trpc/t';
 import { exerciseSplits } from './routes/exerciseSplits';
 import { mesocycles } from './routes/mesocycles';
+import { workouts } from './routes/workouts';
 
 export const router = t.router({
 	exerciseSplits,
-	mesocycles
+	mesocycles,
+	workouts
 });
 
 export const createCaller = t.createCallerFactory(router);
---
+++ src/lib/trpc/routes/workouts.ts
@@ -0,0 +1,7 @@
+import { prisma } from '$lib/prisma';
+import { z } from 'zod';
+import { t } from '$lib/trpc/t';
+
+export const workouts = t.router({
+
+});
