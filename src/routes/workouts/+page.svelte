7ad97a110bffd2eebdf02ff53b58b2f37ed83e88
WhyAsh5114 <whyash5114@gmail.com>
2024-06-25 18:23:08 +0530

    placeholder workouts page

---
+++ src/routes/workouts/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import Button from '$lib/components/ui/button/button.svelte';
+	import H2 from '$lib/components/ui/typography/H2.svelte';
+	import AddIcon from 'virtual:icons/lucide/plus';
+</script>
+
+<H2>Workouts</H2>
+<Button aria-label="create-workout" href="/workouts/manage/start"><AddIcon /></Button>
