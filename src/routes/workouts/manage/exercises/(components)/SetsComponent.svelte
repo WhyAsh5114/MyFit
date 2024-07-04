c2469985ade1119d228fa1771e077ef54bc7898f
Yash Kolekar <whyash5114@gmail.com>
2024-07-04 13:50:49 +0530

    added v2 set component

--- src/routes/workouts/manage/exercises/(components)/SetsComponent.svelte
+++ src/routes/workouts/manage/exercises/(components)/SetsComponent.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
 	import type { WorkoutExerciseInProgress } from '$lib/mesoToWorkouts';
 	import StraightSetsComponent from './(componentsForVariousSets)/StraightSetsComponent.svelte';
+	import V2SetsComponent from './(componentsForVariousSets)/V2SetsComponent.svelte';
 
 	type PropsType = { reordering: boolean; exercise: WorkoutExerciseInProgress };
 	let { reordering, exercise = $bindable() }: PropsType = $props();
 
-	let typeToComponent = [{ setType: 'Straight', component: StraightSetsComponent }];
+	let typeToComponent = [
+		{ setType: 'Straight', component: StraightSetsComponent },
+		{ setType: 'V2', component: V2SetsComponent }
+	];
 	let selected = $derived(typeToComponent.find((e) => e.setType === exercise.setType));
 </script>
 
