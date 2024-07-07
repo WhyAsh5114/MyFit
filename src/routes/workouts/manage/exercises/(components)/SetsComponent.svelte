d6fdc2a3d5e5f0f835868b13e0b37d42c3c117bd
Yash Kolekar <whyash5114@gmail.com>
2024-07-07 20:39:06 +0530

    moved to a common sets component needs debugging soon

--- src/routes/workouts/manage/exercises/(components)/SetsComponent.svelte
+++ src/routes/workouts/manage/exercises/(components)/SetsComponent.svelte
@@ -1,26 +1,253 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import { Input } from '$lib/components/ui/input';
+	import { Separator } from '$lib/components/ui/separator';
 	import type { WorkoutExerciseInProgress } from '$lib/workoutFunctions';
-	import TopDownSetsComponent from './(componentsForVariousSets)/TopDownSetsComponent.svelte';
-	import DropSetsComponent from './(componentsForVariousSets)/DropSetsComponent.svelte';
-	import MyorepMatchSetsComponent from './(componentsForVariousSets)/MyorepMatchSetsComponent.svelte';
-	import StraightSetsComponent from './(componentsForVariousSets)/StraightSetsComponent.svelte';
-	import V2SetsComponent from './(componentsForVariousSets)/V2SetsComponent.svelte';
+	import CheckIcon from 'virtual:icons/lucide/check';
+	import RemoveIcon from 'virtual:icons/lucide/minus';
+	import EditIcon from 'virtual:icons/lucide/pencil';
+	import AddIcon from 'virtual:icons/lucide/plus';
+	import UndoIcon from 'virtual:icons/lucide/undo';
+	import { workoutRunes } from '../../workoutRunes.svelte';
+	import { arraySum, floorToNearestMultiple } from '$lib/utils';
 
-	type PropsType = { reordering: boolean; exercise: WorkoutExerciseInProgress };
+	type PropsType = {
+		reordering: boolean;
+		exercise: WorkoutExerciseInProgress;
+	};
+	type WorkoutExerciseSet = WorkoutExerciseInProgress['sets'][number];
 	let { reordering, exercise = $bindable() }: PropsType = $props();
 
-	let typeToComponent = [
-		{ setType: 'Straight', component: StraightSetsComponent },
-		{ setType: 'V2', component: V2SetsComponent },
-		{ setType: 'Drop', component: DropSetsComponent },
-		{ setType: 'MyorepMatch', component: MyorepMatchSetsComponent },
-		{ setType: 'Down', component: TopDownSetsComponent },
-		{ setType: 'Top', component: TopDownSetsComponent },
-		{ setType: 'Myorep', component: StraightSetsComponent }
-	];
-	let selected = $derived(typeToComponent.find((e) => e.setType === exercise.setType));
+	function shouldSetBeDisabled(set: WorkoutExerciseSet, idx: number): boolean {
+		if (set.completed) return false;
+		if (idx === 0) return false;
+		const previousSet = exercise.sets[idx - 1];
+		if (previousSet.miniSets.length === 0) return !previousSet.completed;
+		return !previousSet.miniSets[previousSet.miniSets.length - 1].completed;
+	}
+
+	function completeSet(e: SubmitEvent, set: WorkoutExerciseSet, idx: number) {
+		e.preventDefault();
+		if (set.skipped) {
+			set.skipped = false;
+			return;
+		}
+		set.completed = !set.completed;
+		// If first set of straight set, set all loads of all sets to this set's load
+		if (exercise.setType === 'Straight' || exercise.setType === 'MyorepMatch') {
+			if (idx === 0) exercise.sets.forEach((_set) => (_set.load = set.load));
+		}
+		workoutRunes.workoutExercises = workoutRunes.workoutExercises;
+	}
+
+	function shouldMiniSetBeDisabled(setIndex: number, miniSetIndex: number) {
+		const parentSet = exercise.sets[setIndex];
+		if (miniSetIndex === 0) return !parentSet.completed;
+		return !parentSet.miniSets[miniSetIndex - 1].completed;
+	}
+
+	function addMiniSet(setIndex: number) {
+		exercise.sets[setIndex].miniSets.push({
+			completed: false,
+			reps: undefined,
+			load: exercise.setType === 'MyorepMatch' ? exercise.sets[setIndex].load : undefined,
+			RIR: undefined
+		});
+	}
+
+	function completeMiniSet(e: SubmitEvent, set: WorkoutExerciseSet, miniSetIndex: number) {
+		e.preventDefault();
+		set.miniSets[miniSetIndex].completed = !set.miniSets[miniSetIndex].completed;
+		workoutRunes.workoutExercises = workoutRunes.workoutExercises;
+	}
+
+	function calculateNextLoad(setIdx: number) {
+		const firstSet = exercise.sets[0];
+		if (typeof firstSet.load !== 'number') return 0;
+		if (!exercise.changeType || setIdx === 0) return 0;
+		if (exercise.changeAmount === null || exercise.changeAmount === undefined) return 0;
+
+		if (exercise.setType === 'Down') setIdx = -setIdx;
+		if (exercise.changeType === 'AbsoluteLoad') {
+			return firstSet.load + setIdx * exercise.changeAmount;
+		}
+		return firstSet.load * (1 + setIdx * (exercise.changeAmount / 100));
+	}
+
+	function getNextLoad(setIdx: number) {
+		if (!['Top', 'Down'].includes(exercise.setType)) return;
+		if (typeof exercise.sets[0].load !== 'number') return;
+		return floorToNearestMultiple(
+			calculateNextLoad(setIdx),
+			exercise.minimumWeightChange ?? 5
+		).toString();
+	}
+
+	function getRemainingMyorepMatchReps(setIdx: number) {
+		const firstSet = exercise.sets[0];
+		const set = exercise.sets[setIdx];
+		if (firstSet.reps === undefined) return;
+		if (set.reps === undefined) return firstSet.reps;
+		return firstSet.reps - set.reps - arraySum(set.miniSets.map((miniSet) => miniSet.reps ?? 0));
+	}
 </script>
 
-{#if selected}
-	<svelte:component this={selected.component} {reordering} bind:exercise />
+{#if !reordering}
+	<Separator class="my-1" />
+	<div class="grid grid-cols-4 gap-1">
+		<span class="text-center text-sm font-medium">Reps</span>
+		<span class="text-center text-sm font-medium">
+			Load
+			{#if typeof exercise.bodyweightFraction === 'number'}
+				<span class="text-xs font-semibold text-muted-foreground">(BW)</span>
+			{/if}
+		</span>
+		<span class="text-center text-sm font-medium">RIR</span>
+		<span></span>
+		{#each exercise.sets as set, idx}
+			<form class="contents" onsubmit={(e) => completeSet(e, set, idx)}>
+				{#if !set.skipped}
+					<Input
+						type="number"
+						min={0}
+						id="{exercise.name}-set-{idx + 1}-reps"
+						disabled={set.completed || set.skipped}
+						required
+						bind:value={set.reps}
+					/>
+					{#if idx === 0 || !['Straight', 'Myorep', 'MyorepMatch'].includes(exercise.setType)}
+						<Input
+							type="number"
+							min={0}
+							id="{exercise.name}-set-{idx + 1}-load"
+							disabled={set.completed || set.skipped}
+							required
+							placeholder={getNextLoad(idx)}
+							bind:value={set.load}
+						/>
+					{:else if exercise.setType === 'MyorepMatch'}
+						<span></span>
+					{:else}
+						<span></span>
+					{/if}
+					<Input
+						type="number"
+						min={0}
+						id="{exercise.name}-set-{idx + 1}-RIR"
+						disabled={set.completed || set.skipped}
+						required
+						bind:value={set.RIR}
+					/>
+				{:else}
+					<div class="col-span-3 flex items-center gap-2">
+						<Separator class="w-px grow" />
+						<span class="text-sm text-muted-foreground">skipped</span>
+						<Separator class="w-px grow" />
+					</div>
+				{/if}
+				<Button
+					size="icon"
+					class="place-self-end"
+					type="submit"
+					variant={set.completed ? 'outline' : 'default'}
+					disabled={shouldSetBeDisabled(set, idx)}
+				>
+					{#if set.skipped}
+						<UndoIcon />
+					{:else if !set.completed}
+						<CheckIcon />
+					{:else}
+						<EditIcon />
+					{/if}
+				</Button>
+			</form>
+			{#if (idx > 0 && exercise.setType === 'MyorepMatch') || exercise.setType === 'Drop'}
+				{#each set.miniSets as miniSet, miniIdx}
+					{@const miniSetButtonDisabled = shouldMiniSetBeDisabled(idx, miniIdx)}
+					{#if set.skipped}
+						<div class="col-span-3 flex items-center gap-2">
+							<Separator class="w-px grow" />
+							<span class="text-sm text-muted-foreground">skipped</span>
+							<Separator class="w-px grow" />
+						</div>
+						<Button size="icon" class="place-self-end" variant="secondary" disabled>
+							<CheckIcon />
+						</Button>
+					{:else}
+						<form class="contents" onsubmit={(e) => completeMiniSet(e, set, miniIdx)}>
+							<Input
+								type="number"
+								min={0}
+								id="{exercise.name}-set-{idx + 1}-mini-set-{miniIdx + 1}-reps"
+								disabled={miniSet.completed}
+								required
+								bind:value={miniSet.reps}
+							/>
+							{#if exercise.setType === 'MyorepMatch'}
+								<span></span>
+							{:else}
+								<Input
+									type="number"
+									min={0}
+									id="{exercise.name}-set-{idx + 1}-mini-set-{miniIdx + 1}-load"
+									disabled={miniSet.completed}
+									required
+									bind:value={miniSet.load}
+								/>
+							{/if}
+							<Input
+								type="number"
+								min={0}
+								id="{exercise.name}-set-{idx + 1}-mini-set-{miniIdx + 1}-RIR"
+								disabled={miniSet.completed}
+								required
+								bind:value={miniSet.RIR}
+							/>
+							<Button
+								size="icon"
+								class="place-self-end"
+								disabled={miniSetButtonDisabled}
+								type="submit"
+								variant={miniSet.completed ? 'outline' : 'default'}
+							>
+								{#if !miniSet.completed}
+									<CheckIcon />
+								{:else}
+									<EditIcon />
+								{/if}
+							</Button>
+						</form>
+					{/if}
+				{/each}
+				<Button
+					variant="secondary"
+					aria-label="add-mini-set-to-set-{idx + 1}-of-{exercise.name}"
+					onclick={() => addMiniSet(idx)}
+				>
+					<AddIcon />
+				</Button>
+				<Button
+					variant="secondary"
+					aria-label="remove-mini-set-from-set-{idx + 1}-of-{exercise.name}"
+					disabled={set.miniSets.length === 0}
+					onclick={() => set.miniSets.pop()}
+				>
+					<RemoveIcon />
+				</Button>
+				{#if exercise.setType === 'MyorepMatch'}
+					{@const repsLeft = getRemainingMyorepMatchReps(idx)}
+					<span class="grid place-items-center text-sm font-medium text-primary">
+						{#if repsLeft && repsLeft > 0}
+							{repsLeft} {repsLeft === 1 ? 'rep' : 'reps'} left
+						{:else}
+							matched
+						{/if}
+					</span>
+				{:else}
+					<span></span>
+				{/if}
+				<span></span>
+			{/if}
+		{/each}
+	</div>
 {/if}
