f5542d3a28f408e636461a5814b4374b15ca4bd1
Yash Kolekar <whyash5114@gmail.com>
2024-07-10 13:10:59 +0530

    use editingId to show correct heading

--- src/routes/workouts/manage/+layout.svelte
+++ src/routes/workouts/manage/+layout.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
 	import H2 from '$lib/components/ui/typography/H2.svelte';
 	import type { Snippet } from 'svelte';
+	import { workoutRunes } from './workoutRunes.svelte';
 
 	let { children }: { children: Snippet } = $props();
+	let editing = $derived(workoutRunes.editingWorkoutId !== null);
 </script>
 
-<H2>New workout</H2>
+<H2>{editing ? 'Edit' : 'New'} workout</H2>
 {@render children()}
