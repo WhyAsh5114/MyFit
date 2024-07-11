abdc642db8d6119bdd4bf0a5334e12f8d8c83127
Yash Kolekar <whyash5114@gmail.com>
2024-07-11 21:52:39 +0530

    remove firefox and webkit

--- playwright.config.ts
+++ playwright.config.ts
@@ -21,29 +21,8 @@ const config: PlaywrightTestConfig = {
 	},
 	timeout: 60000,
 	projects: [
-		{ name: 'chromium', use: { ...devices['Desktop Chrome'] } },
-		{ name: 'firefox', use: { ...devices['Desktop Firefox'] } },
-		{ name: 'webkit', use: { ...devices['Desktop Safari'] } }
-
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
-
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
+		{ name: 'chromium', use: { ...devices['Desktop Chrome'] } }
+		// TODO: maybe add more browsers? but it's very annoying to deal with minor bugs
 	]
 };
 
