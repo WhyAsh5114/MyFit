7a399b7400bd11bf77e7b12b6eb331c0eb384930
Yash Kolekar <whyash5114@gmail.com>
2024-07-11 18:23:22 +0530

    add a createMesocycle function

--- tests/models/commonFunctions.ts
+++ tests/models/commonFunctions.ts
@@ -12,3 +12,24 @@ export async function createTemplateExerciseSplit(page: Page) {
 		page.getByRole('status').filter({ hasText: 'Exercise split created successfully' })
 	).toBeVisible({ timeout: 10000 });
 }
+
+export async function createMesocycle(page: Page) {
+	await page.getByLabel('exercise-split-new-options').click();
+	await page.getByRole('menuitem', { name: 'Use template' }).click();
+	await page.getByRole('button', { name: 'Pull Push Legs 7 days / cycle' }).click();
+	await page.getByRole('button', { name: 'Next' }).click();
+	await page.getByRole('button', { name: 'Next' }).click();
+	await page.getByRole('button', { name: 'Save' }).click();
+	await page.goto('/mesocycles');
+	await page.getByLabel('create-new-mesocycle').click();
+	await page.getByLabel('Mesocycle name').click();
+	await page.getByLabel('Mesocycle name').fill('MyMeso');
+	await page.getByRole('button', { name: 'Next' }).click();
+	await page.getByText('Pick one').click();
+	await page.getByRole('option', { name: 'Pull Push Legs' }).click();
+	await page.getByRole('button', { name: 'Next' }).click();
+	await page.waitForURL(/\/mesocycles\/manage\/volume/);
+	await page.getByRole('button', { name: 'Next' }).click();
+	await page.getByLabel('Start immediately').click();
+	await page.getByRole('button', { name: 'Save' }).click();
+}
