62c95e2eba4f3c9e6bc54514dca3b731c0c417fb
WhyAsh5114 <whyash5114@gmail.com>
2024-09-04 10:18:08 +0530

    fix(analytics): Don't use when in dev mode

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,8 +1,8 @@
-import { browser } from '$app/environment';
+import { browser, dev } from '$app/environment';
 import posthog from 'posthog-js';
 
 export const load = async () => {
-	if (browser) {
+	if (browser && !dev) {
 		posthog.init('phc_QZNnbrh9605g46soHXsZXGXsbob5XHT1w7wopjS7Sha', {
 			api_host: 'https://us.i.posthog.com',
 			person_profiles: 'always'
