1068abad9b173c4c86f2aa80d5c843d263b724c4
WhyAsh5114 <whyash5114@gmail.com>
2024-09-11 15:03:23 +0530

    feat: Include startedMesocycles in entityCounts

--- src/lib/trpc/routes/users.ts
+++ src/lib/trpc/routes/users.ts
@@ -3,9 +3,19 @@ import { t } from '$lib/trpc/t';
 
 export const users = t.router({
 	getEntityCounts: t.procedure.query(async ({ ctx }) => {
-		return await prisma.user.findUnique({
+		const queryResult = await prisma.user.findUnique({
 			where: { id: ctx.userId },
 			select: { _count: { select: { exerciseSplits: true, mesocycles: true, workouts: true } } }
 		});
+		if (!queryResult) {
+			return null;
+		}
+
+		const startedMesocycles = await prisma.mesocycle.count({
+			where: { userId: ctx.userId, startDate: { not: null } }
+		});
+
+		const entityCounts = { ...queryResult._count, startedMesocycles };
+		return entityCounts;
 	})
 });
