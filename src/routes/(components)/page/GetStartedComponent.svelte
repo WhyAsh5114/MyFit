b786b894c2915e23e3c5586304c57b34d6561cf2
WhyAsh5114 <whyash5114@gmail.com>
2024-09-11 15:28:12 +0530

    feat: Completed GetStarted component

--- src/routes/(components)/page/GetStartedComponent.svelte
+++ src/routes/(components)/page/GetStartedComponent.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+	import * as Card from '$lib/components/ui/card';
+	import { Skeleton } from '$lib/components/ui/skeleton';
+	import H3 from '$lib/components/ui/typography/H3.svelte';
 	import type { RouterOutputs } from '$lib/trpc/router';
+	import DoneIcon from 'virtual:icons/lucide/circle-check-big';
+	import OpenLinkIcon from 'virtual:icons/lucide/square-arrow-out-up-right';
 
-	type PropsType = { entityCounts: RouterOutputs['users']['getEntityCounts'] };
+	type PropsType = { entityCounts?: RouterOutputs['users']['getEntityCounts'] };
 	let { entityCounts }: PropsType = $props();
 
-	let taskList: string[] = $state([]);
+	const taskList = $derived([
+		{ task: 'Login', completion: entityCounts !== null, link: '/' },
+		{
+			task: 'Create an exercise split',
+			completion: Number(entityCounts?.exerciseSplits) > 0,
+			link: '/exercise-splits'
+		},
+		{ task: 'Create a mesocycle', completion: Number(entityCounts?.mesocycles) > 0, link: '/mesocycles' },
+		{ task: 'Start a mesocycle', completion: Number(entityCounts?.startedMesocycles) > 0, link: '/mesocycles' },
+		{ task: 'Create workout', completion: Number(entityCounts?.workouts) > 0, link: '/workouts' }
+	]);
 
-	function getTaskListWithCompletion() {}
+	const tasksDone = $derived(taskList.filter(({ completion }) => completion).length);
 </script>
 
-<p>
-	1: login<br />
-	2: create exercise Split<br />
-	3: create mesocycle<br />
-	4: start mesocycle if not active<br />
-	5: create workout<br />
-	6: done!<br />
-</p>
+{#if tasksDone < taskList.length}
+	<H3>Get started</H3>
+	{#if entityCounts === undefined}
+		<Skeleton class="h-[174px] w-full" />
+	{:else}
+		<Card.Root class="grid p-4 text-sm">
+			{#each taskList as { task, completion, link }, idx}
+				{@const prevTaskDone = idx === 0 ? true : taskList.at(idx - 1)?.completion}
+				<div class="flex h-7 items-center justify-between">
+					<span>{task}</span>
+					{#if completion}
+						<Button class="h-7 w-7 p-1" size="icon" variant="ghost">
+							<DoneIcon class="text-primary" />
+						</Button>
+					{:else if idx !== 0}
+						{@const buttonProps = prevTaskDone ? { href: link } : { disabled: true }}
+						<Button class="h-7 w-7 p-1" {...buttonProps} size="icon" variant="ghost">
+							<OpenLinkIcon />
+						</Button>
+					{/if}
+				</div>
+			{/each}
+		</Card.Root>
+	{/if}
+	<br />
+{/if}
