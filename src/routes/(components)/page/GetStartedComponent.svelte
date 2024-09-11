169aef2e5e2db4aea2d45a073ef72119371be41d
WhyAsh5114 <whyash5114@gmail.com>
2024-09-11 14:31:21 +0530

    refactor: Simplified page to component data flow

--- src/routes/(components)/page/GetStartedComponent.svelte
+++ src/routes/(components)/page/GetStartedComponent.svelte
@@ -1,36 +1,19 @@
 <script lang="ts">
 	import type { RouterOutputs } from '$lib/trpc/router';
 
-	type PropsType = { entityCounts?: Promise<RouterOutputs['users']['getEntityCounts']> };
+	type PropsType = { entityCounts: RouterOutputs['users']['getEntityCounts'] };
 	let { entityCounts }: PropsType = $props();
 
 	let taskList: string[] = $state([]);
 
-  function getTaskListWithCompletion() {
-    
-  }
+	function getTaskListWithCompletion() {}
 </script>
 
-{#await entityCounts}
-	TODO: skeletons
-{:then entityCounts}
-	{#if entityCounts}
-		<p>
-			1: login<br />
-			2: create exercise Split<br />
-			3: create mesocycle<br />
-			4: start mesocycle if not active<br />
-			5: create workout<br />
-			6: done!<br />
-		</p>
-	{:else}
-		<p>
-			1: login<br />
-			2: create exercise Split<br />
-			3: create mesocycle<br />
-			4: start mesocycle if not active<br />
-			5: create workout<br />
-			6: done!<br />
-		</p>
-	{/if}
-{/await}
+<p>
+	1: login<br />
+	2: create exercise Split<br />
+	3: create mesocycle<br />
+	4: start mesocycle if not active<br />
+	5: create workout<br />
+	6: done!<br />
+</p>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,21 @@
 <script lang="ts">
 	import H2 from '$lib/components/ui/typography/H2.svelte';
 	import H3 from '$lib/components/ui/typography/H3.svelte';
+	import type { RouterOutputs } from '$lib/trpc/router';
+	import { onMount } from 'svelte';
 	import GetStartedComponent from './(components)/page/GetStartedComponent.svelte';
 	import TodaysWorkoutCard from './(components)/page/TodaysWorkoutCard.svelte';
 
 	let { data } = $props();
+	let entityCounts: RouterOutputs['users']['getEntityCounts'] | 'loading' = $state('loading');
+
+	onMount(async () => {
+		if (data.entityCounts === undefined) {
+			entityCounts = null;
+			return;
+		}
+		entityCounts = await data.entityCounts;
+	});
 </script>
 
 <H2>Home</H2>
@@ -14,4 +25,8 @@
 	<TodaysWorkoutCard {...data} />
 {/if}
 
-<GetStartedComponent entityCounts={data.entityCounts} />
+{#if entityCounts === 'loading'}
+	TODO: skeletons
+{:else}
+	<GetStartedComponent {entityCounts} />
+{/if}
