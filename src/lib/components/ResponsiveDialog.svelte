d7723bf27d0ea1fef5918a4cff147e3b7ade5c7b
WhyAsh5114 <whyash5114@gmail.com>
2024-09-13 15:49:00 +0530

    feat: Migrate ResponsiveDialog to svelte 5 syntax

--- src/lib/components/ResponsiveDialog.svelte
+++ src/lib/components/ResponsiveDialog.svelte
@@ -2,31 +2,49 @@
 	import * as Dialog from '$lib/components/ui/dialog/index.js';
 	import * as Drawer from '$lib/components/ui/drawer/index.js';
 	import { Button } from '$lib/components/ui/button/index.js';
-	import { onMount } from 'svelte';
+	import { onMount, type Snippet } from 'svelte';
 
-	export let dismissible = true;
-	export let needTrigger = true;
-	export let variant: 'outline' | 'link' | 'default' | 'destructive' | 'secondary' | 'ghost' | undefined = 'outline';
-	export let size: 'default' | 'sm' | 'lg' | 'icon' | undefined = 'default';
-	export let ariaLabel = '';
-	export let cancelVariant: 'outline' | 'link' | 'default' | 'destructive' | 'secondary' | 'ghost' | undefined =
-		'outline';
-	export let title: string;
-	export let description: string | undefined = undefined;
-	export let open = false;
+	type PropsType = {
+		title: string;
+		description?: Snippet;
+		open: boolean;
+		dismissible?: boolean;
+		triggerButtonVariant?: Button['$$prop_def']['variant'];
+		triggerButtonSize?: Button['$$prop_def']['size'];
+		triggerButtonAriaLabel?: string;
+		triggerButtonContent?: Snippet;
+		cancelButtonVariant?: Button['$$prop_def']['variant'];
+		children: Snippet;
+	};
 
-	let isDesktop = false;
-	onMount(() => {
-		isDesktop = window.matchMedia('(min-width: 768px)').matches;
-	});
+	let {
+		title,
+		description,
+		open = $bindable(false),
+		dismissible = true,
+		triggerButtonVariant = 'outline',
+		triggerButtonSize = 'default',
+		triggerButtonAriaLabel = '',
+		triggerButtonContent,
+		cancelButtonVariant = 'outline',
+		children
+	}: PropsType = $props();
+
+	let isDesktop = $state(false);
+	onMount(() => (isDesktop = window.matchMedia('(min-width: 768px)').matches));
 </script>
 
 {#if isDesktop}
 	<Dialog.Root bind:open>
-		{#if needTrigger}
+		{#if triggerButtonContent}
 			<Dialog.Trigger asChild let:builder>
-				<Button aria-label={ariaLabel} builders={[builder]} {size} {variant}>
-					<slot name="buttonContent" />
+				<Button
+					aria-label={triggerButtonAriaLabel}
+					builders={[builder]}
+					size={triggerButtonSize}
+					variant={triggerButtonVariant}
+				>
+					{@render triggerButtonContent()}
 				</Button>
 			</Dialog.Trigger>
 		{/if}
@@ -35,19 +53,24 @@
 				<Dialog.Title>{title}</Dialog.Title>
 				{#if description}
 					<Dialog.Description>
-						{description}
+						{@render description()}
 					</Dialog.Description>
 				{/if}
 			</Dialog.Header>
-			<slot />
+			{@render children()}
 		</Dialog.Content>
 	</Dialog.Root>
 {:else}
 	<Drawer.Root {dismissible} bind:open>
-		{#if needTrigger}
+		{#if triggerButtonContent}
 			<Drawer.Trigger asChild let:builder>
-				<Button aria-label={ariaLabel} builders={[builder]} {size} {variant}>
-					<slot name="buttonContent" />
+				<Button
+					aria-label={triggerButtonAriaLabel}
+					builders={[builder]}
+					size={triggerButtonSize}
+					variant={triggerButtonVariant}
+				>
+					{@render triggerButtonContent()}
 				</Button>
 			</Drawer.Trigger>
 		{/if}
@@ -56,16 +79,16 @@
 				<Drawer.Title>{title}</Drawer.Title>
 				{#if description}
 					<Drawer.Description>
-						{description}
+						{@render description()}
 					</Drawer.Description>
 				{/if}
 			</Drawer.Header>
 			<div class="flex flex-col gap-2 px-4">
-				<slot />
+				{@render children()}
 			</div>
 			<Drawer.Footer class="my-0 shrink pt-2">
 				<Drawer.Close asChild let:builder>
-					<Button builders={[builder]} variant={cancelVariant}>Cancel</Button>
+					<Button builders={[builder]} variant={cancelButtonVariant}>Cancel</Button>
 				</Drawer.Close>
 			</Drawer.Footer>
 		</Drawer.Content>
