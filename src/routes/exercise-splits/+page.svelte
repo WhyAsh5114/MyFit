1e725603021ff3d8dad0a27a1dd566289428556c
WhyAsh5114 <whyash5114@gmail.com>
2024-09-17 23:36:45 +0530

    feat: Migrate exercise splits to new package

--- src/routes/exercise-splits/+page.svelte
+++ src/routes/exercise-splits/+page.svelte
@@ -1,32 +1,24 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
+	import { page } from '$app/stores';
+	import { Badge } from '$lib/components/ui/badge/index.js';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import * as DropdownMenu from '$lib/components/ui/dropdown-menu';
 	import { Input } from '$lib/components/ui/input';
 	import H2 from '$lib/components/ui/typography/H2.svelte';
+	import { trpc } from '$lib/trpc/client';
+	import type { RouterOutputs } from '$lib/trpc/router.js';
+	import { loaderState } from 'svelte-infinite';
+	import type { InfiniteEvent } from 'svelte-infinite-loading';
 	import AddIcon from 'virtual:icons/lucide/plus';
 	import SearchIcon from 'virtual:icons/lucide/search';
-	import LoaderCircle from 'virtual:icons/lucide/loader-circle';
-	import { afterNavigate, goto } from '$app/navigation';
-	import { trpc } from '$lib/trpc/client';
-	import { InfiniteLoader, loaderState } from 'svelte-infinite';
-	import { page } from '$app/stores';
-	import type { ExerciseSplit, ExerciseSplitDay } from '@prisma/client';
-	import { Badge } from '$lib/components/ui/badge/index.js';
-	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
-	import Separator from '$lib/components/ui/separator/separator.svelte';
 	import { exerciseSplitRunes } from './manage/exerciseSplitRunes.svelte.js';
+	import InfiniteLoading from 'svelte-infinite-loading';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
+	import LoaderCircle from 'virtual:icons/lucide/loader-circle';
 
-	type ExerciseSplitsWithSplitDays = (ExerciseSplit & { exerciseSplitDays: ExerciseSplitDay[] })[];
-
-	let { data } = $props();
-	let exerciseSplits: ExerciseSplitsWithSplitDays | 'loading' = $state('loading');
+	let exerciseSplits: RouterOutputs['exerciseSplits']['load'] = $state([]);
 	let searchString = $state($page.url.searchParams.get('search') ?? '');
-
-	afterNavigate(async () => {
-		loaderState.reset();
-		exerciseSplits = await data.exerciseSplits;
-		if (exerciseSplits.length !== 10) loaderState.complete();
-	});
 
 	function updateSearchParam(e: Event) {
 		e.preventDefault();
@@ -34,19 +26,26 @@
 		if (searchString) url.searchParams.set('search', searchString);
 		else url.searchParams.delete('search');
 
-		exerciseSplits = 'loading';
+		exerciseSplits = [];
 		goto(url);
 	}
 
-	async function loadMore() {
+	async function loadMore(infiniteEvent: InfiniteEvent) {
 		const lastExerciseSplit = exerciseSplits.at(-1);
 		if (typeof lastExerciseSplit === 'string' || lastExerciseSplit === undefined) return;
 
 		const newExerciseSplits = await trpc().exerciseSplits.load.query({
 			cursorId: lastExerciseSplit.id
 		});
-		if (exerciseSplits !== 'loading') exerciseSplits.push(...newExerciseSplits);
-		if (newExerciseSplits.length !== 10) loaderState.complete();
+
+		if (newExerciseSplits.length === 0) {
+			infiniteEvent.detail.complete();
+			return;
+		}
+
+		infiniteEvent.detail.loaded();
+		exerciseSplits.push(...newExerciseSplits);
+		if (newExerciseSplits.length < 10) loaderState.complete();
 	}
 
 	function createNewExerciseSplit() {
@@ -78,43 +77,26 @@
 		</DropdownMenu.Root>
 	</div>
 	<div class="flex h-px grow flex-col gap-1 overflow-y-auto">
-		{#if exerciseSplits === 'loading'}
-			{#each Array(10) as _}
-				<div class="flex h-12 items-center justify-between rounded-md border bg-card p-2">
-					<Skeleton class="text-lg-skeleton" />
-					<Skeleton class="badge-skeleton" />
-				</div>
-			{/each}
-		{:else}
-			<InfiniteLoader triggerLoad={loadMore}>
-				{#each exerciseSplits as exerciseSplit}
-					<Button
-						class="mb-1 flex h-12 items-center justify-between rounded-md border bg-card p-2"
-						href="/exercise-splits/{exerciseSplit.id}"
-						variant="outline"
-					>
-						<span class="truncate text-lg font-semibold">{exerciseSplit.name}</span>
-						<Badge>{exerciseSplit.exerciseSplitDays.length} days / cycle</Badge>
-					</Button>
-				{:else}
-					<div class="muted-text-box">No exercise splits found</div>
-				{/each}
-				{#snippet loading()}
-					<LoaderCircle class="animate-spin" />
-				{/snippet}
-				{#snippet error(load)}
-					<Button onclick={load} variant="outline">An error occurred. Retry?</Button>
-				{/snippet}
-				{#snippet noData()}
-					{#if exerciseSplits.length > 0}
-						<div class="flex items-center justify-start gap-2 font-semibold text-muted-foreground">
-							<Separator class="h-0.5 w-20" />
-							<span class="whitespace-nowrap">That's all!</span>
-							<Separator class="h-0.5 w-20" />
-						</div>
-					{/if}
-				{/snippet}
-			</InfiniteLoader>
-		{/if}
+		{#each exerciseSplits as exerciseSplit}
+			<Button
+				class="mb-1 flex h-12 items-center justify-between rounded-md border bg-card p-2"
+				href="/exercise-splits/{exerciseSplit.id}"
+				variant="outline"
+			>
+				<span class="truncate text-lg font-semibold">{exerciseSplit.name}</span>
+				<Badge>{exerciseSplit.exerciseSplitDays.length} days / cycle</Badge>
+			</Button>
+		{/each}
+		<InfiniteLoading on:infinite={loadMore} identifier={searchString}>
+			<div class="flex items-center justify-center gap-2 py-2 text-muted-foreground" slot="noMore">
+				<Separator class="w-20" />
+				That's all
+				<Separator class="w-20" />
+			</div>
+			<div class="muted-text-box text-left" slot="noResults">No workouts found</div>
+			<div slot="spinner">
+				<LoaderCircle class="mx-auto my-2 animate-spin" />
+			</div>
+		</InfiniteLoading>
 	</div>
 </div>
