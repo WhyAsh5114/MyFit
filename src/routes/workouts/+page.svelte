f041364151734292592300373af423bdeb20f427
WhyAsh5114 <whyash5114@gmail.com>
2024-09-17 23:27:12 +0530

    feat: Migrate workouts page to new package

--- src/routes/workouts/+page.svelte
+++ src/routes/workouts/+page.svelte
@@ -1,40 +1,63 @@
 <script lang="ts">
-	import { afterNavigate, goto } from '$app/navigation';
+	import { goto } from '$app/navigation';
 	import { page } from '$app/stores';
 	import Button from '$lib/components/ui/button/button.svelte';
-	import Separator from '$lib/components/ui/separator/separator.svelte';
-	import { Skeleton } from '$lib/components/ui/skeleton/index.js';
 	import H2 from '$lib/components/ui/typography/H2.svelte';
 	import { trpc } from '$lib/trpc/client';
-	import type { RouterOutputs } from '$lib/trpc/router.js';
+	import type { RouterInputs, RouterOutputs } from '$lib/trpc/router.js';
 	import type { WorkoutStatus } from '@prisma/client';
 	import type { DateRange } from 'bits-ui';
-	import { InfiniteLoader, loaderState } from 'svelte-infinite';
-	import LoaderCircle from 'virtual:icons/lucide/loader-circle';
+	import InfiniteLoading, { type InfiniteLoadingEvents } from 'svelte-infinite-loading';
 	import AddIcon from 'virtual:icons/lucide/plus';
 	import FilterComponent from './(components)/FilterComponent.svelte';
-	import { workoutRunes } from './manage/workoutRunes.svelte.js';
 	import NoWorkoutsFilterComponent from './(components)/NoWorkoutsFilterComponent.svelte';
+	import { workoutRunes } from './manage/workoutRunes.svelte.js';
+	import LoaderCircle from 'virtual:icons/lucide/loader-circle';
+	import Separator from '$lib/components/ui/separator/separator.svelte';
 
 	let { data } = $props();
-	let workouts: RouterOutputs['workouts']['load'] | 'loading' = $state('loading');
+	let workouts: RouterOutputs['workouts']['load'] = $state([]);
+	let currentFilters = $derived(getCurrentFilters());
 
-	afterNavigate(async () => {
-		loaderState.reset();
-		workouts = await data.workouts;
-		if (workouts.length !== 10) loaderState.complete();
-	});
+	function getCurrentFilters() {
+		const currentFilters: Exclude<RouterInputs['workouts']['load']['filters'], undefined> = {};
+		const startDate = $page.url.searchParams.get('startDate');
+		const endDate = $page.url.searchParams.get('endDate');
+		const selectedMesocycles = $page.url.searchParams.get('selectedMesocycles');
+		const selectedWorkoutStatuses = $page.url.searchParams.get('selectedWorkoutStatuses');
 
-	async function loadMore() {
+		if (startDate) {
+			currentFilters.startDate = new Date(startDate);
+		}
+		if (endDate) {
+			currentFilters.endDate = new Date(endDate);
+		}
+		if (selectedMesocycles) {
+			currentFilters.selectedMesocycles = JSON.parse(selectedMesocycles);
+		}
+		if (selectedWorkoutStatuses) {
+			currentFilters.selectedWorkoutStatuses = JSON.parse(selectedWorkoutStatuses);
+		}
+
+		return currentFilters;
+	}
+
+	async function loadMore(infiniteEvent: InfiniteLoadingEvents['infinite']) {
 		const lastWorkout = workouts.at(-1);
-		if (typeof lastWorkout === 'string' || lastWorkout === undefined) return;
 
 		const newWorkouts = await trpc().workouts.load.query({
-			cursorId: lastWorkout.id,
-			filters: data.currentFilters
+			cursorId: lastWorkout?.id,
+			filters: getCurrentFilters()
 		});
-		if (workouts !== 'loading') workouts.push(...newWorkouts);
-		if (newWorkouts.length !== 10) loaderState.complete();
+
+		if (newWorkouts.length === 0) {
+			infiniteEvent.detail.complete();
+			return;
+		}
+
+		infiniteEvent.detail.loaded();
+		workouts.push(...newWorkouts);
+		if (newWorkouts.length !== 10) infiniteEvent.detail.complete();
 	}
 
 	function createNewWorkout() {
@@ -72,6 +95,8 @@
 		} else {
 			newURL.searchParams.delete('selectedWorkoutStatuses');
 		}
+
+		workouts = [];
 		goto(newURL);
 	}
 </script>
@@ -80,70 +105,51 @@
 
 <div class="flex grow flex-col gap-2">
 	<div class="flex gap-1">
-		{#if workouts !== 'loading'}
-			{#await data.filterData}
-				TODO: skeleton
-			{:then filterData}
-				{#if filterData}
-					<FilterComponent {filterData} currentFilters={data.currentFilters} {setFilters} />
-				{:else}
-					<NoWorkoutsFilterComponent />
-				{/if}
-			{/await}
-		{/if}
+		{#await data.filterData}
+			TODO: skeleton
+		{:then filterData}
+			{#if filterData}
+				<FilterComponent {filterData} {currentFilters} {setFilters} />
+			{:else}
+				<NoWorkoutsFilterComponent />
+			{/if}
+		{/await}
 		<Button aria-label="create-workout" onclick={createNewWorkout}><AddIcon /></Button>
 	</div>
 	<div class="flex h-px grow flex-col gap-1 overflow-y-auto">
-		{#if workouts === 'loading'}
-			{#each Array(10) as _}
-				<div class="flex h-12 items-center justify-between rounded-md border bg-card p-2">
-					<Skeleton class="text-lg-skeleton" />
-					<Skeleton class="badge-skeleton" />
-				</div>
-			{/each}
-		{:else}
-			<InfiniteLoader triggerLoad={loadMore}>
-				{#each workouts as workout}
-					{@const { workoutOfMesocycle } = workout}
-					<Button
-						class="mb-1 flex h-12 items-center justify-between rounded-md border bg-card p-2"
-						href="/workouts/{workout.id}"
-						variant="outline"
-					>
-						<span class="text-lg font-semibold">
-							{workout.startedAt.toLocaleDateString(undefined, {
-								day: '2-digit',
-								month: 'long'
-							})}
-						</span>
-						{#if workoutOfMesocycle}
-							{@const splitDayName =
-								workoutOfMesocycle.mesocycle.mesocycleExerciseSplitDays[workoutOfMesocycle.splitDayIndex].name}
-							<span class="truncate text-muted-foreground">
-								{splitDayName === '' ? 'Rest' : splitDayName}
-								{workoutOfMesocycle.workoutStatus === 'Skipped' ? '(skipped)' : ''}
-							</span>
-						{/if}
-					</Button>
-				{:else}
-					<div class="muted-text-box">No workouts found</div>
-				{/each}
-				{#snippet loading()}
-					<LoaderCircle class="animate-spin" />
-				{/snippet}
-				{#snippet error(load)}
-					<Button onclick={load} variant="outline">An error occurred. Retry?</Button>
-				{/snippet}
-				{#snippet noData()}
-					{#if workouts.length > 0}
-						<div class="flex items-center justify-start gap-2 font-semibold text-muted-foreground">
-							<Separator class="h-0.5 w-20" />
-							<span class="whitespace-nowrap">That's all!</span>
-							<Separator class="h-0.5 w-20" />
-						</div>
-					{/if}
-				{/snippet}
-			</InfiniteLoader>
-		{/if}
+		{#each workouts as workout}
+			{@const { workoutOfMesocycle } = workout}
+			<Button
+				class="flex h-12 items-center justify-between rounded-md border bg-card p-2"
+				href="/workouts/{workout.id}"
+				variant="outline"
+			>
+				<span class="text-lg font-semibold">
+					{workout.startedAt.toLocaleDateString(undefined, {
+						day: '2-digit',
+						month: 'long'
+					})}
+				</span>
+				{#if workoutOfMesocycle}
+					{@const splitDayName =
+						workoutOfMesocycle.mesocycle.mesocycleExerciseSplitDays[workoutOfMesocycle.splitDayIndex].name}
+					<span class="truncate text-muted-foreground">
+						{splitDayName === '' ? 'Rest' : splitDayName}
+						{workoutOfMesocycle.workoutStatus === 'Skipped' ? '(skipped)' : ''}
+					</span>
+				{/if}
+			</Button>
+		{/each}
+		<InfiniteLoading on:infinite={loadMore} identifier={currentFilters}>
+			<div class="flex items-center justify-center gap-2 py-2 text-muted-foreground" slot="noMore">
+				<Separator class="w-20" />
+				That's all
+				<Separator class="w-20" />
+			</div>
+			<div class="muted-text-box text-left" slot="noResults">No workouts found</div>
+			<div slot="spinner">
+				<LoaderCircle class="mx-auto my-2 animate-spin" />
+			</div>
+		</InfiniteLoading>
 	</div>
 </div>
