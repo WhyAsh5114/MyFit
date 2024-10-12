12d1bc4a964cd95f961cf5f42903d40c262b147d
WhyAsh5114 <whyash5114@gmail.com>
2024-10-12 17:31:25 +0530

    fix: compress images to webp and pre-define width and height

--- src/routes/(components)/page/CarouselComponent.svelte
+++ src/routes/(components)/page/CarouselComponent.svelte
@@ -6,20 +6,30 @@
 <Carousel.Root class="mx-auto my-4 w-3/4">
 	<Carousel.Content class="items-center">
 		<Carousel.Item>
-			<img class="border" src="/screenshots/{$mode}/SplitDayChart.png" alt="SplitDayChart" />
-		</Carousel.Item>
-		<Carousel.Item>
 			<img
 				class="border"
-				src="/screenshots/{$mode}/MicrocycleVolumeDistributionChart.png"
-				alt="MicrocycleVolumeDistributionChart"
+				src="/screenshots/{$mode}/SplitDayChart.webp"
+				alt="SplitDayChart"
+				width={240}
+				height={236.5}
 			/>
 		</Carousel.Item>
 		<Carousel.Item>
 			<img
 				class="border"
-				src="/screenshots/{$mode}/MuscleGroupVolumeDistributionChart.png"
+				src="/screenshots/{$mode}/MicrocycleVolumeDistributionChart.webp"
+				alt="MicrocycleVolumeDistributionChart"
+				width={240}
+				height={185.5}
+			/>
+		</Carousel.Item>
+		<Carousel.Item>
+			<img
+				class="border"
+				src="/screenshots/{$mode}/MuscleGroupVolumeDistributionChart.webp"
 				alt="MuscleGroupVolumeDistributionChart"
+				width={240}
+				height={234}
 			/>
 		</Carousel.Item>
 	</Carousel.Content>
