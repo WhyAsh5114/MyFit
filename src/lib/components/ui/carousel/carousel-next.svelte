40635aafd7a6c7be2ab70499e17d9eb24928d2cd
WhyAsh5114 <whyash5114@gmail.com>
2024-10-12 21:54:03 +0530

    fix: remove lucide-svelte dependency

--- src/lib/components/ui/carousel/carousel-next.svelte
+++ src/lib/components/ui/carousel/carousel-next.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import ArrowRight from 'lucide-svelte/icons/arrow-right';
+	import ArrowRight from 'virtual:icons/lucide/arrow-right';
 	import type { VariantProps } from 'tailwind-variants';
 	import { getEmblaContext } from './context.js';
 	import { cn } from '$lib/utils.js';
--- src/lib/components/ui/carousel/carousel-previous.svelte
+++ src/lib/components/ui/carousel/carousel-previous.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import ArrowLeft from 'lucide-svelte/icons/arrow-left';
+	import ArrowLeft from 'virtual:icons/lucide/arrow-left';
 	import type { VariantProps } from 'tailwind-variants';
 	import { getEmblaContext } from './context.js';
 	import { cn } from '$lib/utils.js';
