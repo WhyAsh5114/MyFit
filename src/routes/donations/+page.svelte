77f3993039679bce788e2a547bcf3f7046951636
WhyAsh5114 <whyash5114@gmail.com>
2024-10-12 21:08:44 +0530

    feat: added sections to donations page

--- src/routes/donations/+page.svelte
+++ src/routes/donations/+page.svelte
@@ -4,12 +4,16 @@
 </script>
 
 <H2>Donations</H2>
+
+<H3>How MyFit started</H3>
 <p>
 	I'm just a college student who started MyFit as a fun side project. I never thought I'd actually get users 😅. My
 	original intention was just to have an app for personal use and as a resume project to help me get a software
 	engineering job. Over time, this project grew and gained a handful of users, which made me unreasonably happy.
 </p>
 <br />
+
+<H3>Why you shouldn't donate</H3>
 <p>
 	Please don't feel obliged to donate. I know how it feels to not have, or not feel like spending, over $20 for a simple
 	workout tracking app. To be honest, I don't think I'll even need donations soon, as the free tiers offered by my
@@ -17,16 +21,23 @@
 	giving feedback, or perhaps starring this repository on GitHub is more than enough for me 😊.
 </p>
 <br />
+
+<H3>Why you should donate</H3>
+<p>
+	If you've used the app for quite some time, you genuinely enjoyed it, and feel like I deserve a free diet coke! You'll
+	get some extra benefits such as: issue prioritization and early access to features.
+</p>
+<br />
+
+<H3>How donations will be used</H3>
 <p>
 	The primary way donations to MyFit will be used is for hosting the project (Vercel) and its database (CockroachDB). If
 	any are left over, it'll simply motivate me to work on this project further and improve it in every way possible.
 </p>
 <br />
-<br />
 
 <H3>GitHub Sponsors Link</H3>
 <iframe src="https://github.com/sponsors/WhyAsh5114/button" class="h-8 w-32" title="Sponsor WhyAsh5114"></iframe>
-<br />
 <br />
 
 <H3>Buy Me A Coffee Link</H3>
