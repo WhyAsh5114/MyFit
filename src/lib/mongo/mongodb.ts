ea5ea6d837f176ed2c7178db9aae58e7cac58399
WhyAsh5114 <whyash5114@gmail.com>
2024-10-14 12:33:27 +0530

    feat: added mongodb package

---
+++ src/lib/mongo/mongodb.ts
@@ -0,0 +1,36 @@
+// This approach is taken from https://github.com/vercel/next.js/tree/canary/examples/with-mongodb
+import { MongoClient } from 'mongodb';
+import { MONGODB_URI } from '$env/static/private';
+
+if (!MONGODB_URI) {
+	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
+}
+
+const uri = MONGODB_URI;
+const options = {};
+
+let client;
+let clientPromise: Promise<MongoClient>;
+
+if (process.env.NODE_ENV === 'development') {
+	/*
+	 * In development mode, use a global variable so that the value
+	 * Is preserved across module reloads caused by HMR (Hot Module Replacement).
+	 */
+	// eslint-disable-next-line svelte/@typescript-eslint/no-unnecessary-condition
+	if (!global._mongoClientPromise) {
+		client = new MongoClient(uri, options);
+		global._mongoClientPromise = client.connect();
+	}
+	clientPromise = global._mongoClientPromise;
+} else {
+	// In production mode, it's best to not use a global variable.
+	client = new MongoClient(uri, options);
+	clientPromise = client.connect();
+}
+
+/*
+ * Export a module-scoped MongoClient promise. By doing this in a
+ * separate module, the client can be shared across functions.
+ */
+export default clientPromise;
