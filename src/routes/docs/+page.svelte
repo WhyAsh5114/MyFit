aa16fd5c5442a15447ba5a6451f99cf687a803b6
WhyAsh5114 <whyash5114@gmail.com>
2024-10-17 12:51:14 +0530

    feat: added docs page

---
+++ src/routes/docs/+page.svelte
@@ -0,0 +1,25 @@
+<div class="prose prose-neutral max-w-none dark:prose-invert lg:prose-lg">
+	<h1>Docs</h1>
+	<blockquote>Just a basic overview of things work in this app, as there are some differences from V2</blockquote>
+
+	<h2>Exercise splits</h2>
+	<p>
+		An exercise split refers to how workout sessions are divided across different days to target specific muscle groups
+		or movement patterns. Examples: <i>Push Pull Legs</i>, <i>Upper Lower</i>, and <i>Full Body.</i>
+	</p>
+	<p>Tha app supports both synchronous (weekly) splits, and asynchronous splits (non-weekly) splits.</p>
+
+	<h2>Mesocycle</h2>
+	<p>
+		A mesocycle is a unit of training consisting of several microcycles (repeated splits), usually lasting 4-6 weeks.
+		The mesocycle structures progression and recovery, with increasing intensity or volume followed by deload phases to
+		avoid overtraining.
+	</p>
+
+	<h2>Workouts</h2>
+	<p>
+		A single workout, consisting of multiple exercises, each with their own sets and options. Workouts can be done with
+		or without an active mesocycle. Using an active mesocycle allows the app to progress performance and sets every
+		microcycle.
+	</p>
+</div>
