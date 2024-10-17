c2e7799709744c06b49408c5cb29fc208b346c98
WhyAsh5114 <whyash5114@gmail.com>
2024-10-17 13:04:45 +0530

    feat: finished up basic docs

--- src/routes/docs/+page.svelte
+++ src/routes/docs/+page.svelte
@@ -13,13 +13,25 @@
 	<p>
 		A mesocycle is a unit of training consisting of several microcycles (repeated splits), usually lasting 4-6 weeks.
 		The mesocycle structures progression and recovery, with increasing intensity or volume followed by deload phases to
-		avoid overtraining.
+		avoid overtraining. Deload feature will be coming soon!
 	</p>
 
 	<h2>Workouts</h2>
 	<p>
 		A single workout, consisting of multiple exercises, each with their own sets and options. Workouts can be done with
-		or without an active mesocycle. Using an active mesocycle allows the app to progress performance and sets every
-		microcycle.
+		or without an active mesocycle.
+	</p>
+	<p>Using an active mesocycle allows the app to progress performance and sets every microcycle.</p>
+
+	<h2>Mesocycle exercise split</h2>
+	<p>
+		Whenever you create a mesocycle with an exercise split, that split is actually cloned for that mesocycle. This
+		allows you and the app to make changes to the mesocycle's split without altering the original split.
+	</p>
+
+	<h2 class="italic">"I need more help!"</h2>
+	<p>
+		Feel free to <a href="https://github.com/WhyAsh5114/issues">open an issue</a> on the GitHub repository, will be glad
+		to help you out and add more details to the docs
 	</p>
 </div>
