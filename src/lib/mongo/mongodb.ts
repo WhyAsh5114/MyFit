4f3a2f272228f5b1788a0471af4baa24adc09246
WhyAsh5114 <whyash5114@gmail.com>
2024-11-16 13:46:25 +0530

    fix: don't require MONGODB_URI env var

--- src/lib/mongo/mongodb.ts
+++ src/lib/mongo/mongodb.ts
@@ -3,17 +3,14 @@ import { MongoClient } from 'mongodb';
 import 'dotenv/config';
 
 const MONGODB_URI = process.env.MONGODB_URI;
-if (!MONGODB_URI) {
-	throw new Error('Invalid/Missing environment variable: "MONGODB_URI"');
-}
 
 const uri = MONGODB_URI;
 const options = {};
 
 let client;
-let clientPromise: Promise<MongoClient>;
+let clientPromise: Promise<MongoClient> | undefined;
 
-if (process.env.NODE_ENV === 'development') {
+if (process.env.NODE_ENV === 'development' && uri) {
 	/*
 	 * In development mode, use a global variable so that the value
 	 * Is preserved across module reloads caused by HMR (Hot Module Replacement).
@@ -23,7 +20,7 @@ if (process.env.NODE_ENV === 'development') {
 		global._mongoClientPromise = client.connect();
 	}
 	clientPromise = global._mongoClientPromise;
-} else {
+} else if (uri) {
 	// In production mode, it's best to not use a global variable.
 	client = new MongoClient(uri, options);
 	clientPromise = client.connect();
