4a0ef3b8b4852ec35dc21867945de3adfe11bbc9
WhyAsh5114 <whyash5114@gmail.com>
2024-12-27 08:52:54 +0530

    fix: use sheet instead of drawer for WarmUps to avoid weird layout shifting on mobile

--- src/routes/workouts/manage/exercises/(components)/WarmUpDialog.svelte
+++ src/routes/workouts/manage/exercises/(components)/WarmUpDialog.svelte
@@ -1,9 +1,9 @@
 <script lang="ts">
-	import ResponsiveDialog from '$lib/components/ResponsiveDialog.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import { Input } from '$lib/components/ui/input/index.js';
 	import { Label } from '$lib/components/ui/label/index.js';
 	import * as Popover from '$lib/components/ui/popover';
+	import * as Sheet from '$lib/components/ui/sheet';
 	import * as Table from '$lib/components/ui/table';
 	import { untrack } from 'svelte';
 	import CalculatorIcon from 'virtual:icons/lucide/calculator';
@@ -60,64 +60,67 @@
 	}
 </script>
 
-<ResponsiveDialog title="Warm up" bind:open={workoutRunes.exerciseWarmUpDialogOpen}>
-	{#snippet description()}
-		{exercise?.name}
-	{/snippet}
-	{#if exercise}
-		<form class="grid grid-cols-2 gap-1" onsubmit={generateWarmUp}>
-			<div class="flex w-full max-w-sm flex-col gap-1.5">
-				<Label for="one-rep-max">One rep max</Label>
-				<Input required id="one-rep-max" type="number" step={0.25} placeholder="Type here" bind:value={oneRepMax} />
-			</div>
-			<div class="flex w-full max-w-sm flex-col gap-1.5">
-				<Label for="total-warm-up-sets">Total warm-up sets</Label>
-				<Input required id="total-warm-up-sets" type="number" placeholder="Type here" bind:value={totalWarmUpSets} />
-			</div>
-			<Popover.Root bind:open={oneRepMaxCalculatorOpen} onOpenChange={(v) => (oneRepMaxCalculatorOpen = v)}>
-				<Popover.Trigger asChild let:builder>
-					<Button variant="secondary" builders={[builder]} class="gap-2">
-						1-RM calculator
-						<CalculatorIcon />
-					</Button>
-				</Popover.Trigger>
-				<Popover.Content>
-					<form onsubmit={submitOneRepMax} class="grid grid-cols-2 gap-1">
-						<div class="flex w-full max-w-sm flex-col gap-1.5">
-							<Label for="weight">Weight</Label>
-							<Input required id="weight" type="number" step={0.25} placeholder="Type here" bind:value={weight} />
-						</div>
-						<div class="flex w-full max-w-sm flex-col gap-1.5">
-							<Label for="reps">Reps</Label>
-							<Input required id="reps" type="number" placeholder="Type here" bind:value={reps} />
-						</div>
-						<Button type="submit" class="col-span-full">Calculate</Button>
-					</form>
-				</Popover.Content>
-			</Popover.Root>
-			<Button type="submit">Generate warm up</Button>
-		</form>
-	{/if}
-	{#if warmUpSets}
-		<Table.Root>
-			<Table.Header>
-				<Table.Row>
-					<Table.Head></Table.Head>
-					<Table.Head>Reps</Table.Head>
-					<Table.Head>Load</Table.Head>
-					<Table.Head>%RM</Table.Head>
-				</Table.Row>
-			</Table.Header>
-			<Table.Body>
-				{#each warmUpSets as warmUpSet, idx}
+<Sheet.Root bind:open={workoutRunes.exerciseWarmUpDialogOpen}>
+	<Sheet.Content class="w-5/6">
+		<Sheet.Header>
+			<Sheet.Title>Warm up</Sheet.Title>
+			<Sheet.Description>{exercise?.name}</Sheet.Description>
+		</Sheet.Header>
+		{#if exercise}
+			<form class="grid gap-2 py-4" onsubmit={generateWarmUp}>
+				<div class="flex w-full max-w-sm flex-col gap-1.5">
+					<Label for="one-rep-max">One rep max</Label>
+					<Input required id="one-rep-max" type="number" step={0.25} placeholder="Type here" bind:value={oneRepMax} />
+				</div>
+				<div class="flex w-full max-w-sm flex-col gap-1.5">
+					<Label for="total-warm-up-sets">Total warm-up sets</Label>
+					<Input required id="total-warm-up-sets" type="number" placeholder="Type here" bind:value={totalWarmUpSets} />
+				</div>
+				<Popover.Root bind:open={oneRepMaxCalculatorOpen} onOpenChange={(v) => (oneRepMaxCalculatorOpen = v)}>
+					<Popover.Trigger asChild let:builder>
+						<Button variant="secondary" builders={[builder]} class="gap-2">
+							1-RM calculator
+							<CalculatorIcon />
+						</Button>
+					</Popover.Trigger>
+					<Popover.Content>
+						<form onsubmit={submitOneRepMax} class="grid grid-cols-2 gap-1">
+							<div class="flex w-full max-w-sm flex-col gap-1.5">
+								<Label for="weight">Weight</Label>
+								<Input required id="weight" type="number" step={0.25} placeholder="Type here" bind:value={weight} />
+							</div>
+							<div class="flex w-full max-w-sm flex-col gap-1.5">
+								<Label for="reps">Reps</Label>
+								<Input required id="reps" type="number" placeholder="Type here" bind:value={reps} />
+							</div>
+							<Button type="submit" class="col-span-full">Calculate</Button>
+						</form>
+					</Popover.Content>
+				</Popover.Root>
+				<Button type="submit">Generate warm up</Button>
+			</form>
+		{/if}
+		{#if warmUpSets}
+			<Table.Root>
+				<Table.Header>
 					<Table.Row>
-						<Table.Cell class="font-medium">{idx + 1}</Table.Cell>
-						<Table.Cell>{warmUpSet.reps}</Table.Cell>
-						<Table.Cell>{warmUpSet.load.toFixed(1)}</Table.Cell>
-						<Table.Cell>{warmUpSet.oneRMPercentage}</Table.Cell>
+						<Table.Head></Table.Head>
+						<Table.Head>Reps</Table.Head>
+						<Table.Head>Load</Table.Head>
+						<Table.Head>%RM</Table.Head>
 					</Table.Row>
-				{/each}
-			</Table.Body>
-		</Table.Root>
-	{/if}
-</ResponsiveDialog>
+				</Table.Header>
+				<Table.Body>
+					{#each warmUpSets as warmUpSet, idx}
+						<Table.Row>
+							<Table.Cell class="font-medium">{idx + 1}</Table.Cell>
+							<Table.Cell>{warmUpSet.reps}</Table.Cell>
+							<Table.Cell>{warmUpSet.load.toFixed(1)}</Table.Cell>
+							<Table.Cell>{warmUpSet.oneRMPercentage}</Table.Cell>
+						</Table.Row>
+					{/each}
+				</Table.Body>
+			</Table.Root>
+		{/if}
+	</Sheet.Content>
+</Sheet.Root>
