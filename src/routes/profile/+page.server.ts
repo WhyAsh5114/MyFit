12bb0a97273b37f1d109c457dcfd54b2052ba645
WhyAsh5114 <whyash5114@gmail.com>
2024-12-28 13:44:44 +0530

    feat: add user workout, exercise, and set counts to profile page

--- src/routes/profile/+page.server.ts
+++ src/routes/profile/+page.server.ts
@@ -1,7 +1,19 @@
+import { prisma } from '$lib/prisma.js';
 import { createContext } from '$lib/trpc/context';
 import { createCaller } from '$lib/trpc/router';
+import { redirect } from '@sveltejs/kit';
 
 export const load = async (event) => {
 	const trpc = createCaller(await createContext(event));
-	return { V2Counts: trpc.users.checkV2MigrationAvailability() };
+	const session = await event.locals.auth();
+	if (!session) redirect(302, '/');
+
+	return {
+		V2Counts: trpc.users.checkV2MigrationAvailability(),
+		userCounts: {
+			workouts: prisma.workout.count({ where: { userId: session.user?.id } }),
+			exercises: prisma.workoutExercise.count({ where: { workout: { userId: session.user?.id } } }),
+			sets: prisma.workoutExerciseSet.count({ where: { workoutExercise: { workout: { userId: session.user?.id } } } })
+		}
+	};
 };
