4ff7767439e87c4adeee82f79cb341af99ea031b
WhyAsh5114 <whyash5114@gmail.com>
2025-01-05 18:44:43 +0000

    feat: add MDX support

---
+++ app/(app)/docs/page.tsx
@@ -0,0 +1,9 @@
+import Docs from "./docs.mdx";
+
+export default function Page() {
+  return (
+    <article className="prose dark:prose-invert">
+      <Docs />
+    </article>
+  );
+}
--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
+import createMDX from "@next/mdx";
 
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
 };
 
-export default nextConfig;
+const withMDX = createMDX();
+
+export default withMDX(nextConfig);
