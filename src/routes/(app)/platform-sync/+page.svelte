2ea8f9b7549cea8e48559ffbb96927c39714c993
WhyAsh5114 <whyash5114@gmail.com>
2025-04-12 19:34:16 +0530

    feat: enhance platform sync page with loading states and OS detection

--- src/routes/(app)/platform-sync/+page.svelte
+++ src/routes/(app)/platform-sync/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import H1 from '$lib/components/typography/h1.svelte';
-	import { LoaderCircle } from 'lucide-svelte';
+	import Button from '$lib/components/ui/button/button.svelte';
+	import { CircleOffIcon, DownloadIcon, LoaderCircle } from 'lucide-svelte';
 	import { onMount } from 'svelte';
 
+	let detectedOS = $state<'iOS' | 'Android'>();
 	let isAvailable = $state<boolean>();
 	let isAuthorized = $state<boolean>();
 	let result = $state<{ startDate: string; endDate: string; value: number }[]>([]);
@@ -15,6 +17,14 @@
 			);
 		} else {
 			isAvailable = false;
+		}
+
+		const userAgent = navigator.userAgent;
+		if (/iPad|iPhone|iPod/.test(userAgent)) {
+			detectedOS = 'iOS';
+		}
+		if (/android/i.test(userAgent)) {
+			detectedOS = 'Android';
 		}
 	});
 
@@ -43,28 +53,47 @@
 	});
 </script>
 
-<H1>Platform Sync</H1>
+<H1>Platform sync</H1>
 
 {#if isAvailable === undefined}
-	<LoaderCircle class="animate-spin" />
-{:else if isAvailable}
-	<p class="text-primary">Platform Sync is available</p>
-{:else}
-	<p class="text-red-500">Platform Sync is not available</p>
-{/if}
-
-{#if isAuthorized === undefined}
-	<LoaderCircle class="animate-spin" />
-{:else if isAuthorized}
-	<p class="text-primary">Platform Sync is authorized</p>
-{:else}
-	<p class="text-red-500">Platform Sync is not authorized</p>
-{/if}
-
-{#each result as item (item.startDate)}
-	<div>
-		<p>Steps: {item.value}</p>
-		<p>Start Date: {new Date(item.startDate).toLocaleString()}</p>
-		<p>End Date: {new Date(item.endDate).toLocaleString()}</p>
+	<div class="text-muted-foreground flex h-full flex-col items-center justify-center gap-2">
+		<LoaderCircle size={128} strokeWidth={1} class="animate-spin" />
+		<span>Loading</span>
 	</div>
-{/each}
+{:else if isAvailable}
+	{#if isAuthorized === undefined}
+		<LoaderCircle class="animate-spin" />
+	{:else if isAuthorized}
+		{#each result as item (item.startDate)}
+			<div>
+				<p>Steps: {item.value}</p>
+				<p>Start Date: {new Date(item.startDate).toLocaleString()}</p>
+				<p>End Date: {new Date(item.endDate).toLocaleString()}</p>
+			</div>
+		{/each}
+	{:else}
+		<Button>Grant permissions</Button>
+	{/if}
+{:else}
+	<div class="text-muted-foreground flex h-full flex-col items-center justify-center gap-2">
+		<CircleOffIcon size={128} strokeWidth={1} />
+		<span class="font-semibold">Platform syncing not available</span>
+		<p class="text-center text-sm">
+			You're running the web version of the app, which doesn't support platform syncing.
+		</p>
+		{#if detectedOS}
+			<Button
+				href={detectedOS === 'iOS' ? '/platform-sync/capacitor' : '/platform-sync/cordova'}
+				variant="secondary"
+			>
+				<DownloadIcon />
+				Download for {detectedOS}
+			</Button>
+		{:else}
+			<p class="text-center text-sm">
+				To use platform syncing, please download the native version of the app on a supported
+				platform.
+			</p>
+		{/if}
+	</div>
+{/if}
