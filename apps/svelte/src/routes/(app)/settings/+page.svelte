bdde10576d5228d978e41482dfaf9774c5195386
WhyAsh5114 <whyash5114@gmail.com>
2025-08-16 18:07:42 +0000

    feat: added mode setting

--- apps/svelte/src/routes/(app)/settings/+page.svelte
+++ apps/svelte/src/routes/(app)/settings/+page.svelte
@@ -2,9 +2,11 @@
 	import H1 from '$lib/components/typography/h1.svelte';
 	import Button from '$lib/components/ui/button/button.svelte';
 	import * as Card from '$lib/components/ui/card/index.js';
+	import * as Select from '$lib/components/ui/select/index.js';
 	import { client } from '$lib/idb-client';
 	import { LoaderCircleIcon, RefreshCcwIcon, RotateCwIcon, TrashIcon } from '@lucide/svelte';
 	import { createMutation, createQuery } from '@tanstack/svelte-query';
+	import { setMode, userPrefersMode } from 'mode-watcher';
 	import { toast } from 'svelte-sonner';
 	import { appLayoutState } from '../../_components/app-layout-state.svelte';
 
@@ -108,6 +110,29 @@
 				Check now <RefreshCcwIcon />
 			</Button>
 		{/if}
+	</Card.Content>
+</Card.Root>
+
+<Card.Root class="gap-4">
+	<Card.Header>
+		<Card.Title>Color mode</Card.Title>
+		<Card.Description>Manage the app's color mode settings</Card.Description>
+	</Card.Header>	
+	<Card.Content class="flex justify-end">
+		<Select.Root
+			type="single"
+			value={userPrefersMode.current}
+			onValueChange={(v) => setMode(v as Parameters<typeof setMode>[0])}
+		>
+			<Select.Trigger class="capitalize">
+				{userPrefersMode.current}
+			</Select.Trigger>
+			<Select.Content align="end">
+				<Select.Item value="light">Light</Select.Item>
+				<Select.Item value="dark">Dark</Select.Item>
+				<Select.Item value="system">System</Select.Item>
+			</Select.Content>
+		</Select.Root>
 	</Card.Content>
 </Card.Root>
 
