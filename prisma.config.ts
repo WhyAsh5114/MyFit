f53e475afe173af9febfb6bc42c8bc21cf532f72
WhyAsh5114 <whyash5114@gmail.com>
2026-01-04 22:50:25 +0530

    refactor: change schema.prisma location

--- prisma.config.ts
+++ prisma.config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { defineConfig, env } from 'prisma/config';
 
 export default defineConfig({
-	schema: 'prisma/schema',
+	schema: 'prisma',
 	migrations: {
 		path: 'prisma/migrations',
 		seed: 'tsx prisma/seed.ts'
