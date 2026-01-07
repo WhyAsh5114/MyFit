fc567ab202176a81873ab4afa326f91a07f7e2fb
WhyAsh5114 <whyash5114@gmail.com>
2026-01-07 15:14:18 +0530

    ci: update actions files

--- prisma.config.ts
+++ prisma.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
 	schema: 'prisma',
 	migrations: {
 		path: 'prisma/migrations',
-		seed: 'tsx prisma/seed.ts'
+		seed: 'tsx prisma/seed/seed.ts'
 	},
 	datasource: {
 		url: env('DATABASE_URL')
