79a7e94bbe2854ea98441c691e04e71e67ffbc96
WhyAsh5114 <whyash5114@gmail.com>
2026-02-06 20:36:26 +0530

    feat: setup capacitor 8

---
+++ apps/frontend/android/app/src/main/java/fit/myfit/MainActivity.java
@@ -0,0 +1,5 @@
+package fit.myfit;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ apps/frontend/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'fit.myfit',
+  appName: 'MyFit',
+  webDir: 'build'
+};
+
+export default config;
