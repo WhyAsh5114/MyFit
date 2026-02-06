a80950d92327f28902c21e5011731474e361a90c
WhyAsh5114 <whyash5114@gmail.com>
2026-02-06 21:38:51 +0530

    feat: link db package to both apps

--- apps/frontend/capacitor.config.ts
+++ apps/frontend/capacitor.config.ts
@@ -1,9 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'fit.myfit',
-  appName: 'MyFit',
-  webDir: 'build'
+	appId: 'fit.myfit',
+	appName: 'MyFit',
+	webDir: 'build'
 };
 
 export default config;
