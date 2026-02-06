3eb71209a57998d7dc13586d61dc50983418efa6
WhyAsh5114 <whyash5114@gmail.com>
2026-02-06 19:08:25 +0530

    feat: monorepo with sveltekit app

---
+++ apps/frontend/vite.config.ts
@@ -0,0 +1,14 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import devtoolsJson from 'vite-plugin-devtools-json';
+import tailwindcss from '@tailwindcss/vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		devtoolsJson(),
+		paraglideVitePlugin({ project: './project.inlang', outdir: './src/lib/paraglide' })
+	]
+});
