437eb61558b3ed4c0ecdea7cf28d097a12f86645
WhyAsh5114 <whyash5114@gmail.com>
2026-02-07 18:05:36 +0530

    ui: add some padding to icons

--- apps/frontend/capacitor.config.ts
+++ apps/frontend/capacitor.config.ts
@@ -5,7 +5,7 @@ const config: CapacitorConfig = {
 	appName: 'MyFit',
 	webDir: 'build',
 	plugins: { StatusBar: { overlaysWebView: false } },
-	server: { cleartext: true, url: 'http://localhost:5173' }
+	// server: { cleartext: true, url: 'http://localhost:5173' }
 };
 
 export default config;
